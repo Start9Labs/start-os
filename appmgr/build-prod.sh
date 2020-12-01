2b3309b002d69c7857e83bab2e2b895b6f213369
Aiden McClelland <me@drbonez.dev>
2020-11-30 22:25:47 -0700

    update build scripts

--- appmgr/build-prod.sh
+++ appmgr/build-prod.sh
@@ -5,5 +5,7 @@ shopt -s expand_aliases
 
 alias 'rust-arm-builder'='docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-arm-cross:latest'
 
-rust-arm-builder cargo build --release --features=production
+cd ..
+rust-arm-builder sh -c "(cd appmgr && cargo build --release --features=production)"
+cd appmgr
 rust-arm-builder arm-linux-gnueabi-strip target/armv7-unknown-linux-gnueabihf/release/appmgr
