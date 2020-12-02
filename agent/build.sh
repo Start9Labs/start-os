6c57491d3f25a690e9a2af91f45aeb85859d8763
Keagan McClelland <keagan.mcclelland@gmail.com>
2020-12-02 13:20:09 -0700

    fix Makefile

--- agent/build.sh
+++ agent/build.sh
@@ -3,4 +3,4 @@
 cat config/settings.yml | grep app-mgr-version-spec
 cat package.yaml | grep version
 
-stack --local-bin-path ./executables build --copy-bins #--flag start9-agent:disable-auth
+stack --local-bin-path ./dist build --copy-bins #--flag start9-agent:disable-auth
