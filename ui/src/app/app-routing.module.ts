7e39f81f5dee59f2ae48ed4e53c3790f9a2693a0
Matt Hill <matthewonthemoon@gmail.com>
2020-12-03 09:16:38 -0700

    changes for 0.2.7

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
-      initialNavigation: false,
+      initialNavigation: 'disabled',
       useHash: true,
     }),
   ],
