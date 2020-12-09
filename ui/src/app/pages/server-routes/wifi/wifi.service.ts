eece10da88f29b4689879e15e2742eac5e722691
Matt Hill <matthewonthemoon@gmail.com>
2020-12-09 13:47:13 -0700

    alert after connecting to new wifi

--- ui/src/app/pages/server-routes/wifi/wifi.service.ts
+++ ui/src/app/pages/server-routes/wifi/wifi.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { ToastController } from '@ionic/angular'
+import { AlertController, ToastController } from '@ionic/angular'
 import { ApiService } from 'src/app/services/api/api.service'
 import { pauseFor } from 'src/app/util/misc.util'
 import { ServerModel } from 'src/app/models/server-model'
@@ -12,6 +12,7 @@ export class WifiService {
   constructor (
     private readonly apiService: ApiService,
     private readonly toastCtrl: ToastController,
+    private readonly alertCtrl: AlertController,
     private readonly serverModel: ServerModel,
   ) { }
 
@@ -30,10 +31,13 @@ export class WifiService {
     const maxAttempts = 5
     let attempts = 0
 
+    let old: string | null
+
     while (attempts < maxAttempts) {
       try {
         const start = new Date().valueOf()
         const { current, ssids } = (await this.apiService.getServer(timeout)).wifi
+        old = current
         const end = new Date().valueOf()
         if (current === ssid) {
           this.serverModel.update({ wifi: { current, ssids } })
@@ -53,7 +57,13 @@ export class WifiService {
     }
 
     if (this.serverModel.peek().wifi.current === ssid) {
-      return true
+      let message = 'Note. It may take a few minutes for your Embassy to reconnect over Tor. If it does not reconnect after 5 minutes, please unplug the device and plug it back in. You may also need to hard refresh your browser cache.'
+      const alert = await this.alertCtrl.create({
+        header: `Connected to "${ssid}"`,
+        message: old ? message : 'You may now unplug your Embassy from Ethernet.<br /></br />' + message,
+        buttons: ['OK'],
+      })
+      await alert.present()
     } else {
       const toast = await this.toastCtrl.create({
         header: 'Failed to connect:',
