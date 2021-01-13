848e5cd0a2aa3be19d2384282ea5447d44452284
Aaron Greenspan <afg419@gmail.com>
2021-01-13 13:10:40 -0700

    ui: adds developer-notes component

---
+++ ui/src/app/components/install-wizard/developer-notes/developer-notes.component.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { DeveloperNotesComponent } from './developer-notes.component'
+import { IonicModule } from '@ionic/angular'
+import { RouterModule } from '@angular/router'
+import { SharingModule } from 'src/app/modules/sharing.module'
+
+@NgModule({
+  declarations: [
+    DeveloperNotesComponent,
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule.forChild([]),
+    SharingModule,
+  ],
+  exports: [DeveloperNotesComponent],
+})
+export class DeveloperNotesComponentModule { }
