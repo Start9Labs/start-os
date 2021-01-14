588b55a37aa0007e82f45f584ee3305ca4a9839a
Matt Hill <matthewonthemoon@gmail.com>
2021-01-14 13:39:04 -0700

    start adding welcome OS screen

--- ui/src/app/services/sync.notifier.ts
+++ ui/src/app/services/sync.notifier.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core'
-import { ToastController, NavController } from '@ionic/angular'
+import { ConfigService } from 'src/app/services/config.service'
+import { ToastController, NavController, ModalController } from '@ionic/angular'
 import { ServerModel, S9Server } from '../models/server-model'
+import { OSWelcomePage } from '../modals/os-welcome/os-welcome.page'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SyncNotifier {
   constructor (
+    private readonly config: ConfigService,
     private readonly toastCtrl: ToastController,
+    private readonly modalCtrl: ModalController,
     private readonly navCtrl: NavController,
     private readonly serverModel: ServerModel,
   ) { }
 
-  async handleNotifications (server: Readonly<S9Server>): Promise<void> {
+  async handleSpecial (server: Readonly<S9Server>): Promise<void> {
+    this.handleNotifications(server)
+    this.handleOSWelcome(server)
+  }
+
+  private async handleNotifications (server: Readonly<S9Server>) {
     const count = server.notifications.length
 
     if (!count) { return }
@@ -46,5 +55,20 @@ export class SyncNotifier {
     })
     await toast.present()
     this.serverModel.update(updates)
+  }
+
+  private async handleOSWelcome(server: Readonly<S9Server>) {
+    if (server.welcomeSeen || server.versionInstalled !== this.config.version) return
+
+    const modal = await this.modalCtrl.create({
+      backdropDismiss: false,
+      component: OSWelcomePage,
+      presentingElement: await this.modalCtrl.getTop(),
+      componentProps: {
+        version: server.versionInstalled
+      },
+    })
+
+    await modal.present()
   }
 }
