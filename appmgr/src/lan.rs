412bd3ae7acd2a4d9bf81b12b0f2f3de2550437d
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-02-21 16:37:26 -0700

    dbus linker errors

--- appmgr/src/lan.rs
+++ appmgr/src/lan.rs
@@ -19,6 +19,52 @@ pub async fn enable_lan(app_id: &AppId) -> Result<(), Error> {
         .to_owned()
         + ".local";
     println!("{}", lan_address);
-    ctrlc::set_handler(move || {}).expect("Error setting signal handler");
+    let lan_address_ptr =
+        std::ffi::CString::new(lan_address).expect("Could not cast lan address to c string");
+    unsafe {
+        let simple_poll = avahi_sys::avahi_simple_poll_new();
+        let poll = avahi_sys::avahi_simple_poll_get(simple_poll);
+        let err_c = Box::into_raw(Box::new(0)) as *mut i32;
+        let avahi_client = avahi_sys::avahi_client_new(
+            poll,
+            avahi_sys::AvahiClientFlags::AVAHI_CLIENT_NO_FAIL,
+            None,
+            std::ptr::null_mut(),
+            err_c,
+        );
+        let group =
+            avahi_sys::avahi_entry_group_new(avahi_client, Some(noop), std::ptr::null_mut());
+        let hostname = avahi_sys::avahi_client_get_host_name_fqdn(avahi_client);
+        let hostname_len = libc::strlen(hostname);
+        let _ = avahi_sys::avahi_entry_group_add_record(
+            group,
+            avahi_sys::AVAHI_IF_UNSPEC,
+            avahi_sys::AVAHI_PROTO_UNSPEC,
+            avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_USE_MULTICAST
+                | avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_ALLOW_MULTIPLE,
+            lan_address_ptr.as_ptr(),
+            avahi_sys::AVAHI_DNS_CLASS_IN as u16,
+            avahi_sys::AVAHI_DNS_TYPE_CNAME as u16,
+            avahi_sys::AVAHI_DEFAULT_TTL,
+            hostname.cast(),
+            hostname_len,
+        );
+        avahi_sys::avahi_entry_group_commit(group);
+        ctrlc::set_handler(move || {
+            // please the borrow checker with the below semantics
+            // avahi_sys::avahi_entry_group_free(group);
+            // avahi_sys::avahi_client_free(avahi_client);
+            // drop(Box::from_raw(err_c));
+            std::process::exit(0);
+        })
+        .expect("Error setting signal handler");
+    }
     pending().await
+}
+
+unsafe extern "C" fn noop(
+    _group: *mut avahi_sys::AvahiEntryGroup,
+    _state: avahi_sys::AvahiEntryGroupState,
+    _userdata: *mut core::ffi::c_void,
+) {
 }
