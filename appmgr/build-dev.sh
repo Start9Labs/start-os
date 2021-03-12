be3761b98c509b8fed334d17eea4cebd608db1d2
Aiden McClelland <me@drbonez.dev>
2021-03-12 10:10:39 -0700

    appmgr: only mount binds if installed also improve build scripts

--- appmgr/build-dev.sh
+++ appmgr/build-dev.sh
@@ -3,6 +3,11 @@
 set -e
 shopt -s expand_aliases
 
+if [ "$0" != "./build-dev.sh" ]; then
+	>&2 echo "Must be run from appmgr directory"
+	exit 1
+fi
+
 alias 'rust-arm-builder'='docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-arm-cross:latest'
 
 cd ..
