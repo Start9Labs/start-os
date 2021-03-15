782e7ed5f8730cae228aac4d32c6c0083d366934
Kogen Mantis <k0gen@pm.me>
2021-03-15 20:36:08 +0000

    Adding BuildGuide branch for the ease of use

--- make_image.sh
+++ make_image.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+arch=$(uname -m)
+if [[ $arch == armv7l ]]; then
+    dev_target="target"
+else 
+    dev_target="target/armv7-unknown-linux-musleabihf"
+fi
 mv buster.img embassy.img
 product_key=$(cat product_key)
 loopdev=$(losetup -f -P embassy.img --show)
@@ -9,6 +14,12 @@ mkdir -p "${root_mountpoint}"
 mkdir -p "${boot_mountpoint}"
 mount "${loopdev}p2" "${root_mountpoint}"
 mount "${loopdev}p1" "${boot_mountpoint}"
+mkdir -p "${root_mountpoint}/root/agent"
+mkdir -p "${root_mountpoint}/etc/docker"
+mkdir -p "${root_mountpoint}/home/pi/.ssh"
+echo -n "" > "${root_mountpoint}/home/pi/.ssh/authorized_keys"
+chown -R pi:pi "${root_mountpoint}/home/pi/.ssh"
+echo -n "" > "${boot_mountpoint}/ssh"
 echo "${product_key}" > "${root_mountpoint}/root/agent/product_key"
 echo -n "start9-" > "${root_mountpoint}/etc/hostname"
 echo -n "${product_key}" | shasum -t -a 256 | cut -c1-8 >> "${root_mountpoint}/etc/hostname"
@@ -18,20 +29,23 @@ echo -n "${product_key}" | shasum -t -a 256 | cut -c1-8 >> "${root_mountpoint}/e
 mv "${root_mountpoint}/etc/hosts.tmp" "${root_mountpoint}/etc/hosts"
 cp agent/dist/agent "${root_mountpoint}/usr/local/bin/agent"
 chmod 700 "${root_mountpoint}/usr/local/bin/agent"
-cp appmgr/target/armv7-unknown-linux-musleabihf/release/appmgr "${root_mountpoint}/usr/local/bin/appmgr"
+cp "appmgr/${dev_target}/release/appmgr" "${root_mountpoint}/usr/local/bin/appmgr"
 chmod 700 "${root_mountpoint}/usr/local/bin/appmgr"
-cp lifeline/target/armv7-unknown-linux-musleabihf/release/lifeline "${root_mountpoint}/usr/local/bin/lifeline"
+cp "lifeline/${dev_target}/release/lifeline" "${root_mountpoint}/usr/local/bin/lifeline"
 chmod 700 "${root_mountpoint}/usr/local/bin/lifeline"
 cp docker-daemon.json "${root_mountpoint}/etc/docker/daemon.json"
 cp setup.sh "${root_mountpoint}/root/setup.sh"
 chmod 700 "${root_mountpoint}/root/setup.sh"
-cp setup.service /etc/systemd/system/setup.service
-cp lifeline/lifeline.service /etc/systemd/system/lifeline.service
-cp agent/config/agent.service /etc/systemd/system/agent.service
-cat "${boot_mountpoint}/config.txt" | grep -v "dtoverlay=pwm-2chan" > "${boot_mountpoint}/config.txt.tmp"
+cp setup.service "${root_mountpoint}/etc/systemd/system/setup.service"
+ln -s  /etc/systemd/system/setup.service "${root_mountpoint}/etc/systemd/system/getty.target.wants/setup.service"
+cp lifeline/lifeline.service "${root_mountpoint}/etc/systemd/system/lifeline.service"
+cp agent/config/agent.service "${root_mountpoint}/etc/systemd/system/agent.service"
+cat "${boot_mountpoint}/config.txt" | grep -v "dtoverlay=" > "${boot_mountpoint}/config.txt.tmp"
 echo "dtoverlay=pwm-2chan" >> "${boot_mountpoint}/config.txt.tmp"
+mv "${boot_mountpoint}/config.txt.tmp" "${boot_mountpoint}/config.txt"
 umount "${root_mountpoint}"
 rm -r "${root_mountpoint}"
 umount "${boot_mountpoint}"
 rm -r "${boot_mountpoint}"
 losetup -d ${loopdev}
+echo "DONE! Here is your EmbassyOS key: ${product_key}"
--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-apt update
-apt install -y libsecp256k1-0
-apt install -y tor
-apt install -y docker.io
-apt install -y iotop
-apt install -y bmon
-apt autoremove -y
 mkdir -p /root/volumes
 mkdir -p /root/tmp/appmgr
 mkdir -p /root/agent
 mkdir -p /root/appmgr/tor
+apt-get update -y
+apt-get install -y tor
+apt-get install -y iotop
+apt-get install -y bmon
+apt-get install -y libavahi-client3
+apt-get install -y libsecp256k1-0
+apt-get install -y docker.io needrestart-
+mv /root/setup.sh /root/setup-s1.sh.done
+cat <<EOT >> /root/setup-s2.sh
+#!/bin/bash
+apt-get update -y
+apt-get install -y tor
+apt-get install -y iotop
+apt-get install -y bmon
+apt-get install -y libavahi-client3
+apt-get install -y libsecp256k1-0
+apt-get install -y docker.io needrestart-
+apt-get autoremove -y
 systemctl enable lifeline
 systemctl enable agent
 systemctl enable ssh
@@ -17,5 +28,8 @@ systemctl enable avahi-daemon
 passwd -l root
 passwd -l pi
 sync
-systemctl disable setup.service
+systemctl disable setup
+mv /root/setup-s2.sh /root/setup-s2.sh.done
 reboot
+EOT
+chmod +x /root/setup-s2.sh
