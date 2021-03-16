6187148a9a1a8930ece30458f2cd5decfda62720
Chris Guida <chrisguida@gmail.com>
2021-03-16 15:46:20 -0600

    add nginx refresh to migration script

--- appmgr/src/version/v0_2_11.rs
+++ appmgr/src/version/v0_2_11.rs
@@ -13,6 +13,22 @@ impl VersionT for Version {
         &V0_2_11
     }
     async fn up(&self) -> Result<(), Error> {
+        crate::tor::write_lan_services(
+            &crate::tor::services_map(&PersistencePath::from_ref(crate::SERVICES_YAML)).await?,
+        )
+        .await?;
+        let svc_exit = std::process::Command::new("service")
+            .args(&["nginx", "reload"])
+            .status()?;
+        crate::ensure_code!(
+            svc_exit.success(),
+            crate::error::GENERAL_ERROR,
+            "Failed to Reload Nginx: {}",
+            svc_exit
+                .code()
+                .or_else(|| { svc_exit.signal().map(|a| 128 + a) })
+                .unwrap_or(0)
+        );
         Ok(())
     }
     async fn down(&self) -> Result<(), Error> {
