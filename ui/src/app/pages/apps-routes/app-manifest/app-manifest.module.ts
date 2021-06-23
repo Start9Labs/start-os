a350e759c7c592c08324e7ab9423eb20cf86bcf3
Matt Hill <matthewonthemoon@gmail.com>
2021-06-22 18:53:20 -0600

    fix manifest page

--- ui/src/app/pages/apps-routes/app-manifest/app-manifest.module.ts
+++ ui/src/app/pages/apps-routes/app-manifest/app-manifest.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular'
 import { AppManifestPage } from './app-manifest.page'
 import { PwaBackComponentModule } from 'src/app/components/pwa-back-button/pwa-back.component.module'
 import { SharingModule } from 'src/app/modules/sharing.module'
+import { FormsModule } from '@angular/forms'
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     IonicModule,
+    FormsModule,
     RouterModule.forChild(routes),
     PwaBackComponentModule,
     SharingModule,
