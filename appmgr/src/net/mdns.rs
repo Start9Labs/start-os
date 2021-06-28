172ef71c8db68d013f05fa155ad41d5c7995f207
Aiden McClelland <me@drbonez.dev>
2021-06-28 14:07:14 -0600

    appmgr: mdns controller

--- appmgr/src/net/mdns.rs
+++ appmgr/src/net/mdns.rs
@@ -1,93 +1,148 @@
-use avahi_sys;
-use futures::future::pending;
+use avahi_sys::{
+    self, avahi_client_free, avahi_entry_group_commit, avahi_entry_group_free,
+    avahi_entry_group_reset, avahi_free, AvahiClient, AvahiEntryGroup,
+};
+use libc::c_void;
 use patch_db::{DbHandle, OptionModel};
+use tokio::sync::RwLock;
 
-use crate::db::model::{InterfaceAddressesModel, InterfaceInfoModel};
 use crate::util::Apply;
 use crate::Error;
 
-pub async fn enable_lan<Db: DbHandle>(mut db: Db) -> Result<LanHandle, Error> {
-    unsafe {
-        // let app_list = crate::apps::list_info().await?;
+const HOSTNAME_LEN: usize = 1 + 15 + 1 + 5; // leading byte, main address, dot, "local"
 
-        let simple_poll = avahi_sys::avahi_simple_poll_new();
-        let poll = avahi_sys::avahi_simple_poll_get(simple_poll);
-        let mut stack_err = 0;
-        let err_c: *mut i32 = &mut stack_err;
-        let avahi_client = avahi_sys::avahi_client_new(
-            poll,
-            avahi_sys::AvahiClientFlags::AVAHI_CLIENT_NO_FAIL,
-            None,
-            std::ptr::null_mut(),
-            err_c,
-        );
-        let group =
-            avahi_sys::avahi_entry_group_new(avahi_client, Some(noop), std::ptr::null_mut());
-        let hostname_raw = avahi_sys::avahi_client_get_host_name_fqdn(avahi_client);
-        let hostname_bytes = std::ffi::CStr::from_ptr(hostname_raw).to_bytes_with_nul();
-        const HOSTNAME_LEN: usize = 1 + 15 + 1 + 5; // leading byte, main address, dot, "local"
-        debug_assert_eq!(hostname_bytes.len(), HOSTNAME_LEN);
-        let mut hostname_buf = [0; HOSTNAME_LEN + 1];
-        hostname_buf[1..].copy_from_slice(hostname_bytes);
-        // assume fixed length prefix on hostname due to local address
-        hostname_buf[0] = 15; // set the prefix length to 15 for the main address
-        hostname_buf[16] = 5; // set the prefix length to 5 for "local"
+pub struct MdnsController(RwLock<MdnsControllerInner>);
+impl MdnsController {
+    pub async fn init<Db: DbHandle>(db: &mut Db) -> Result<Self, Error> {
+        Ok(MdnsController(RwLock::new(
+            MdnsControllerInner::init(db).await?,
+        )))
+    }
+    pub async fn sync<Db: DbHandle>(&self, db: &mut Db) -> Result<(), Error> {
+        self.0.write().await.sync(db).await
+    }
+}
 
-        for app_id in crate::db::DatabaseModel::new()
-            .package_data()
-            .keys(&mut db)
-            .await?
-        {
-            let iface_model = if let Some(model) = crate::db::DatabaseModel::new()
+pub struct MdnsControllerInner {
+    hostname: [u8; HOSTNAME_LEN + 1],
+    client: *mut AvahiClient,
+    entry_group: *mut AvahiEntryGroup,
+}
+unsafe impl Send for MdnsControllerInner {}
+unsafe impl Sync for MdnsControllerInner {}
+
+impl MdnsControllerInner {
+    async fn load_services<Db: DbHandle>(&mut self, db: &mut Db) -> Result<(), Error> {
+        unsafe {
+            for app_id in crate::db::DatabaseModel::new()
                 .package_data()
-                .idx_model(&app_id)
-                .expect(&mut db)
-                .await?
-                .installed()
-                .map(|i| i.interface_info().addresses())
-                .apply(OptionModel::from)
-                .check(&mut db)
+                .keys(db)
                 .await?
             {
-                model
-            } else {
-                continue;
-            };
-            for iface in iface_model.keys(&mut db).await? {
-                let lan_address = if let Some(addr) = iface_model
-                    .clone()
-                    .idx_model(&iface)
-                    .expect(&mut db)
+                let iface_model = if let Some(model) = crate::db::DatabaseModel::new()
+                    .package_data()
+                    .idx_model(&app_id)
+                    .expect(db)
                     .await?
-                    .lan_address()
-                    .get(&mut db)
+                    .installed()
+                    .map(|i| i.interface_info().addresses())
+                    .apply(OptionModel::from)
+                    .check(db)
                     .await?
-                    .to_owned()
                 {
-                    addr
+                    model
                 } else {
                     continue;
                 };
-                let lan_address_ptr = std::ffi::CString::new(lan_address)
-                    .expect("Could not cast lan address to c string");
-                let _ = avahi_sys::avahi_entry_group_add_record(
-                    group,
-                    avahi_sys::AVAHI_IF_UNSPEC,
-                    avahi_sys::AVAHI_PROTO_UNSPEC,
-                    avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_USE_MULTICAST
-                        | avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_ALLOW_MULTIPLE,
-                    lan_address_ptr.as_ptr(),
-                    avahi_sys::AVAHI_DNS_CLASS_IN as u16,
-                    avahi_sys::AVAHI_DNS_TYPE_CNAME as u16,
-                    avahi_sys::AVAHI_DEFAULT_TTL,
-                    hostname_buf.as_ptr().cast(),
-                    hostname_buf.len(),
-                );
-                log::info!("Published {:?}", lan_address_ptr);
+                for iface in iface_model.keys(db).await? {
+                    let lan_address = if let Some(addr) = iface_model
+                        .clone()
+                        .idx_model(&iface)
+                        .expect(db)
+                        .await?
+                        .lan_address()
+                        .get(db)
+                        .await?
+                        .to_owned()
+                    {
+                        addr
+                    } else {
+                        continue;
+                    };
+                    let lan_address_ptr = std::ffi::CString::new(lan_address)
+                        .expect("Could not cast lan address to c string");
+                    let _ = avahi_sys::avahi_entry_group_add_record(
+                        self.entry_group,
+                        avahi_sys::AVAHI_IF_UNSPEC,
+                        avahi_sys::AVAHI_PROTO_UNSPEC,
+                        avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_USE_MULTICAST
+                            | avahi_sys::AvahiPublishFlags_AVAHI_PUBLISH_ALLOW_MULTIPLE,
+                        lan_address_ptr.as_ptr(),
+                        avahi_sys::AVAHI_DNS_CLASS_IN as u16,
+                        avahi_sys::AVAHI_DNS_TYPE_CNAME as u16,
+                        avahi_sys::AVAHI_DEFAULT_TTL,
+                        self.hostname.as_ptr().cast(),
+                        self.hostname.len(),
+                    );
+                    log::info!("Published {:?}", lan_address_ptr);
+                }
             }
         }
-        avahi_sys::avahi_entry_group_commit(group);
-        Ok(LanHandle(group))
+        Ok(())
+    }
+    async fn init<Db: DbHandle>(db: &mut Db) -> Result<Self, Error> {
+        unsafe {
+            // let app_list = crate::apps::list_info().await?;
+
+            let simple_poll = avahi_sys::avahi_simple_poll_new();
+            let poll = avahi_sys::avahi_simple_poll_get(simple_poll);
+            let mut stack_err = 0;
+            let err_c: *mut i32 = &mut stack_err;
+            let avahi_client = avahi_sys::avahi_client_new(
+                poll,
+                avahi_sys::AvahiClientFlags::AVAHI_CLIENT_NO_FAIL,
+                None,
+                std::ptr::null_mut(),
+                err_c,
+            );
+            let group =
+                avahi_sys::avahi_entry_group_new(avahi_client, Some(noop), std::ptr::null_mut());
+            let mut hostname_buf = [0; HOSTNAME_LEN + 1];
+            {
+                let hostname_raw = avahi_sys::avahi_client_get_host_name_fqdn(avahi_client);
+                hostname_buf[1..]
+                    .copy_from_slice(std::ffi::CStr::from_ptr(hostname_raw).to_bytes_with_nul());
+                avahi_free(hostname_raw as *mut c_void);
+            }
+            // assume fixed length prefix on hostname due to local address
+            hostname_buf[0] = 15; // set the prefix length to 15 for the main address
+            hostname_buf[16] = 5; // set the prefix length to 5 for "local"
+
+            let mut ctrl = MdnsControllerInner {
+                hostname: hostname_buf,
+                client: avahi_client,
+                entry_group: group,
+            };
+            avahi_entry_group_commit(group);
+            ctrl.load_services(db).await?;
+            Ok(ctrl)
+        }
+    }
+    async fn sync<Db: DbHandle>(&mut self, db: &mut Db) -> Result<(), Error> {
+        unsafe {
+            avahi_entry_group_reset(self.entry_group);
+            self.load_services(db).await?;
+            avahi_entry_group_commit(self.entry_group);
+        }
+        Ok(())
+    }
+}
+impl Drop for MdnsControllerInner {
+    fn drop(&mut self) {
+        unsafe {
+            avahi_entry_group_free(self.entry_group);
+            avahi_client_free(self.client);
+        }
     }
 }
 
@@ -97,15 +152,3 @@ unsafe extern "C" fn noop(
     _userdata: *mut core::ffi::c_void,
 ) {
 }
-
-pub struct LanHandle(*mut avahi_sys::AvahiEntryGroup);
-impl Drop for LanHandle {
-    fn drop(&mut self) {
-        unsafe {
-            avahi_sys::avahi_entry_group_reset(self.0);
-            avahi_sys::avahi_entry_group_free(self.0);
-        }
-    }
-}
-unsafe impl Send for LanHandle {}
-unsafe impl Sync for LanHandle {}
