517251d6722f9ebda7cec9e642f59636f6a2bee5
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-07-07 14:44:55 -0600

    mo style

--- setup-wizard/src/app/app-routing.module.ts
+++ setup-wizard/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { CanActivateHome, CanActivateRecover } from './guards/guards'
 const routes: Routes = [
   {
     path: 'wizard',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [CanActivateHome],
+
   },
   {
     path: 'recover',
-    loadChildren: () => import('./pages/recover/recover.module').then( m => m.RecoverPageModule)
+    loadChildren: () => import('./pages/recover/recover.module').then( m => m.RecoverPageModule),
+    canActivate: [CanActivateRecover]
   },
 ];
 
