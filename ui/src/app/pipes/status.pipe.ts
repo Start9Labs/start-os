96b4108087be2e811fb91f5a66737969be5ea876
Matt Hill <matthewonthemoon@gmail.com>
2021-07-06 21:53:49 -0600

    react to sequnce changes

--- ui/src/app/pipes/status.pipe.ts
+++ ui/src/app/pipes/status.pipe.ts
@@ -1,4 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 import { PatchDbModel } from '../services/patch-db/patch-db.service'
 import { FEStatus, renderPkgStatus } from '../services/pkg-status-rendering.service'
 
@@ -11,9 +13,12 @@ export class StatusPipe implements PipeTransform {
     private readonly patch: PatchDbModel,
   ) { }
 
-  transform (pkgId: string): FEStatus {
-    console.log(pkgId)
-    const pkg = this.patch.data['package-data'][pkgId]
-    return renderPkgStatus(pkg.state, pkg.installed.status).feStatus
+  transform (pkgId: string): Observable<FEStatus> {
+    return this.patch.sequence$.pipe(
+      map(_ => {
+        const pkg = this.patch.data['package-data'][pkgId]
+        return renderPkgStatus(pkg.state, pkg.installed.status).feStatus
+      }),
+    )
   }
 }
