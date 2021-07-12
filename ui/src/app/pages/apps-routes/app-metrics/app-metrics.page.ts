fc506c387f7dd133e21d2a3c1c8a5302abfce2d1
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-07-12 11:39:16 -0600

    pkg metrics

--- ui/src/app/pages/apps-routes/app-metrics/app-metrics.page.ts
+++ ui/src/app/pages/apps-routes/app-metrics/app-metrics.page.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { IonContent } from '@ionic/angular'
+import { Metric } from 'src/app/services/api/api-types'
+import { ApiService } from 'src/app/services/api/api.service'
 import { PackageDataEntry } from 'src/app/services/patch-db/data-model'
 import { PatchDbModel } from 'src/app/services/patch-db/patch-db.service'
+import { pauseFor } from 'src/app/util/misc.util'
 
 @Component({
   selector: 'app-metrics',
@@ -10,18 +13,50 @@ import { PatchDbModel } from 'src/app/services/patch-db/patch-db.service'
   styleUrls: ['./app-metrics.page.scss'],
 })
 export class AppMetricsPage {
+  pkgId: string
   pkg: PackageDataEntry
+  going = false
+  metrics: Metric
 
   @ViewChild(IonContent) content: IonContent
 
   constructor (
     private readonly route: ActivatedRoute,
     private readonly patch: PatchDbModel,
+    private readonly apiService: ApiService,
   ) { }
 
   ngOnInit () {
-    const pkgId = this.route.snapshot.paramMap.get('pkgId')
-    this.pkg = this.patch.data['package-data'][pkgId]
+    this.pkgId = this.route.snapshot.paramMap.get('pkgId')
+    this.pkg = this.patch.data['package-data'][this.pkgId]
+
+    this.startDaemon()
+  }
+
+  ngOnDestroy () {
+    this.stopDaemon()
+  }
+
+  async startDaemon (): Promise<void> {
+    this.going = true
+    while (this.going) {
+      await this.getMetrics()
+      await pauseFor(250)
+    }
+  }
+
+  stopDaemon () {
+    this.going = false
+  }
+
+  async getMetrics (): Promise<void> {
+    try {
+      this.metrics = await this.apiService.getPkgMetrics({ id: this.pkgId})
+    } catch (e) {
+      this.stopDaemon()
+      await pauseFor(1000)
+      this.startDaemon()
+    }
   }
 
   ngAfterViewInit () {
