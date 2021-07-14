7c51117a561d9a978640febb36f7682a4fcbdba6
Matt Hill <matthewonthemoon@gmail.com>
2021-07-14 00:40:36 -0600

    skeleton list component

--- ui/src/app/pages/server-routes/server-metrics/server-metrics.module.ts
+++ ui/src/app/pages/server-routes/server-metrics/server-metrics.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router'
 import { IonicModule } from '@ionic/angular'
 import { ServerMetricsPage } from './server-metrics.page'
 import { PwaBackComponentModule } from 'src/app/components/pwa-back-button/pwa-back.component.module'
-import { TextSpinnerComponentModule } from 'src/app/components/text-spinner/text-spinner.component.module'
+import { SkeletonListComponentModule } from 'src/app/components/skeleton-list/skeleton-list.component.module'
 
 const routes: Routes = [
   {
@@ -19,7 +19,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes),
     PwaBackComponentModule,
-    TextSpinnerComponentModule,
+    SkeletonListComponentModule,
   ],
   declarations: [ServerMetricsPage],
 })
