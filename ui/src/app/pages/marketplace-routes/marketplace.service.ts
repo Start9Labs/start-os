c4a5cf1c511809cb7e65fe82cbc37025685be472
Matt Hill <matthewonthemoon@gmail.com>
2021-07-14 16:43:25 -0600

    rework endpoints and enable setting custom marketplace urls

--- ui/src/app/pages/marketplace-routes/marketplace.service.ts
+++ ui/src/app/pages/marketplace-routes/marketplace.service.ts
@@ -1,20 +1,53 @@
 import { Injectable } from '@angular/core'
-import { AvailableShow } from 'src/app/services/api/api-types'
+import { MarketplacePkg } from 'src/app/services/api/api-types'
 import { ApiService } from 'src/app/services/api/api.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarketplaceService {
-  pkgs: { [id: string]: AvailableShow } = { }
-  additionalInfo
+  pkgs: { [id: string]: MarketplacePkg } = { }
+  releaseNotes: { [id: string]: {
+    [version: string]: string
+  } }
 
   constructor (
     private readonly apiService: ApiService,
   ) { }
 
-  async setPkg (id: string, version?: string): Promise<void> {
-    this.pkgs[id] = await this.apiService.getAvailableShow({ id, version })
+  async getMarketplaceData () {
+    return this.apiService.getMarketplaceData({ })
+  }
+
+  async getEos () {
+    return this.apiService.getEos({ })
+  }
+
+  async getPkgs (category: string, query: string, page: number, perPage: number) : Promise<MarketplacePkg[]> {
+    const pkgs = await this.apiService.getMarketplacePkgs({
+      category: category !== 'all' ? category : undefined,
+      query,
+      page: String(page),
+      'per-page': String(perPage),
+    })
+    this.pkgs = pkgs.reduce((cur, val) => {
+      cur[val.manifest.id] = val
+      return cur
+    }, { })
+    return pkgs
+  }
+
+  async getPkg (id: string, version?: string): Promise<void> {
+    const pkg = (await this.apiService.getMarketplacePkgs({ id, version }))[0]
+    if (pkg) {
+      this.pkgs[id] = pkg
+    } else {
+      throw new Error(`No results for ${id}${version ? ' ' + version : ''}.`)
+    }
+  }
+
+  async getReleaseNotes (id: string): Promise<void> {
+    this.releaseNotes[id] = await this.apiService.getReleaseNotes({ id })
   }
 }
 
