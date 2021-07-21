24ccdf800e97382eb60b651fce85c3ce93585e36
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-07-21 10:10:21 -0600

    progress

--- ui/src/app/services/api/embassy/embassy-mock-api.service.ts
+++ ui/src/app/services/api/embassy/embassy-mock-api.service.ts
@@ -504,7 +504,7 @@ export class MockApiService extends ApiService {
       while (i < initialProgress.size) {
         console.log(i)
         await pauseFor(1000)
-        i = Math.min(i + 40, initialProgress.size)
+        i = Math.min(i + 5, initialProgress.size)
         initialProgress[phase.progress] = i
         if (i === initialProgress.size) {
           initialProgress[phase.completion] = true
