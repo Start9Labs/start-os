e8e7457cf244297553222d2d6abf51dff772c00a
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-08-10 10:35:59 -0600

    complete flow

---
+++ setup-wizard/src/app/pages/loading/loading.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { LoadingPage } from './loading.page';
+import { PasswordPageModule } from '../password/password.module';
+import { LoadingPageRoutingModule } from './loading-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoadingPageRoutingModule,
+    PasswordPageModule,
+  ],
+  declarations: [LoadingPage]
+})
+export class LoadingPageModule {}
---
+++ setup-wizard/src/app/pages/loading/loading.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core'
+import { AlertController, NavController } from '@ionic/angular'
+import { BehaviorSubject } from 'rxjs'
+import { StateService } from 'src/app/services/state.service'
+
+@Component({
+  selector: 'loading',
+  templateUrl: 'loading.page.html',
+  styleUrls: ['loading.page.scss'],
+})
+export class LoadingPage {
+  constructor(
+    private stateService: StateService,
+    private alertCtrl: AlertController
+  ) {}
+
+  ngOnInit () {
+    this.stateService.pollDataTransferProgress()
+    const progSub = this.stateService.dataProgSubject.subscribe(async progress => {
+      if(progress === 1) {
+        await this.successAlert()
+        progSub.unsubscribe()
+      }
+    })
+  }
+
+  async successAlert () {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'success-alert',
+      header: 'Success!',
+      subHeader: `Your Embassy is set up and ready to go.`,
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Go To Embassy',
+          handler: () => {
+            window.location.reload()
+          }
+        }
+      ]
+    })
+    await alert.present()
+  }
+}
+
