45c46036578056e77153e8b5de416ab304057719
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-08-23 10:32:07 -0600

    errs fixed

--- ui/src/app/pages/server-routes/server-logs/server-logs.page.ts
+++ ui/src/app/pages/server-routes/server-logs/server-logs.page.ts
@@ -30,12 +30,6 @@ export class ServerLogsPage {
     try {
       // get logs
       const logs = await this.embassyApi.getServerLogs({ before: this.before, limit })
-
-      if (logs.length < limit) {
-        this.needInfinite = false
-        return
-      }
-
       this.before = logs[0].timestamp
 
       this.firstTimeLoaded = true
@@ -43,13 +37,18 @@ export class ServerLogsPage {
       const container = document.getElementById('container')
       const beforeContainerHeight = container.scrollHeight
       const newLogs = document.getElementById('template').cloneNode(true) as HTMLElement
-      newLogs.innerHTML = logs.map(l => `${l.timestamp} ${l.log}`).join('\n\n') + '\n\n'
+      newLogs.innerHTML = logs.map(l => `${l.timestamp} ${l.log}`).join('\n\n') + (logs.length ? '\n\n' : '')
       container.prepend(newLogs)
       const afterContainerHeight = container.scrollHeight
 
       // scroll down
       scrollBy(0, afterContainerHeight - beforeContainerHeight)
       this.content.scrollToPoint(0, afterContainerHeight - beforeContainerHeight)
+
+      if (logs.length < limit) {
+        this.needInfinite = false
+      }
+
     } catch (e) {
       this.errToast.present(e)
     }
