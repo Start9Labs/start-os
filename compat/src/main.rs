8562ccd164c45717fafa7699755ed34ed297639e
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2021-08-24 22:49:58 -0600

    wip

--- compat/src/main.rs
+++ compat/src/main.rs
@@ -1,28 +1,52 @@
 use std::{fs::File, io::stdout, path::Path};
 
 mod backup;
+use anyhow::anyhow;
 use backup::{create_backup, restore_backup};
 use clap::{App, Arg, SubCommand};
 use embassy::config::action::{ConfigRes, SetResult};
 use embassy::Error;
 use futures::future::FutureExt;
 
-fn main() {
-    let app = App::new("compat").subcommand(
-        SubCommand::with_name("config").subcommand(
-            SubCommand::with_name("get")
-                .arg(
-                    Arg::with_name("mountpoint")
-                        .help("The `mount` field from manifest.yaml")
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("spec")
-                        .help("The path to the config spec in the container")
-                        .required(true),
-                ),
-        ),
-    );
+async fn main() {
+    let app = App::new("compat")
+        .subcommand(
+            SubCommand::with_name("config").subcommand(
+                SubCommand::with_name("get")
+                    .arg(
+                        Arg::with_name("mountpoint")
+                            .help("The `mount` field from manifest.yaml")
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("spec")
+                            .help("The path to the config spec in the container")
+                            .required(true),
+                    ),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("duplicity").subcommand(
+                SubCommand::with_name("create")
+                    .arg(
+                        Arg::with_name("package-id")
+                            .help("The `id` field from the manifest file")
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("mountpoint")
+                            .help(
+                                "The backups mount point in the format: file:///<mount-point-path>",
+                            )
+                            .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("datapath")
+                            .help("The path to the data to be backed up in the container")
+                            .required(true),
+                    ),
+            ),
+        );
     let matches = app.get_matches();
     match matches.subcommand() {
         ("config", Some(sub_m)) => match sub_m.subcommand() {
@@ -58,26 +82,26 @@ fn main() {
         },
         ("duplicity", Some(sub_m)) => match sub_m.subcommand() {
             ("create", Some(sub_m)) => {
-                serde_yaml::to_writer(stdout(), &create_backup(
-                    sub_m.value_of("mountpoint").unwrap(),
-                    sub_m.value_of("datapath").unwrap(),
-                    sub_m.value_of("package-id").unwrap(),
-                )
-                .await
-                .unwrap()).unwrap();
+                let res = create_backup(
+                        sub_m.value_of("mountpoint").unwrap(),
+                        sub_m.value_of("datapath").unwrap(),
+                        sub_m.value_of("package-id").unwrap(),
+                    ).await;
+                match res {
+                    Ok(r) => serde_yaml::to_writer(stdout(), &r).unwrap(),
+                    Err(e) => Err(e),
+                };
             },
             ("restore", Some(sub_m)) => {
-                let res = async { restore_backup(
-                    sub_m.value_of("package-id").unwrap(),
-                    sub_m.value_of("datapath").unwrap(),
-                    sub_m.value_of("mountpoint").unwrap(),
-                ).await;
+                restore_backup(
+                        sub_m.value_of("package-id").unwrap(),
+                        sub_m.value_of("datapath").unwrap(),
+                        sub_m.value_of("mountpoint").unwrap(),
+                    ).await;
             }
-                serde_yaml::to_writer(stdout(), res).unwrap();
-            },
             (subcmd, _) => {
                 panic!("unknown subcommand: {}", subcmd);
-            },
+            }
         },
         (subcmd, _) => {
             panic!("unknown subcommand: {}", subcmd);
