dec707d69e074b2414cb2984bc335e549c2bd5c4
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2021-09-01 15:27:18 -0600

    wip update set result

--- compat/src/config/mod.rs
+++ compat/src/config/mod.rs
@@ -2,27 +2,25 @@ use std::borrow::Cow;
 use std::path::Path;
 use std::time::Duration;
 
-use failure::ResultExt as _;
+use embassy::config::action::SetResult;
 use futures::future::{BoxFuture, FutureExt};
 use itertools::Itertools;
 use linear_map::{set::LinearSet, LinearMap};
-use rand::SeedableRng;
 use regex::Regex;
 
-use crate::dependencies::{DependencyError, TaggedDependencyError};
-use crate::util::PersistencePath;
-use crate::util::{from_yaml_async_reader, to_yaml_async_writer};
-use crate::ResultExt as _;
+use embassy::dependencies::{DependencyError, TaggedDependencyError};
+use embassy::util::{from_yaml_async_reader, to_yaml_async_writer};
+use embassy::ResultExt as _;
 
 pub mod rules;
-pub mod spec;
 pub mod util;
 pub mod value;
 
 pub use rules::{ConfigRuleEntry, ConfigRuleEntryWithSuggestions};
-pub use spec::{ConfigSpec, Defaultable};
 use util::NumRange;
 pub use value::Config;
+
+use embassy::config::spec;
 
 #[derive(Debug, Fail)]
 pub enum ConfigurationError {
@@ -108,220 +106,43 @@ pub enum MatchError {
     ListUniquenessViolation,
 }
 
-#[derive(Clone, Debug, Default, serde::Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct ConfigurationRes {
-    pub changed: LinearMap<String, Config>,
-    pub needs_restart: LinearSet<String>,
-    pub stopped: LinearMap<String, TaggedDependencyError>,
-}
-
-// returns apps with changed configurations
-pub async fn configure(
+pub async fn set_configuration(
     name: &str,
-    config: Option<Config>,
-    timeout: Option<Duration>,
-    dry_run: bool,
-) -> Result<ConfigurationRes, crate::Error> {
-    async fn handle_broken_dependent(
-        name: &str,
-        dependent: String,
-        dry_run: bool,
-        res: &mut ConfigurationRes,
-        error: DependencyError,
-    ) -> Result<(), crate::Error> {
-        crate::control::stop_dependents(
-            &dependent,
-            dry_run,
-            DependencyError::NotRunning,
-            &mut res.stopped,
-        )
-        .await?;
-        if crate::apps::status(&dependent, false).await?.status
-            != crate::apps::DockerStatus::Stopped
-        {
-            crate::control::stop_app(&dependent, false, dry_run).await?;
-            res.stopped.insert(
-                // TODO: maybe don't do this if its not running
-                dependent,
-                TaggedDependencyError {
-                    dependency: name.to_owned(),
-                    error,
-                },
-            );
-        }
-        Ok(())
-    }
+    config: Config,
+) -> Result<SetResult, crate::Error> {
     fn configure_rec<'a>(
         name: &'a str,
-        config: Option<Config>,
-        timeout: Option<Duration>,
-        dry_run: bool,
-        res: &'a mut ConfigurationRes,
+        config: Config,
+        res: &'a mut SetResult,
     ) -> BoxFuture<'a, Result<Config, crate::Error>> {
         async move {
-            let info = crate::apps::list_info()
-                .await?
-                .remove(name)
-                .ok_or_else(|| failure::format_err!("{} is not installed", name))
-                .with_code(crate::error::NOT_FOUND)?;
-            let mut rng = rand::rngs::StdRng::from_entropy();
-            let spec_path = PersistencePath::from_ref("apps")
-                .join(name)
-                .join("config_spec.yaml");
-            let rules_path = PersistencePath::from_ref("apps")
+            let rules_path = Path::new("apps")
                 .join(name)
                 .join("config_rules.yaml");
-            let config_path = PersistencePath::from_ref("apps")
+            let config_path = Path::new("apps")
                 .join(name)
                 .join("config.yaml");
-            let spec: ConfigSpec =
-                from_yaml_async_reader(&mut *spec_path.read(false).await?).await?;
+
             let rules: Vec<ConfigRuleEntry> =
-                from_yaml_async_reader(&mut *rules_path.read(false).await?).await?;
-            let old_config: Option<Config> =
-                if let Some(mut f) = config_path.maybe_read(false).await.transpose()? {
-                    Some(from_yaml_async_reader(&mut *f).await?)
-                } else {
-                    None
-                };
-            let mut config = if let Some(cfg) = config {
-                cfg
-            } else {
-                if let Some(old) = &old_config {
-                    old.clone()
-                } else {
-                    spec.gen(&mut rng, &timeout)
-                        .with_code(crate::error::CFG_SPEC_VIOLATION)?
-                }
-            };
-            spec.matches(&config)
-                .with_code(crate::error::CFG_SPEC_VIOLATION)?;
-            spec.update(&mut config)
-                .await
-                .with_code(crate::error::CFG_SPEC_VIOLATION)?;
+                from_yaml_async_reader(&mut tokio::fs::File::open(rules_path).await?).await?;
+
             let mut cfgs = LinearMap::new();
             cfgs.insert(name, Cow::Borrowed(&config));
             for rule in rules {
                 rule.check(&config, &cfgs)
-                    .with_code(crate::error::CFG_RULES_VIOLATION)?;
+                    .with_kind(embassy::ErrorKind::ConfigRulesViolation)?;
             }
-            match old_config {
-                Some(old) if &old == &config && info.configured && !info.recoverable => {
-                    return Ok(config)
-                }
-                _ => (),
-            };
-            res.changed.insert(name.to_owned(), config.clone());
-            for dependent in crate::apps::dependents(name, false).await? {
-                match configure_rec(&dependent, None, timeout, dry_run, res).await {
-                    Ok(dependent_config) => {
-                        let man = crate::apps::manifest(&dependent).await?;
-                        if let Some(dep_info) = man.dependencies.0.get(name) {
-                            match dep_info
-                                .satisfied(
-                                    name,
-                                    Some(config.clone()),
-                                    &dependent,
-                                    &dependent_config,
-                                )
-                                .await?
-                            {
-                                Ok(_) => (),
-                                Err(e) => {
-                                    handle_broken_dependent(name, dependent, dry_run, res, e)
-                                        .await?;
-                                }
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        if e.code == Some(crate::error::CFG_RULES_VIOLATION)
-                            || e.code == Some(crate::error::CFG_SPEC_VIOLATION)
-                        {
-                            if !dry_run {
-                                crate::apps::set_configured(&dependent, false).await?;
-                            }
-                            handle_broken_dependent(
-                                name,
-                                dependent,
-                                dry_run,
-                                res,
-                                DependencyError::PointerUpdateError(format!("{}", e)),
-                            )
-                            .await?;
-                        } else {
-                            handle_broken_dependent(
-                                name,
-                                dependent,
-                                dry_run,
-                                res,
-                                DependencyError::Other(format!("{}", e)),
-                            )
-                            .await?;
-                        }
-                    }
-                }
-            }
-            if !dry_run {
-                let mut file = config_path.write(None).await?;
-                to_yaml_async_writer(file.as_mut(), &config).await?;
-                file.commit().await?;
-                let volume_config = Path::new(crate::VOLUMES)
-                    .join(name)
-                    .join("start9")
-                    .join("config.yaml");
-                tokio::fs::copy(config_path.path(), &volume_config)
-                    .await
-                    .with_context(|e| {
-                        format!(
-                            "{}: {} -> {}",
-                            e,
-                            config_path.path().display(),
-                            volume_config.display()
-                        )
-                    })
-                    .with_code(crate::error::FILESYSTEM_ERROR)?;
-                crate::apps::set_configured(name, true).await?;
-                crate::apps::set_recoverable(name, false).await?;
-            }
-            if crate::apps::status(name, false).await?.status != crate::apps::DockerStatus::Stopped
-            {
-                if !dry_run {
-                    crate::apps::set_needs_restart(name, true).await?;
-                }
-                res.needs_restart.insert(name.to_string());
-            }
+
+            // res.depends_on.new();
+            
             Ok(config)
         }
         .boxed()
     }
-    let mut res = ConfigurationRes::default();
-    configure_rec(name, config, timeout, dry_run, &mut res).await?;
-    Ok(res)
-}
-
-pub async fn remove(name: &str) -> Result<(), crate::Error> {
-    let config_path = PersistencePath::from_ref("apps")
-        .join(name)
-        .join("config.yaml")
-        .path();
-    if config_path.exists() {
-        tokio::fs::remove_file(&config_path)
-            .await
-            .with_context(|e| format!("{}: {}", e, config_path.display()))
-            .with_code(crate::error::FILESYSTEM_ERROR)?;
-    }
-    let volume_config = Path::new(crate::VOLUMES)
-        .join(name)
-        .join("start9")
-        .join("config.yaml");
-    if volume_config.exists() {
-        tokio::fs::remove_file(&volume_config)
-            .await
-            .with_context(|e| format!("{}: {}", e, volume_config.display()))
-            .with_code(crate::error::FILESYSTEM_ERROR)?;
-    }
-    crate::apps::set_configured(name, false).await?;
-    Ok(())
+    let mut res = SetResult {
+        depends_on: indexmap::IndexMap::new(),
+        signal: None
+    };
+    configure_rec(name, config, &mut res).await?;
+    // Ok(res);
 }
