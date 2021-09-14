e29a91fbde59bb4d0011dbc4f0169f8833e9f78b
kn0wmad <kn0wmad@protonmail.com>
2021-09-14 11:36:11 -0600

    Minor edit

--- setup/copy.sh
+++ setup/copy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Copy over the built EmbassyOS binaries and systemd services, then create the .ssh directory
+# Enter the appmgr directory, copy over the built EmbassyOS binaries and systemd services, then create the .ssh directory
+cd appmgr/
+
 sudo cp target/aarch64-unknown-linux-gnu/release/embassy-init /mnt/usr/local/bin
 sudo cp target/aarch64-unknown-linux-gnu/release/embassyd /mnt/usr/local/bin
 sudo cp target/aarch64-unknown-linux-gnu/release/embassy-cli /mnt/usr/local/bin
