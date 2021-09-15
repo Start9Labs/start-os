29b4c2781226ab0fcdbf38ec29f1e73b1a1668b6
Matt Hill <matthewonthemoon@gmail.com>
2021-09-14 18:19:00 -0600

    initial commit for diagnostic ui

---
+++ diagnosticUI/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
