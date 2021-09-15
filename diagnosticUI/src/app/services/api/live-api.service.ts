2d301e84eae0cab9a42cb60637901385d97ec084
Matt Hill <matthewonthemoon@gmail.com>
2021-09-15 16:06:33 -0600

    mvp

---
+++ diagnosticUI/src/app/services/api/live-api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core"
+import { HttpService } from "../http.service"
+import { ApiService, GetErrorRes, GetLogsReq, GetLogsRes } from "./api.service"
+
+@Injectable()
+export class LiveApiService extends ApiService {
+
+  constructor (
+    private readonly http: HttpService,
+  ) { super() }
+
+  getError (): Promise<GetErrorRes> {
+    return this.http.rpcRequest<GetErrorRes>({
+      method: 'diagnostic.error',
+      params: { },
+    })
+  }
+
+  restart (): Promise<void> {
+    return this.http.rpcRequest<void>({
+      method: 'diagnostic.restart',
+      params: { },
+    })
+  }
+
+  forgetDrive (): Promise<void> {
+    return this.http.rpcRequest<void>({
+      method: 'diagnostic.forget-disk',
+      params: { },
+    })
+  }
+
+  getLogs (params: GetLogsReq): Promise<GetLogsRes> {
+    return this.http.rpcRequest<GetLogsRes>({
+      method: 'diagnostic.logs',
+      params,
+    })
+  }
+}
---
+++ diagnosticUI/src/app/services/api/mock-api.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core"
+import { pauseFor } from "../../util/misc.util"
+import { ApiService, GetErrorRes, GetLogsReq, GetLogsRes, Log } from "./api.service"
+
+@Injectable()
+export class MockApiService extends ApiService {
+
+  constructor () { super() }
+
+  async getError (): Promise<GetErrorRes> {
+    await pauseFor(1000)
+    return {
+      code: 15,
+      message: 'Unknown Embassy',
+      data: { details: 'Some details about the error here' }
+    }
+  }
+
+  async restart (): Promise<void> {
+    await pauseFor(1000)
+    return null
+  }
+
+  async forgetDrive (): Promise<void> {
+    await pauseFor(1000)
+    return null
+  }
+
+  async getLogs (params: GetLogsReq): Promise<GetLogsRes> {
+    await pauseFor(1000)
+    let entries: Log[]
+    if (Math.random() < .2) {
+      entries = packageLogs
+    } else {
+      const arrLength = params.limit ? Math.ceil(params.limit / packageLogs.length) : 10
+      entries = new Array(arrLength).fill(packageLogs).reduce((acc, val) => acc.concat(val), [])
+    }
+    return {
+      entries,
+      'start-cursor': 'startCursor',
+      'end-cursor': 'endCursor',
+    }
+  }
+}
+
+const packageLogs = [
+  {
+    timestamp: '2019-12-26T14:20:30.872Z',
+    message: '****** START *****',
+  },
+  {
+    timestamp: '2019-12-26T14:21:30.872Z',
+    message: 'ServerLogs ServerLogs ServerLogs ServerLogs ServerLogs',
+  },
+  {
+    timestamp: '2019-12-26T14:22:30.872Z',
+    message: '****** FINISH *****',
+  },
+]
