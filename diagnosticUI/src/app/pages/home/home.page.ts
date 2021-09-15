de50b1fefc3e2e74df86814af95496c546c0ebf0
Matt Hill <matthewonthemoon@gmail.com>
2021-09-15 16:06:33 -0600

    mvp

---
+++ diagnosticUI/src/app/pages/home/home.page.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core'
+import { LoadingController } from '@ionic/angular'
+import { ApiService } from 'src/app/services/api/api.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  error: {
+    code: number
+    problem: string
+    solution: string
+  } = { } as any
+  solutions: string[] = []
+  restarted = false
+  forgotten = false
+
+  constructor (
+    private readonly loadingCtrl: LoadingController,
+    private readonly api: ApiService,
+  ) { }
+
+  async ngOnInit () {
+    try {
+      const error = await this.api.getError()
+      // incorrect drive
+      if (error.code === 15) {
+        this.error = {
+          code: 15,
+          problem: 'Unknown storage drive detected',
+          solution: 'To use a different storage drive, replace the current one and click RESTART EMBASSY below. To use the current storage drive, click USE CURRENT DRIVE below, then follow instructions. No data will be erased during this process.'
+        }
+      // no drive
+      } else if (error.code === 20) {
+        this.error = {
+          code: 20,
+          problem: 'Storage drive not found',
+          solution: 'Insert your EmbassyOS storage drive and click RESTART EMBASSY below.'
+        }
+      // drive corrupted
+      } else if (error.code === 25) {
+        this.error = {
+          code: 25,
+          problem: 'Storage drive corrupted. This could be the result of data corruption or a physical damage.',
+          solution: 'It may or may not be possible to re-use this drive by reformatting and recovering from backup. To enter recovery mode, click ENTER RECOVERY MODE below, then follow instructions. No data will be erased during this step.'
+        }
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async restart (): Promise<void> {
+    const loader = await this.loadingCtrl.create({
+      spinner: 'lines',
+      cssClass: 'loader',
+    })
+    await loader.present()
+
+    try {
+      await this.api.restart()
+      this.restarted = true
+    } catch (e) {
+      console.error(e)
+    } finally {
+      loader.dismiss()
+    }
+  }
+
+  async forgetDrive (): Promise<void> {
+    const loader = await this.loadingCtrl.create({
+      spinner: 'lines',
+      cssClass: 'loader',
+    })
+    await loader.present()
+
+    try {
+      await this.api.forgetDrive()
+      this.forgotten = true
+    } catch (e) {
+      console.error(e)
+    } finally {
+      loader.dismiss()
+    }
+  }
+
+  refreshPage (): void {
+    window.location.reload()
+  }
+}
---
+++ diagnosticUI/src/app/services/http.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+
+  constructor (
+    private readonly http: HttpClient,
+  ) { }
+
+  async rpcRequest<T> (options: RPCOptions): Promise<T> {
+    const res = await this.httpRequest<RPCResponse<T>>(options)
+
+    if (isRpcError(res)) throw new RpcError(res.error)
+
+    if (isRpcSuccess(res)) return res.result
+  }
+
+  async httpRequest<T> (body: RPCOptions): Promise<T> {
+    const url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/rpc/v1`
+    return this.http.post(url, body)
+      .toPromise()
+      .then((res: any) => res.body)
+      .catch(e => { throw new HttpError(e) })
+  }
+}
+
+function RpcError (e: RPCError['error']): void {
+  const { code, message, data } = e
+
+  this.code = code
+  this.message = message
+
+  if (typeof data === 'string') {
+    this.details = e.data
+    this.revision = null
+  } else {
+    this.details = data.details
+  }
+}
+
+function HttpError (e: HttpErrorResponse): void {
+  const { status, statusText } = e
+
+  this.code = status
+  this.message = statusText
+  this.details = null
+  this.revision = null
+}
+
+function isRpcError<Error, Result> (arg: { error: Error } | { result: Result}): arg is { error: Error } {
+  return !!(arg as any).error
+}
+
+function isRpcSuccess<Error, Result> (arg: { error: Error } | { result: Result}): arg is { result: Result } {
+  return !!(arg as any).result
+}
+
+export interface RPCOptions {
+  method: string
+  params: { [param: string]: Params }
+}
+
+export interface RequestError {
+  code: number
+  message: string
+  details: string
+}
+
+export type Params = string | number | boolean | object | string[] | number[]
+
+interface RPCBase {
+  jsonrpc: '2.0'
+  id: string
+}
+
+export interface RPCRequest<T> extends RPCBase {
+  method: string
+  params?: T
+}
+
+export interface RPCSuccess<T> extends RPCBase {
+  result: T
+}
+
+export interface RPCError extends RPCBase {
+  error: {
+    code: number,
+    message: string
+    data?: {
+      details: string
+    } | string
+  }
+}
+
+export type RPCResponse<T> = RPCSuccess<T> | RPCError
+
+type HttpError = HttpErrorResponse & { error: { code: string, message: string } }
