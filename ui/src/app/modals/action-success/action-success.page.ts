8a4698cf79f035893c882674b0b1ef4b83db19fc
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-09-26 15:31:32 -0600

    comments addressed

--- ui/src/app/modals/action-success/action-success.page.ts
+++ ui/src/app/modals/action-success/action-success.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core'
 import { ModalController, ToastController } from '@ionic/angular'
+import { ActionResponse } from 'src/app/services/api/api.types'
 import { copyToClipboard } from 'src/app/util/web.util'
 
 @Component({
@@ -8,12 +9,8 @@ import { copyToClipboard } from 'src/app/util/web.util'
   styleUrls: ['./action-success.page.scss'],
 })
 export class ActionSuccessPage {
-  @Input() version: string
-  @Input() header: string
-  @Input() message: string
-  @Input() value: string
-  @Input() qr: boolean = false
-  @Input() copyable: boolean = false
+
+  @Input() actionRes: ActionResponse
 
   constructor (
     private readonly modalCtrl: ModalController,
