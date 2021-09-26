18a290f502b0845df4b06151341e63078d3dcd5a
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-09-26 15:31:32 -0600

    comments addressed

--- ui/src/app/modals/action-success/action-success.module.ts
+++ ui/src/app/modals/action-success/action-success.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { IonicModule } from '@ionic/angular'
 import { ActionSuccessPage } from './action-success.page'
-import { SharingModule } from 'src/app/modules/sharing.module'
-import { FormsModule } from '@angular/forms'
-import { QRComponentModule } from 'src/app/components/qr/qr.component.module'
 import { QrCodeModule } from 'ng-qrcode'
 
 @NgModule({
@@ -12,8 +9,6 @@ import { QrCodeModule } from 'ng-qrcode'
   imports: [
     CommonModule,
     IonicModule,
-    FormsModule,
-    SharingModule,
     QrCodeModule,
   ],
   exports: [ActionSuccessPage],
