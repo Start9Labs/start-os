64ddb0799f93d6b0a5025daba06fe3ffceaa8c02
kn0wmad <kn0wmad@protonmail.com>
2021-09-27 16:48:40 -0600

    Script/README updates

---
+++ build/filesystems.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Write contents of LOOPDEV (Ubuntu image) to sd card and make filesystems, then detach the loop device
+echo $LOOPDEV
+echo $OUTPUT_DEVICE
+sudo dd if=${LOOPDEV}p1 of=/dev/${OUTPUT_DEVICE}p1 status=progress
+sudo mkfs.vfat /dev/${OUTPUT_DEVICE}p2
+sudo dd if=${LOOPDEV}p2 of=/dev/${OUTPUT_DEVICE}p3 status=progress
+sudo mkfs.ext4 /dev/${OUTPUT_DEVICE}p4
+
+sudo losetup -d $LOOPDEV
+
+# Label the filesystems
+sudo fatlabel /dev/${OUTPUT_DEVICE}p1 system-boot
+sudo fatlabel /dev/${OUTPUT_DEVICE}p2 EMBASSY
+sudo e2label /dev/${OUTPUT_DEVICE}p3 writable
+sudo e2label /dev/${OUTPUT_DEVICE}p4 reserved
+
+# Mount the boot partition and config
+sudo mount /dev/${OUTPUT_DEVICE}p1 /mnt
+
+cat "/mnt/config.txt" | grep -v "dtoverlay=" | sudo tee "/mnt/config.txt.tmp"
+echo "dtoverlay=pwm-2chan" | sudo tee -a "/mnt/config.txt.tmp"
+sudo mv "/mnt/config.txt.tmp" "/mnt/config.txt"
+
+# Unmount the boot partition and mount embassy partition
+sudo umount /mnt
+sudo mount /dev/${OUTPUT_DEVICE}p2 /mnt
