66e7d2c703fa8f0258b89cc9489b7f1a4bc3f1b9
Aiden McClelland <me@drbonez.dev>
2021-09-28 12:33:40 -0600

    misc fixes

--- appmgr/build-dev.sh
+++ appmgr/build-dev.sh
@@ -8,7 +8,9 @@ if [ "$0" != "./build-dev.sh" ]; then
 	exit 1
 fi
 
-alias 'rust-arm-builder'='docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-arm-cross:latest'
+alias 'rust-arm64-builder'='docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-arm-cross:aarch64'
 
-cd ..
-rust-arm-builder sh -c "(cd appmgr && cargo build)"
+cd ../..
+rust-arm64-builder sh -c "(cd embassy-os/appmgr && cargo build)"
+cd embassy-os/appmgr
+#rust-arm64-builder aarch64-linux-gnu-strip target/aarch64-unknown-linux-gnu/release/embassyd
---
+++ appmgr/build-portable-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+shopt -s expand_aliases
+
+if [ "$0" != "./build-portable-dev.sh" ]; then
+	>&2 echo "Must be run from appmgr directory"
+	exit 1
+fi
+
+alias 'rust-musl-builder'='docker run --rm -it -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-musl-cross:x86_64-musl'
+
+cd ../..
+rust-musl-builder sh -c "(cd embassy-os/appmgr && cargo +beta build --target=x86_64-unknown-linux-musl --no-default-features)"
+cd embassy-os/appmgr
