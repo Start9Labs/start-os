030cf56a6d39da75bbc7af26e8cdb38d21e6f58c
kn0wmad <kn0wmad@protonmail.com>
2021-09-29 13:39:20 -0600

    Copy script edit

--- build/copy.sh
+++ build/copy.sh
@@ -8,7 +8,9 @@ sudo cp target/aarch64-unknown-linux-gnu/release/embassyd /mnt/usr/local/bin
 sudo cp target/aarch64-unknown-linux-gnu/release/embassy-cli /mnt/usr/local/bin
 sudo cp *.service /mnt/etc/systemd/system/
 
-# Enter the embassy-os directory, copy over the 3 ui components
+# Make the frontend directories, enter the embassy-os directory, copy over the 3 ui components
+mkdir /mnt/var/www
+mkdir /mnt/var/www/html
 cd ..
 
 sudo cp -R ui/ /mnt/var/www/html/
