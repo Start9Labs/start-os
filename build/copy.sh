75e1aa8031fbdf34a4d7df44f6ab89ebc20b708b
kn0wmad <kn0wmad@protonmail.com>
2021-09-29 13:33:33 -0600

    Script/README edits

--- build/copy.sh
+++ build/copy.sh
@@ -8,12 +8,12 @@ sudo cp target/aarch64-unknown-linux-gnu/release/embassyd /mnt/usr/local/bin
 sudo cp target/aarch64-unknown-linux-gnu/release/embassy-cli /mnt/usr/local/bin
 sudo cp *.service /mnt/etc/systemd/system/
 
-# Enter the ui directory, copy over the 3 ui components
-cd ../ui/
+# Enter the embassy-os directory, copy over the 3 ui components
+cd ..
 
 sudo cp -R ui/ /mnt/var/www/html/
-sudo cp -R setup/ /mnt/var/www/html/
-sudo cp -R diagnostic/ /mnt/var/www/html/
+sudo cp -R setup-wizard/ /mnt/var/www/html/
+sudo cp -R diagnostic-ui/ /mnt/var/www/html/
 
 # Make the .ssh directory
 sudo mkdir -p /mnt/root/.ssh
