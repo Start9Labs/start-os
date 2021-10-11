8ab2bb7252aabd9e40423282f8659d6130716559
Matt Hill <matthewonthemoon@gmail.com>
2021-10-11 11:55:27 -0600

    only backing up for maintenance page

--- ui/src/app/pages/maintenance/maintenance.page.ts
+++ ui/src/app/pages/maintenance/maintenance.page.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core'
-import { ServerStatus } from 'src/app/services/patch-db/data-model'
-import { PatchDbService } from 'src/app/services/patch-db/patch-db.service'
 
 @Component({
   selector: 'maintenance',
   templateUrl: 'maintenance.page.html',
   styleUrls: ['maintenance.page.scss'],
 })
-export class MaintenancePage {
-  ServerStatus = ServerStatus
-
-  constructor (
-    public readonly patch: PatchDbService,
-  ) { }
-}
+export class MaintenancePage { }
 
