1e02345141fcd030eaca71668fe196ed692b9786
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-10-11 16:58:15 -0600

    add wlan0 to avahi conf allowed interfaces

--- build/initialization.sh
+++ build/initialization.sh
@@ -22,6 +22,7 @@ sed -i 's/Restart=on-failure/Restart=always/g' /lib/systemd/system/tor@default.s
 sed -i '/}/i \ \ \ \ application\/wasm \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ wasm;' /etc/nginx/mime.types
 sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
 sed -i 's/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant -c \/etc\/wpa_supplicant.conf -i wlan0/g' /lib/systemd/system/wpa_supplicant.service
+sed -i 's/#allow-interfaces=eth0/allow-interfaces=eth0,wlan0/g' /etc/avahi/avahi-daemon.conf
 echo "auto wlan0" > /etc/network/interfaces
 echo "iface wlan0 inet dhcp" >> /etc/network/interfaces
 mkdir -p /etc/nginx/ssl
