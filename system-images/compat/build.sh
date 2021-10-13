2e310a6c1ab43a1443a3ab58bc17e8ad34cc1cee
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2021-10-13 16:40:56 -0600

    fix compat build

--- system-images/compat/build.sh
+++ system-images/compat/build.sh
@@ -10,6 +10,6 @@ fi
 
 alias 'rust-musl-builder'='docker run --rm -it -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-musl-cross:aarch64-musl'
 
-cd ../..
-rust-musl-builder sh -c "(cd embassy-os/compat && cargo +beta build --release --target=aarch64-unknown-linux-musl --no-default-features)"
-cd embassy-os/compat
+cd ../../..
+rust-musl-builder sh -c "(cd embassy-os/system-images/compat && cargo +beta build --release --target=aarch64-unknown-linux-musl --no-default-features)"
+cd embassy-os/system-images/compat
