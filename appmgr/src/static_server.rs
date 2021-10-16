7e4ced60a773318a18dd200715eb541636ffe540
Justin Miller <dragondef@gmail.com>
2021-10-16 12:10:02 -0600

    feat: Content headers

--- appmgr/src/static_server.rs
+++ appmgr/src/static_server.rs
@@ -6,9 +6,10 @@ use digest::Digest;
 use http::response::Builder;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Error as HyperError, Method, Request, Response, Server, StatusCode};
+use std::fs::Metadata;
 use std::future::Future;
 use std::path::PathBuf;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::UNIX_EPOCH;
 use tokio::fs::File;
 use tokio_util::codec::{BytesCodec, FramedRead};
 
@@ -45,7 +46,6 @@ pub fn init(
 }
 
 async fn file_server_router(req: Request<Body>, ctx: RpcContext) -> Result<Response<Body>, Error> {
-    println!("Request = {:?}", req);
     let (request_parts, _body) = req.into_parts();
     let valid_session = HasValidSession::from_request_parts(&request_parts, &ctx).await;
     match (
@@ -54,11 +54,8 @@ async fn file_server_router(req: Request<Body>, ctx: RpcContext) -> Result<Respo
         request_parts.uri.path().strip_prefix("/"),
     ) {
         (Err(error), _, _) => {
-            tracing::warn!(
-                "unauthorized for {:?} @{:?}",
-                error,
-                request_parts.uri.path()
-            );
+            tracing::warn!("unauthorized for {} @{:?}", error, request_parts.uri.path());
+            tracing::debug!("{:?}", error);
             return Ok(Response::builder()
                 .status(StatusCode::UNAUTHORIZED)
                 .body(NOT_AUTHORIZED.into())
@@ -95,24 +92,39 @@ async fn file_send(
     let path = ctx.datadir.join(PKG_PUBLIC_DIR).join(filename);
 
     if let Ok(file) = File::open(path.clone()).await {
-        let mut builder = Response::builder().status(StatusCode::OK);
-
         let metadata = file.metadata().await.with_kind(ErrorKind::Filesystem)?;
-        let length = metadata.len();
-        if !metadata.is_file() || length == 0 {
-            return Ok(not_found());
-        }
-        let modified = metadata.modified().with_kind(ErrorKind::Filesystem)?;
-        builder = with_e_tag(path, modified, builder);
+        let _is_non_empty = match IsNonEmptyFile::new(&metadata, &path) {
+            Some(a) => a,
+            None => return Ok(not_found()),
+        };
+
+        let mut builder = Response::builder().status(StatusCode::OK);
+        builder = with_e_tag(&path, &metadata, builder)?;
+        builder = with_content_type(&path, builder);
+        builder = with_content_length(&metadata, builder);
         let stream = FramedRead::new(file, BytesCodec::new());
         let body = Body::wrap_stream(stream);
         return Ok(builder.body(body).with_kind(ErrorKind::Network)?);
     }
+    tracing::debug!("File not found: {:?}", path);
 
     Ok(not_found())
 }
 
-fn with_e_tag(path: PathBuf, modified: SystemTime, builder: Builder) -> Builder {
+struct IsNonEmptyFile(());
+impl IsNonEmptyFile {
+    fn new(metadata: &Metadata, path: &PathBuf) -> Option<Self> {
+        let length = metadata.len();
+        if !metadata.is_file() || length == 0 {
+            tracing::debug!("File is empty: {:?}", path);
+            return None;
+        }
+        Some(Self(()))
+    }
+}
+
+fn with_e_tag(path: &PathBuf, metadata: &Metadata, builder: Builder) -> Result<Builder, Error> {
+    let modified = metadata.modified().with_kind(ErrorKind::Filesystem)?;
     let mut hasher = sha2::Sha256::new();
     hasher.update(format!("{:?}", path).as_bytes());
     hasher.update(
@@ -126,8 +138,37 @@ fn with_e_tag(path: PathBuf, modified: SystemTime, builder: Builder) -> Builder 
         .as_bytes(),
     );
     let res = hasher.finalize();
-    builder.header(
+    Ok(builder.header(
         "ETag",
         base32::encode(base32::Alphabet::RFC4648 { padding: false }, res.as_slice()).to_lowercase(),
-    )
+    ))
+}
+///https://en.wikipedia.org/wiki/Media_type
+fn with_content_type(path: &PathBuf, builder: Builder) -> Builder {
+    let content_type = match path.extension() {
+        Some(os_str) => match os_str.to_str() {
+            Some("apng") => "image/apng",
+            Some("avif") => "image/avif",
+            Some("flif") => "image/flif",
+            Some("gif") => "image/gif",
+            Some("jpg") | Some("jpeg") | Some("jfif") | Some("pjpeg") | Some("pjp") => "image/jpeg",
+            Some("jxl") => "image/jxl",
+            Some("png") => "image/png",
+            Some("svg") => "image/svg+xml",
+            Some("webp") => "image/webp",
+            Some("mng") | Some("x-mng") => "image/x-mng",
+            Some("css") => "text/css",
+            Some("csv") => "text/csv",
+            Some("html") => "text/html",
+            Some("php") => "text/php",
+            Some("plain") | Some("md") | Some("txt") => "text/plain",
+            Some("xml") => "text/xml",
+            None | Some(_) => "text/plain",
+        },
+        None => "text/plain",
+    };
+    builder.header("Content-Type", content_type)
+}
+fn with_content_length(metadata: &Metadata, builder: Builder) -> Builder {
+    builder.header("Content-Length", metadata.len())
 }
