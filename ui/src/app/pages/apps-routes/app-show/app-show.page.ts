76eafc676a5abb028fb51c4e69e1776afd2daae3
Matt Hill <matthewonthemoon@gmail.com>
2021-10-21 16:34:06 -0600

    fix health check display

--- ui/src/app/pages/apps-routes/app-show/app-show.page.ts
+++ ui/src/app/pages/apps-routes/app-show/app-show.page.ts
@@ -105,7 +105,7 @@ export class AppShowPage {
       )
       .subscribe(main => {
         if (main.status === PackageMainStatus.Running) {
-          this.healthChecks = main.health
+          this.healthChecks = { ...main.health }
         } else {
           this.healthChecks = { }
         }
