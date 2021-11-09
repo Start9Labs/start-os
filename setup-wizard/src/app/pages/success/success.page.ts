61ec5e95d97ccedc6949b998655a38d66a505afa
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-11-09 10:29:36 -0700

    success selector in init

--- setup-wizard/src/app/pages/success/success.page.ts
+++ setup-wizard/src/app/pages/success/success.page.ts
@@ -16,10 +16,6 @@ export class SuccessPage {
     public readonly stateService: StateService,
   ) { }
 
-  ngAfterViewInit () {
-    setTimeout(() => this.download(), 500)
-  }
-
   async copy (address: string): Promise<void> {
     const success = await this.copyToClipboard(address)
     const message = success ? 'copied to clipboard!' : 'failed to copy'
@@ -42,23 +38,6 @@ export class SuccessPage {
 
   installCert () {
     document.getElementById('install-cert').click()
-  }
-
-  download () {
-    document.getElementById('tor-addr').innerHTML = this.stateService.torAddress
-    document.getElementById('lan-addr').innerHTML = this.stateService.lanAddress
-    document.getElementById('cert').setAttribute('href', 'data:application/x-x509-ca-cert;base64,' + encodeURIComponent(this.stateService.cert))
-    let html = document.getElementById('downloadable').innerHTML
-    const filename = 'embassy-info.html'
-
-    const elem = document.createElement('a')
-    elem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(html))
-    elem.setAttribute('download', filename)
-    elem.style.display = 'none'
-
-    document.body.appendChild(elem)
-    elem.click()
-    document.body.removeChild(elem)
   }
 
   private async copyToClipboard (str: string): Promise<boolean> {
