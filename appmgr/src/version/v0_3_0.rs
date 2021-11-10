e3f7e857e3a63c3768861bba0ba9c79cd5006dd0
Aiden McClelland <me@drbonez.dev>
2021-11-10 13:22:28 -0700

    eos-version-compat

--- appmgr/src/version/v0_3_0.rs
+++ appmgr/src/version/v0_3_0.rs
@@ -1,6 +1,18 @@
+use emver::VersionRange;
+use lazy_static::lazy_static;
+
 use super::*;
 
 const V0_3_0: emver::Version = emver::Version::new(0, 3, 0, 0);
+lazy_static! {
+    static ref V0_3_0_COMPAT: VersionRange = VersionRange::Conj(
+        Box::new(VersionRange::Anchor(
+            emver::GTE,
+            emver::Version::new(0, 3, 0, 0),
+        )),
+        Box::new(VersionRange::Anchor(emver::LTE, Current::new().semver())),
+    );
+}
 
 pub struct Version;
 #[async_trait]
@@ -11,6 +23,9 @@ impl VersionT for Version {
     }
     fn semver(&self) -> emver::Version {
         V0_3_0
+    }
+    fn compat(&self) -> &'static VersionRange {
+        &*V0_3_0_COMPAT
     }
     async fn up<Db: DbHandle>(&self, _db: &mut Db) -> Result<(), Error> {
         Ok(())
