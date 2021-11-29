4e68d5c91d19fabecfd26de926659716edeaecd3
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-11-29 14:50:07 -0700

    drew water edits

--- ui/src/app/pages/apps-routes/app-list/app-list-reorder/app-list-reorder.component.ts
+++ ui/src/app/pages/apps-routes/app-list/app-list-reorder/app-list-reorder.component.ts
@@ -4,47 +4,47 @@ import {
   EventEmitter,
   Input,
   Output,
-} from "@angular/core";
-import { ItemReorderEventDetail } from "@ionic/core";
-import { PackageDataEntry } from "src/app/services/patch-db/data-model";
-import { map } from "rxjs/operators";
+} from '@angular/core'
+import { ItemReorderEventDetail } from '@ionic/core'
+import { PackageDataEntry } from 'src/app/services/patch-db/data-model'
+import { map } from 'rxjs/operators'
 import {
   ConnectionFailure,
   ConnectionService,
-} from "src/app/services/connection.service";
+} from 'src/app/services/connection.service'
 
 @Component({
-  selector: "app-list-reorder",
-  templateUrl: "app-list-reorder.component.html",
-  styleUrls: ["app-list-reorder.component.scss"],
+  selector: 'app-list-reorder',
+  templateUrl: 'app-list-reorder.component.html',
+  styleUrls: ['app-list-reorder.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppListReorderComponent {
   @Input()
-  reordering = false;
+  reordering = false
 
   @Input()
-  pkgs: readonly PackageDataEntry[] = [];
+  pkgs: readonly PackageDataEntry[] = []
 
   @Output()
-  readonly reorderingChange = new EventEmitter<boolean>();
+  readonly reorderingChange = new EventEmitter<boolean>()
 
   @Output()
-  readonly pkgsChange = new EventEmitter<readonly PackageDataEntry[]>();
+  readonly pkgsChange = new EventEmitter<readonly PackageDataEntry[]>()
 
   readonly connectionFailure$ = this.connectionService
     .watchFailure$()
-    .pipe(map((failure) => failure !== ConnectionFailure.None));
+    .pipe(map((failure) => failure !== ConnectionFailure.None))
 
-  constructor(private readonly connectionService: ConnectionService) {}
+  constructor (private readonly connectionService: ConnectionService) { }
 
-  toggle() {
-    this.reordering = !this.reordering;
-    this.reorderingChange.emit(this.reordering);
+  toggle () {
+    this.reordering = !this.reordering
+    this.reorderingChange.emit(this.reordering)
   }
 
-  reorder({ detail }: CustomEvent<ItemReorderEventDetail>): void {
-    this.pkgs = detail.complete([...this.pkgs]);
-    this.pkgsChange.emit(this.pkgs);
+  reorder ({ detail }: CustomEvent<ItemReorderEventDetail>): void {
+    this.pkgs = detail.complete([...this.pkgs])
+    this.pkgsChange.emit(this.pkgs)
   }
 }
