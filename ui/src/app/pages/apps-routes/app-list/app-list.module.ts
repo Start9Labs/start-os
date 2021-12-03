2d73340e2a85c12f4541a49b1beb9737190bcaf2
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2021-12-03 13:57:41 -0700

    matt comments addressed

--- ui/src/app/pages/apps-routes/app-list/app-list.module.ts
+++ ui/src/app/pages/apps-routes/app-list/app-list.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import { IonicModule } from "@ionic/angular";
-import { AppListPage } from "./app-list.page";
-import { StatusComponentModule } from "src/app/components/status/status.component.module";
-import { SharingModule } from "src/app/modules/sharing.module";
-import { BadgeMenuComponentModule } from "src/app/components/badge-menu-button/badge-menu.component.module";
-import { AppListIconComponent } from "./app-list-icon/app-list-icon.component";
-import { AppListEmptyComponent } from "./app-list-empty/app-list-empty.component";
-import { AppListPkgComponent } from "./app-list-pkg/app-list-pkg.component";
-import { AppListRecComponent } from "./app-list-rec/app-list-rec.component";
-import { AppListReorderComponent } from "./app-list-reorder/app-list-reorder.component";
-import { PackageInfoPipe } from "./package-info.pipe";
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Routes, RouterModule } from '@angular/router'
+import { IonicModule } from '@ionic/angular'
+import { AppListPage } from './app-list.page'
+import { StatusComponentModule } from 'src/app/components/status/status.component.module'
+import { SharingModule } from 'src/app/modules/sharing.module'
+import { BadgeMenuComponentModule } from 'src/app/components/badge-menu-button/badge-menu.component.module'
+import { AppListIconComponent } from './app-list-icon/app-list-icon.component'
+import { AppListEmptyComponent } from './app-list-empty/app-list-empty.component'
+import { AppListPkgComponent } from './app-list-pkg/app-list-pkg.component'
+import { AppListRecComponent } from './app-list-rec/app-list-rec.component'
+import { AppListReorderComponent } from './app-list-reorder/app-list-reorder.component'
+import { PackageInfoPipe } from './package-info.pipe'
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: AppListPage,
   },
-];
+]
 
 @NgModule({
   imports: [
@@ -39,4 +39,4 @@ const routes: Routes = [
     PackageInfoPipe,
   ],
 })
-export class AppListPageModule {}
+export class AppListPageModule { }
