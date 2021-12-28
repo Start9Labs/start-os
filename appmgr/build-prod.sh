320f2e720620dac933baa3ceac44140a2541364a
Aiden McClelland <me@drbonez.dev>
2021-12-27 17:41:35 -0700

    unstable flag

--- appmgr/build-prod.sh
+++ appmgr/build-prod.sh
@@ -11,6 +11,10 @@ fi
 alias 'rust-arm64-builder'='docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v "$(pwd)":/home/rust/src start9/rust-arm-cross:aarch64'
 
 cd ..
-rust-arm64-builder sh -c "(cd appmgr && cargo build --release)"
+if [[ "$ENVIRONMENT" =~ (^|-)unstable($|-) ]]; then
+	rust-arm64-builder sh -c "(cd appmgr && cargo build --release --features unstable)"
+else
+	rust-arm64-builder sh -c "(cd appmgr && cargo build --release)"
+fi
 cd appmgr
 #rust-arm64-builder aarch64-linux-gnu-strip target/aarch64-unknown-linux-gnu/release/embassyd
