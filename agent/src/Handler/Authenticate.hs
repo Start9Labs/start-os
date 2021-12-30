ad832394b7af4b5a18ae4147375b25ef8e7cc15d
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-12-30 11:32:26 -0700

    kill the agent

--- agent/src/Handler/Authenticate.hs
+++
@@ -1,9 +0,0 @@
-module Handler.Authenticate where
-
-import           Startlude
-
-import           Foundation
-
--- handled by auth switch in Foundation
-getAuthenticateR :: Handler ()
-getAuthenticateR = pure ()
