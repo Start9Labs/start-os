c7c018c30888171d3ab72e311f9aeef6211d1d33
Drew Ansbacher <drew.ansbacher@gmail.com>
2021-12-31 09:37:59 -0700

    login error fix (#1013)
    * login error fix
    
    * loading gone
    
    * fix type erros
    
    Co-authored-by: Drew Ansbacher <drew.ansbacher@spiredigital.com>
    Co-authored-by: Matt Hill <matthewonthemoon@gmail.com>

--- ui/src/app/pages/apps-routes/app-list/app-list.page.ts
+++ ui/src/app/pages/apps-routes/app-list/app-list.page.ts
@@ -18,13 +18,12 @@ export class AppListPage {
   pkgs: readonly PackageDataEntry[] = []
   recoveredPkgs: readonly RecoveredInfo[] = []
   order: readonly string[] = []
-  loading = true
   reordering = false
 
   constructor (
     private readonly api: ApiService,
-    private readonly patch: PatchDbService,
     private readonly destroy$: DestroyService,
+    public readonly patch: PatchDbService,
   ) { }
 
   get empty (): boolean {
@@ -42,7 +41,6 @@ export class AppListPage {
           this.pkgs = pkgs
           this.recoveredPkgs = recoveredPkgs
           this.order = order
-          this.loading = false
 
           // set order in UI DB if there were unknown packages
           if (order.length < pkgs.length) {
