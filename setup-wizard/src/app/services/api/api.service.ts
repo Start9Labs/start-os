badf8c508fcdcec2e1f20339a081b3f9e20127d8
Matt Hill <matthewonthemoon@gmail.com>
2022-01-19 07:38:34 -0700

    fixes #1096

--- setup-wizard/src/app/services/api/api.service.ts
+++ setup-wizard/src/app/services/api/api.service.ts
@@ -46,7 +46,6 @@ export interface DiskListResponse {
 export interface DiskBackupTarget {
   vendor: string | null
   model: string | null
-  guid: string | null
   logicalname: string | null
   label: string | null
   capacity: number
