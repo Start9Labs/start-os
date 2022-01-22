edde4783828d61a90b9f4dbc0dd3eb85f851dac0
Aiden McClelland <me@drbonez.dev>
2022-01-21 19:02:23 -0700

    rename appmgr

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# backend
+(TODO)
--- backend/copy.sh
+++ backend/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Enter the appmgr directory, copy over the built EmbassyOS binaries and systemd services, edit the nginx config, then create the .ssh directory
+# Enter the backend directory, copy over the built EmbassyOS binaries and systemd services, edit the nginx config, then create the .ssh directory
 
 cp target/aarch64-unknown-linux-gnu/release/embassy-init /mnt/usr/local/bin
 cp target/aarch64-unknown-linux-gnu/release/embassyd /mnt/usr/local/bin
