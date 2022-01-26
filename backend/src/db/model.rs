8288cb40fe963f0876236d59cd4ce1c89cf40bb8
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-01-26 15:29:43 -0700

    registry.start9.com -> marketplace.start9.com

--- backend/src/db/model.rs
+++ backend/src/db/model.rs
@@ -52,7 +52,7 @@ impl Database {
                     .unwrap(),
                 status: ServerStatus::Running {},
                 #[cfg(not(feature = "beta"))]
-                eos_marketplace: "https://registry.start9.com".parse().unwrap(),
+                eos_marketplace: "https://marketplace.start9.com".parse().unwrap(),
                 #[cfg(feature = "beta")]
                 eos_marketplace: "https://beta-registry-0-3.start9labs.com".parse().unwrap(),
                 package_marketplace: None,
