90281dbbd7254f804333b90b22eea89aa5a4d62a
Matt Hill <matthewonthemoon@gmail.com>
2022-01-31 15:24:30 -0700

    update marked to latest version

--- frontend/projects/ui/src/app/pipes/markdown.pipe.ts
+++ frontend/projects/ui/src/app/pipes/markdown.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core'
-import * as marked from 'marked'
+import { marked } from 'marked'
 import * as DOMPurify from 'dompurify'
 
 @Pipe({
   name: 'markdown',
 })
 export class MarkdownPipe implements PipeTransform {
-  transform (value: any): any {
+  transform(value: any): any {
     if (value && value.length > 0) {
       const html = marked(value)
       const sanitized = DOMPurify.sanitize(html)
