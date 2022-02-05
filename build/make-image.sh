79e4c6880a04b686d3159bed7355cd43bef12788
Aiden McClelland <me@drbonez.dev>
2022-02-05 00:44:01 -0700

    switch base os to raspberry pi os
    minor fixes
    
    more robust

--- build/make-image.sh
+++ build/make-image.sh
@@ -12,8 +12,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
 truncate --size=$[(31116287+1)*512] eos.img
-export OUTPUT_DEVICE=$(sudo losetup --show -fP eos.img)
-export LOOPDEV=$(sudo losetup --show -fP ubuntu.img)
+if [ -z "$OUTPUT_DEVICE" ]; then
+  export OUTPUT_DEVICE=$(sudo losetup --show -fP eos.img)
+  export DETACH_OUTPUT_DEVICE=1
+else
+  export DETACH_OUTPUT_DEVICE=0
+fi
+export LOOPDEV=$(sudo losetup --show -fP raspios.img)
 ./build/partitioning.sh
 ./build/write-image.sh
 sudo e2fsck -f ${OUTPUT_DEVICE}p3
@@ -23,4 +28,6 @@ BLOCK_COUNT=$(echo "$BLOCK_INFO" | grep "Block count:" | sed 's/Block count:\s\+
 BLOCK_SIZE=$(echo "$BLOCK_INFO" | grep "Block size:" | sed 's/Block size:\s\+//g')
 echo "YOUR GREEN FILESYSTEM is '$[$BLOCK_COUNT*$BLOCK_SIZE]' BYTES"
 echo "IF YOU ARE QUICK-FLASHING FROM MAC-OS, NOTE THIS NUMBER FOR LATER"
-sudo losetup -d $OUTPUT_DEVICE
+if [ "$DETACH_OUTPUT_DEVICE" -eq "1" ]; then
+  sudo losetup -d $OUTPUT_DEVICE
+fi
