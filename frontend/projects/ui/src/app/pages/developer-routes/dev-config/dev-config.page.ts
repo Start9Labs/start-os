e06c863c2e3f0582887784d0ac98c4bf94a6824a
Matt Hill <matthewonthemoon@gmail.com>
2022-02-08 12:52:05 -0700

    use typed config and add comment to yaml

--- frontend/projects/ui/src/app/pages/developer-routes/dev-config/dev-config.page.ts
+++ frontend/projects/ui/src/app/pages/developer-routes/dev-config/dev-config.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { ModalController } from '@ionic/angular'
 import * as yaml from 'js-yaml'
 import { GenericFormPage } from '../../../modals/generic-form/generic-form.page'
+import { ConfigSpec } from '../../../pkg-config/config-types'
 import { ErrorToastService } from '../../../services/error-toast.service'
 
 @Component({
@@ -11,18 +12,23 @@ import { ErrorToastService } from '../../../services/error-toast.service'
 })
 export class DevConfigPage {
   editorOptions = { theme: 'vs-dark', language: 'yaml' }
-  code: string = SAMPLE_CODE
+  code: string
 
   constructor(
     private readonly errToast: ErrorToastService,
     private readonly modalCtrl: ModalController,
   ) {}
 
+  ngOnInit() {
+    this.code = yaml
+      .dump(SAMPLE_CODE)
+      .replace(/warning:/g, '# Optional\n  warning:')
+  }
+
   async submit() {
-    let doc: string
+    let doc: any
     try {
       doc = yaml.load(this.code)
-      console.log(JSON.parse(JSON.stringify(doc, null, 2)))
     } catch (e) {
       this.errToast.present(e)
     }
@@ -47,37 +53,54 @@ export class DevConfigPage {
   }
 }
 
-const SAMPLE_CODE = `sample-input:
-  type: string
-  nullable: false
-  name: "Example"
-  description: "This is an example string input for a config"
-  default: "Welcome to config!"
-  pattern: "^[a-zA-Z0-9! _]+$"
-  pattern-description: "Must be alphanumeric (can contain underscore)."
-favenum:
-  name: Favorite Number
-  description: Whats your favorite number?  We're dying to know
-  type: number
-  nullable: false
-  range: "[5,1000000]"
-  integral: true
-  default: 30
-friends:
-  type: boolean
-  name: Are we friends?
-  description: I could really use a ride to the airport this weekend
-  default: false
-favecolor:
-  name: Favorite Color
-  description: Select your favorite color.  Unfortunately your options are pretty limited
-  type: enum
-  values:
-    - red
-    - blue
-    - green
-  value-names:
-    red: Red
-    blue: Blue
-    green: Green
-  default: blue`
+const SAMPLE_CODE: ConfigSpec = {
+  'sample-string': {
+    type: 'string',
+    name: 'Example String Input',
+    nullable: false,
+    masked: false,
+    copyable: false,
+    // optional
+    warning: null,
+    description: 'Example description for required string input.',
+    default: null,
+    placeholder: 'Enter string value',
+    pattern: '^[a-zA-Z0-9! _]+$',
+    'pattern-description': 'Must be alphanumeric (may contain underscore).',
+  },
+  'sample-number': {
+    type: 'number',
+    name: 'Example Number Input',
+    nullable: false,
+    range: '[5,1000000]',
+    integral: true,
+    // optional
+    warning: 'Example warning to display when changing this number value.',
+    units: 'ms',
+    description: 'Example description for optional number input.',
+    default: null,
+    placeholder: 'Enter number value',
+  },
+  'sample-boolean': {
+    type: 'boolean',
+    name: 'Example Boolean Toggle',
+    // optional
+    warning: null,
+    description: 'Example description for boolean toggle',
+    default: true,
+  },
+  'sample-enum': {
+    type: 'enum',
+    name: 'Example Enum Select',
+    values: ['red', 'blue', 'green'],
+    'value-names': {
+      red: 'Red',
+      blue: 'Blue',
+      green: 'Green',
+    },
+    // optional
+    warning: 'Example warning to display when changing this enum value.',
+    description: 'Example description for enum select',
+    default: 'red',
+  },
+}
