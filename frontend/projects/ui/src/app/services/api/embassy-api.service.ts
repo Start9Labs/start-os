6d22f0b40d01d33d73af30a0d0372cdbade72f61
Drew Ansbacher <drew.ansbacher@spiredigital.com>
2022-02-10 10:33:05 -0700

    bugfix

--- frontend/projects/ui/src/app/services/api/embassy-api.service.ts
+++ frontend/projects/ui/src/app/services/api/embassy-api.service.ts
@@ -77,7 +77,7 @@ export abstract class ApiService implements Source<DataModel>, Http<DataModel> {
   }
 
   abstract restartServer(
-    params: RR.UpdateServerReq,
+    params: RR.RestartServerReq,
   ): Promise<RR.RestartServerRes>
 
   abstract shutdownServer(
