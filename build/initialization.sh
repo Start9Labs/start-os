45e0bca6d51691a3ff1c8cd8414ba418827b06bd
Aiden McClelland <3732071+dr-bonez@users.noreply.github.com>
2022-02-15 10:27:25 -0700

    fix wifi for raspios (#1207)
    * Wip: Fix most of the wifi and 80% of initialization.sh
    
    * fix wifi
    
    * reorganize code
    
    Co-authored-by: J M <mogulslayer@gmail.com>

--- build/initialization.sh
+++ build/initialization.sh
@@ -6,8 +6,6 @@ set -e
 ! test -f /etc/docker/daemon.json || rm /etc/docker/daemon.json
 mount -o remount,rw /boot
 
-curl -fsSL https://get.docker.com | sh # TODO: commit this script into git instead of live fetching it
-
 apt-get update
 apt-get install -y \
 	tor \
@@ -28,16 +26,21 @@ apt-get install -y \
 	samba-common-bin \
 	ntp \
 	network-manager
+
+curl -fsSL https://get.docker.com | sh # TODO: commit this script into git instead of live fetching it
+
+apt-get purge openresolv dhcpcd5 -y
+systemctl disable wpa_supplicant.service 
+
 apt-get autoremove -y
 apt-get upgrade -y
 
 sed -i 's/Restart=on-failure/Restart=always/g' /lib/systemd/system/tor@default.service
 sed -i '/}/i \ \ \ \ application\/wasm \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ wasm;' /etc/nginx/mime.types
 sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
-sed -i 's/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant -c \/etc\/wpa_supplicant.conf -i wlan0/g' /lib/systemd/system/wpa_supplicant.service
+# sed -i 's/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant/ExecStart=\/sbin\/wpa_supplicant -u -s -O \/run\/wpa_supplicant -c \/etc\/wpa_supplicant.conf -i wlan0/g' /lib/systemd/system/wpa_supplicant.service
 sed -i 's/#allow-interfaces=eth0/allow-interfaces=eth0,wlan0/g' /etc/avahi/avahi-daemon.conf
-echo "auto wlan0" > /etc/network/interfaces
-echo "iface wlan0 inet dhcp" >> /etc/network/interfaces
+echo "#" > /etc/network/interfaces
 mkdir -p /etc/nginx/ssl
 
 # fix to suppress docker warning, fixed in 21.xx release of docker cli: https://github.com/docker/cli/pull/2934
