94ae6ebf0366032cac8dde45a8439723155570f8
Matt Hill <matthewonthemoon@gmail.com>
2022-02-26 10:55:13 -0700

    remove console logs

--- frontend/projects/ui/src/app/modals/snake/snake.page.ts
+++ frontend/projects/ui/src/app/modals/snake/snake.page.ts
@@ -80,7 +80,6 @@ export class SnakePage {
     this.canvas.style.border = '1px solid #e0e0e0'
     this.context = this.canvas.getContext('2d')
     const container = document.getElementsByClassName('canvas-center')[0]
-    console.log('height', container.clientHeight)
     this.grid = Math.min(
       Math.floor(container.clientWidth / this.width),
       Math.floor(container.clientHeight / this.height),
