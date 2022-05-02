21e5d461a1db88c80622fa69a20c8973feeadc60
Aiden McClelland <me@drbonez.dev>
2022-05-02 14:28:17 -0600

    version bump

--- backend/src/version/v0_3_0_3.rs
+++ backend/src/version/v0_3_0_3.rs
@@ -2,17 +2,17 @@ use emver::VersionRange;
 
 use super::*;
 
-const V0_3_0_2: emver::Version = emver::Version::new(0, 3, 0, 2);
+const V0_3_0_3: emver::Version = emver::Version::new(0, 3, 0, 3);
 
 pub struct Version;
 #[async_trait]
 impl VersionT for Version {
-    type Previous = v0_3_0_1::Version;
+    type Previous = v0_3_0_2::Version;
     fn new() -> Self {
         Version
     }
     fn semver(&self) -> emver::Version {
-        V0_3_0_2
+        V0_3_0_3
     }
     fn compat(&self) -> &'static VersionRange {
         &*v0_3_0::V0_3_0_COMPAT
