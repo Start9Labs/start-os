00b582786ccf7f06c200daaf08d43277b5184f1f
kn0wmad <kn0wmad@protonmail.com>
2022-06-25 21:45:24 -0600

    Apt appendage

--- build/initialization.sh
+++ build/initialization.sh
@@ -47,7 +47,7 @@ touch /etc/apt/sources.list.d/tor.list
 echo "deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org bullseye main" >> /etc/apt/sources.list.d/tor.list
 echo "deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org bullseye main" >> /etc/apt/sources.list.d/tor.list
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
-apt update && apt install tor deb.torproject.org-keyring
+apt update && apt install -y tor deb.torproject.org-keyring
 
 curl -fsSL https://get.docker.com | sh # TODO: commit this script into git instead of live fetching it
 
