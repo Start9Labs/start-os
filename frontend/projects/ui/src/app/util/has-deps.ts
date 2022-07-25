83fe3917966fb2cb25133371781a55e298ed17cd
Matt Hill <MattDHill@users.noreply.github.com>
2022-07-25 12:05:44 -0600

    replace bang with question mark in html (#1683)

--- frontend/projects/ui/src/app/util/has-deps.ts
+++ frontend/projects/ui/src/app/util/has-deps.ts
@@ -1,7 +1,7 @@
 import { PackageDataEntry } from '../services/patch-db/data-model'
 
 export function hasCurrentDeps(pkg: PackageDataEntry): boolean {
-  return !!Object.keys(pkg.installed?.['current-dependents'] || {})
-    // @TODO fix Manifest type
-    .filter(depId => depId !== (pkg.manifest as any).id).length
+  return !!Object.keys(pkg.installed?.['current-dependents'] || {}).filter(
+    depId => depId !== pkg.manifest.id,
+  ).length
 }
