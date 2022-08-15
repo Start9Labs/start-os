e1c30a918b5dcc13e7383055fcc378f79592f9f4
Matt Hill <MattDHill@users.noreply.github.com>
2022-08-15 11:03:11 -0600

    highlight instructions if not viewed (#1731)

--- frontend/projects/ui/src/app/util/get-server-info.ts
+++ frontend/projects/ui/src/app/util/get-server-info.ts
@@ -1,4 +1,3 @@
-import { first } from 'rxjs/operators'
 import { PatchDbService } from 'src/app/services/patch-db/patch-db.service'
 import { ServerInfo } from 'src/app/services/patch-db/data-model'
 import { firstValueFrom } from 'rxjs'
