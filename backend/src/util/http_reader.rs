a20152723c64265b18ffa75d7557a34227d295f2
Stephen Chavez <stephen@start9labs.com>
2022-08-16 12:39:20 -0600

    v1 of http_reader

--- backend/src/util/http_reader.rs
+++ backend/src/util/http_reader.rs
@@ -1,15 +1,15 @@
-#![feature(io_error_other)]
-
-
 use std::fmt::Display;
+use std::io::Error as StdIOError;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use color_eyre::eyre::eyre;
+use futures::future::BoxFuture;
+use futures::FutureExt;
 use http::header::{ACCEPT_RANGES, CONTENT_LENGTH, RANGE};
 use pin_project::pin_project;
 use reqwest::{Client, Url};
-use tokio::io::AsyncRead;
+use tokio::io::{AsyncRead, AsyncReadExt};
 
 use crate::{Error, ResultExt};
 
@@ -22,9 +22,11 @@ pub struct HttpReader {
     http_client: Client,
     total_bytes: usize,
     range_unit: Option<RangeUnit>,
+    read_in_progress: Option<BoxFuture<'static, Result<Vec<u8>, Error>>>,
 }
 
 // If we want to add support for units other than Accept-Ranges: bytes, we can use this enum
+#[derive(Clone, Copy)]
 enum RangeUnit {
     Bytes,
 }
@@ -51,7 +53,7 @@ impl HttpReader {
             .with_kind(crate::ErrorKind::InvalidRequest)?;
 
         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
-        // TODO: We should decide if the web request should be rejected if http_ranges is not supported. Or we could download the entire contents otherwise.
+
         let accept_ranges = head_request.headers().get(ACCEPT_RANGES);
 
         let range_unit = match accept_ranges {
@@ -74,7 +76,15 @@ impl HttpReader {
                 }
             }
             // None can mean just get entire contents.
-            None => None,
+            None => {
+                return Err(Error::new(
+                    eyre!(
+                        "{} HTTP range downloading not supported with this url",
+                        http_url
+                    ),
+                    crate::ErrorKind::HttpRange,
+                ))
+            }
         };
 
         let total_bytes_option = head_request.headers().get(CONTENT_LENGTH);
@@ -96,24 +106,32 @@ impl HttpReader {
             http_client,
             total_bytes,
             range_unit,
+            read_in_progress: None,
         })
     }
 
-    pub async fn get_range(&self, start: usize, end: usize) -> Result<Vec<u8>, Error> {
+    async fn get_range(
+        range_unit: Option<RangeUnit>,
+        http_client: Client,
+        http_url: Url,
+        start: usize,
+        end: usize,
+    ) -> Result<Vec<u8>, Error> {
         let mut data = Vec::new();
-        match &self.range_unit {
+        match range_unit {
             Some(unit) => {
                 let data_range = format!("{}={}-{} ", unit, start, end);
+                println!("get range alive?");
 
-                let data_resp = self
-                    .http_client
-                    .get(self.http_url.to_owned())
+                let data_resp = http_client
+                    .get(http_url)
                     .header(RANGE, data_range)
                     .send()
                     .await
                     .with_kind(crate::ErrorKind::InvalidRequest)?;
 
                 let status_code = data_resp.status();
+                //let data_res = data_resp.bytes().await;
                 if status_code.is_success() {
                     data = data_resp
                         .bytes()
@@ -123,7 +141,7 @@ impl HttpReader {
                 }
             }
 
-            None => todo!(),
+            None => unreachable!(),
         }
 
         Ok(data)
@@ -136,23 +154,60 @@ impl AsyncRead for HttpReader {
         cx: &mut Context<'_>,
         buf: &mut tokio::io::ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let data_chunk = self
-            .get_range(self.cursor_pos, buf.remaining())
-            .await
-            .map_err(|err| std::io::Error::other(err))?;
+        let this = self.project();
 
-        if data_chunk.len() <= buf.capacity() {
-            buf.put_slice(&data_chunk);
-            self.cursor_pos = data_chunk.len();
-
-            return Poll::Ready(Ok(()));
+        let mut fut = if let Some(fut) = this.read_in_progress.take() {
+            fut
         } else {
-            buf.put_slice(&data_chunk);
+            HttpReader::get_range(
+                *this.range_unit,
+                this.http_client.clone(),
+                this.http_url.clone(),
+                *this.cursor_pos,
+                buf.remaining(),
+            )
+            .boxed()
+        };
+        let res_poll = fut.as_mut().poll(cx);
+        println!("polling");
+        match res_poll {
+            Poll::Ready(result) => match result {
+                Ok(data_chunk) => {
+                    if data_chunk.len() <= buf.capacity() {
+                        buf.put_slice(&data_chunk);
+                        *this.cursor_pos = buf.filled().len();
 
-            return Poll::Ready(Ok(()));
+                        Poll::Ready(Ok(()))
+                    } else {
+                        buf.put_slice(&data_chunk);
+
+                        Poll::Ready(Ok(()))
+                    }
+                }
+                Err(err) => {
+                    Poll::Ready(Err(StdIOError::new(
+                        std::io::ErrorKind::Interrupted,
+                        Box::<dyn std::error::Error + Send + Sync>::from(err.source),
+                    )))
+                }
+            },
+            Poll::Pending => {
+                Poll::Pending
+            }
         }
+
     }
 }
 
 #[tokio::test]
-async fn test() {}
+async fn test() {
+    let http_url = Url::parse("https://start9.com/latest/_static/css/main.css").unwrap();
+
+    println!("{}", http_url);
+    let mut test_reader = HttpReader::new(http_url).await.unwrap();
+
+    let mut buf = Vec::with_capacity(DEFAULT_BUF_SIZE);
+    let bytes_read = test_reader.read(&mut buf).await.unwrap();
+
+    println!("bytes read: {}", bytes_read);
+}
