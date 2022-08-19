4d61f73ec2b5c836cceedb8b955278a9438edaf2
Matt Hill <matthewonthemoon@gmail.com>
2022-08-19 15:10:36 -0600

    update offline toast

--- frontend/projects/ui/src/app/components/toast-container/offline-toast/offline-toast.service.ts
+++ frontend/projects/ui/src/app/components/toast-container/offline-toast/offline-toast.service.ts
@@ -1,60 +1,42 @@
 import { Injectable } from '@angular/core'
-import { Observable, of } from 'rxjs'
-import {
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  switchMap,
-} from 'rxjs/operators'
-
+import { combineLatest, Observable, of } from 'rxjs'
+import { map, switchMap } from 'rxjs/operators'
 import { AuthService } from 'src/app/services/auth.service'
-import {
-  ConnectionFailure,
-  ConnectionService,
-} from 'src/app/services/connection.service'
+import { ConnectionService } from 'src/app/services/connection.service'
 
 export interface OfflineMessage {
   readonly message: string
   readonly link?: string
 }
 
-// TODO: Update according to new offline statuses
 // Watch for connection status
 @Injectable({ providedIn: 'root' })
 export class OfflineToastService extends Observable<OfflineMessage | null> {
   private readonly stream$ = this.authService.isVerified$.pipe(
-    switchMap(verified =>
-      verified
-        ? this.connectionService
-            .watchFailure$()
-            .pipe(distinctUntilChanged(), debounceTime(500), map(getMessage))
-        : of(null),
-    ),
+    switchMap(verified => (verified ? this.getMessage() : of(null))),
   )
+
+  private getMessage(): Observable<OfflineMessage | null> {
+    return combineLatest([
+      this.connectionService.networkConnected$,
+      this.connectionService.websocketConnected$,
+    ]).pipe(
+      map(([network, websocket]) => {
+        if (!network) return { message: 'No Internet' }
+        if (!websocket)
+          return {
+            message: 'Connecting to Embassy...',
+            link: 'https://start9.com/latest/support/common-issues',
+          }
+        return null
+      }),
+    )
+  }
 
   constructor(
     private readonly authService: AuthService,
     private readonly connectionService: ConnectionService,
   ) {
     super(subscriber => this.stream$.subscribe(subscriber))
-  }
-}
-
-function getMessage(failure: ConnectionFailure): OfflineMessage | null {
-  switch (failure) {
-    case ConnectionFailure.Network:
-      return { message: 'Phone or computer has no network connection.' }
-    case ConnectionFailure.Tor:
-      return {
-        message: 'Browser unable to connect over Tor.',
-        link: 'https://start9.com/latest/support/common-issues',
-      }
-    case ConnectionFailure.Lan:
-      return {
-        message: 'Embassy not found on Local Area Network.',
-        link: 'https://start9.com/latest/support/common-issues',
-      }
-    default:
-      return null
   }
 }
