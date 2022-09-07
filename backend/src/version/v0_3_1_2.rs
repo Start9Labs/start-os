ad59dd68b3bdb3608e587a8faa82f23aff01c2fd
Aiden McClelland <me@drbonez.dev>
2022-09-07 10:25:52 -0600

    bump version with stubbed release notes

--- backend/src/version/v0_3_1_2.rs
+++ backend/src/version/v0_3_1_2.rs
@@ -1,7 +1,5 @@
 use emver::VersionRange;
 
-use crate::hostname::{generate_id, get_hostname, sync_hostname};
-
 use super::v0_3_0::V0_3_0_COMPAT;
 use super::*;
 
@@ -21,41 +19,10 @@ impl VersionT for Version {
     fn compat(&self) -> &'static VersionRange {
         &*V0_3_0_COMPAT
     }
-    async fn up<Db: DbHandle>(&self, db: &mut Db) -> Result<(), Error> {
-        let hostname = get_hostname(db).await?;
-        crate::db::DatabaseModel::new()
-            .server_info()
-            .hostname()
-            .put(db, &Some(hostname.0))
-            .await?;
-        crate::db::DatabaseModel::new()
-            .server_info()
-            .id()
-            .put(db, &generate_id())
-            .await?;
-
-        sync_hostname(db).await?;
-        let mut ui = crate::db::DatabaseModel::new()
-            .ui()
-            .get(db, false)
-            .await?
-            .clone();
-        if let serde_json::Value::Object(ref mut ui) = ui {
-            ui.insert("ack-instructions".to_string(), serde_json::json!({}));
-        }
-        crate::db::DatabaseModel::new().ui().put(db, &ui).await?;
+    async fn up<Db: DbHandle>(&self, _db: &mut Db) -> Result<(), Error> {
         Ok(())
     }
-    async fn down<Db: DbHandle>(&self, db: &mut Db) -> Result<(), Error> {
-        let mut ui = crate::db::DatabaseModel::new()
-            .ui()
-            .get(db, false)
-            .await?
-            .clone();
-        if let serde_json::Value::Object(ref mut ui) = ui {
-            ui.remove("ack-instructions");
-        }
-        crate::db::DatabaseModel::new().ui().put(db, &ui).await?;
+    async fn down<Db: DbHandle>(&self, _db: &mut Db) -> Result<(), Error> {
         Ok(())
     }
 }
