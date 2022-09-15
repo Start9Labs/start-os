c2efdfc3772e8382e1f883c760fdc4659917f0f8
J M <2364004+Blu-J@users.noreply.github.com>
2022-09-15 11:38:25 -0600

    Update lib.rs

--- libs/js_engine/src/lib.rs
+++ libs/js_engine/src/lib.rs
@@ -798,15 +798,6 @@ mod fns {
         let parent = tokio::fs::canonicalize(parent).await?;
         Ok(child.starts_with(parent))
     }
-    #[tokio::test]
-    async fn test_is_subset() {
-        assert!(!dbg!(is_subset(
-            "/home/jm",
-            "/home/jm/code/Projects/../../.."
-        )
-        .await
-        .unwrap_or(false)))
-    }
 }
 
 fn system_time_as_unix_ms(system_time: &SystemTime) -> Option<u64> {
