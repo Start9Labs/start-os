13c0985f1a7095213a37165d5667bb118af5a212
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-09-29 17:46:10 -0600

    enable service worker config to work with ionic livereload

--- frontend/projects/ui/src/app/app.module.ts
+++ frontend/projects/ui/src/app/app.module.ts
@@ -65,13 +65,7 @@ import { environment } from '../environments/environment'
     DarkThemeModule,
     LightThemeModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: environment.useServiceWorker,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
