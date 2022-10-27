60e5ce76db893281de01940fc6b6e98e7082142a
BluJ <mogulslayer@gmail.com>
2022-10-27 16:01:11 -0600

    Make it so we skip rebuil.

--- backend/src/init.rs
+++ backend/src/init.rs
@@ -225,8 +225,7 @@ pub async fn init(cfg: &RpcContextConfig) -> Result<InitResult, Error> {
     let receipts = InitReceipts::new(&mut handle).await?;
 
     let should_rebuild = tokio::fs::metadata(SYSTEM_REBUILD_PATH).await.is_ok()
-        || &*receipts.server_version.get(&mut handle).await?
-            < &crate::version::Current::new().semver();
+        || &*receipts.server_version.get(&mut handle).await? < &emver::Version::new(0, 3, 2, 0);
 
     let song = if should_rebuild {
         Some(NonDetachingJoinHandle::from(tokio::spawn(async {
