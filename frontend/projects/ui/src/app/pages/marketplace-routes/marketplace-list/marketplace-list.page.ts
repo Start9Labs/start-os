57cb24f4caa278888868b6facf3a43eb15fc32cf
Matt Hill <MattDHill@users.noreply.github.com>
2022-10-27 22:56:40 -0600

    move marketplace settings into marketplace tab (#1895)
    * move marketplace settings into marketplace tab
    
    * Update frontend/projects/ui/src/app/modals/marketplace-settings/marketplace-settings.page.ts
    
    Co-authored-by: Lucy C <12953208+elvece@users.noreply.github.com>

--- frontend/projects/ui/src/app/pages/marketplace-routes/marketplace-list/marketplace-list.page.ts
+++ frontend/projects/ui/src/app/pages/marketplace-routes/marketplace-list/marketplace-list.page.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core'
+import { ModalController } from '@ionic/angular'
 import { AbstractMarketplaceService } from '@start9labs/marketplace'
 import { PatchDB } from 'patch-db-client'
 import { filter, map } from 'rxjs'
+import { MarketplaceSettingsPage } from 'src/app/modals/marketplace-settings/marketplace-settings.page'
 import { MarketplaceService } from 'src/app/services/marketplace.service'
 import { DataModel } from 'src/app/services/patch-db/data-model'
 
@@ -67,10 +69,21 @@ export class MarketplaceListPage {
     private readonly patch: PatchDB<DataModel>,
     @Inject(AbstractMarketplaceService)
     private readonly marketplaceService: MarketplaceService,
+    private readonly modalCtrl: ModalController,
   ) {}
 
   category = 'featured'
   query = ''
+
+  async presentModalMarketplaceSettings() {
+    const modal = await this.modalCtrl.create({
+      component: MarketplaceSettingsPage,
+    })
+    modal.onDidDismiss().then(res => {
+      console.log(res)
+    })
+    await modal.present()
+  }
 
   onCategoryChange(category: string): void {
     this.category = category
