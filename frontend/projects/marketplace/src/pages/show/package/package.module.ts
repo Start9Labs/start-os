9ed2e2b0ca7c82391fcb47100abb4e70ca457190
Alex Inkin <alexander@inkin.ru>
2022-11-11 22:08:13 +0300

    feat(shared): `Ticker` add new component and use it in marketplace (#1940)

--- frontend/projects/marketplace/src/pages/show/package/package.module.ts
+++ frontend/projects/marketplace/src/pages/show/package/package.module.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { IonicModule } from '@ionic/angular'
-import { EmverPipesModule, SharedPipesModule } from '@start9labs/shared'
+import {
+  EmverPipesModule,
+  SharedPipesModule,
+  TickerModule,
+} from '@start9labs/shared'
 
 import { PackageComponent } from './package.component'
 
 @NgModule({
-  imports: [CommonModule, IonicModule, SharedPipesModule, EmverPipesModule],
+  imports: [
+    CommonModule,
+    IonicModule,
+    SharedPipesModule,
+    EmverPipesModule,
+    TickerModule,
+  ],
   declarations: [PackageComponent],
   exports: [PackageComponent],
 })
