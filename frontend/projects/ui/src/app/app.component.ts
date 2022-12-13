5c6b44a5deb8ffac1ea1923a6d1c655e4cff0c07
waterplea <alexander@inkin.ru>
2022-12-13 12:25:38 +0300

    chore: remove unused code

--- frontend/projects/ui/src/app/app.component.ts
+++ frontend/projects/ui/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { PatchMonitorService } from './services/patch-monitor.service'
 import { ConnectionService } from './services/connection.service'
 import { Title } from '@angular/platform-browser'
 import { ServerNameService } from './services/server-name.service'
-import { createGesture } from '@ionic/angular'
 
 @Component({
   selector: 'app-root',
@@ -32,35 +31,6 @@ export class AppComponent implements OnDestroy {
     this.serverNameService.name$.subscribe(({ current }) =>
       this.titleService.setTitle(current),
     )
-  }
-
-  ngAfterViewInit() {
-    // You could also read the value of `--side-width`
-    // using `getComputedStyle` in an `onStart` callback
-    // let workingWidth = 400
-    // let startingWidth = 400
-    //
-    // const onMove = (ev: any) => {
-    //   requestAnimationFrame(() => {
-    //     workingWidth = startingWidth - ev.deltaX
-    //     rightMenu.style.setProperty('--side-width', `${workingWidth}px`)
-    //   })
-    // }
-    //
-    // const onEnd = () => {
-    //   startingWidth = workingWidth
-    // }
-    //
-    // const rightMenu = document.querySelector('.right')! as any
-    // const divider = document.querySelector('.divider')!
-    // const gesture = createGesture({
-    //   gestureName: 'resize-menu',
-    //   el: divider,
-    //   onEnd,
-    //   onMove,
-    // })
-    //
-    // gesture.enable(true)
   }
 
   splitPaneVisible({ detail }: any) {
