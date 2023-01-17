8317c6bbc758277fcad95bcae5d5d5b7ed792a0f
BluJ <mogulslayer@gmail.com>
2023-01-16 17:07:54 -0700

    chore: Add some checking

--- backend/src/s9pk/reader.rs
+++ backend/src/s9pk/reader.rs
@@ -24,6 +24,9 @@ use crate::s9pk::docker::DockerReader;
 use crate::util::Version;
 use crate::{Error, ResultExt};
 
+const MAX_REPLACES: usize = 10;
+const MAX_TITLE_LEN: usize = 30;
+
 #[pin_project::pin_project]
 #[derive(Debug)]
 pub struct ReadHandle<'a, R = File> {
@@ -231,6 +234,35 @@ impl<R: AsyncRead + AsyncSeek + Unpin + Send + Sync> S9pkReader<R> {
             &validated_image_ids,
         )?;
 
+<<<<<<< Updated upstream
+=======
+        #[cfg(feature = "js_engine")]
+        if man.containers.is_some()
+            || matches!(man.main, crate::procedure::PackageProcedure::Script(_))
+        {
+            return Err(Error::new(
+                eyre!("Right now we don't support the containers and the long running main"),
+                crate::ErrorKind::ValidateS9pk,
+            ));
+        }
+        if man
+            .replaces
+            .map(|x| x.len() >= MAX_REPLACES)
+            .unwrap_or(false)
+        {
+            return Err(Error::new(
+                eyre!("Cannot have more than {MAX_REPLACES} replaces"),
+                crate::ErrorKind::ValidateS9pk,
+            ));
+        }
+        if man.title.len() >= MAX_TITLE_LEN {
+            return Err(Error::new(
+                eyre!("Cannot have more than a length of {MAX_TITLE_LEN} for title"),
+                crate::ErrorKind::ValidateS9pk,
+            ));
+        }
+
+>>>>>>> Stashed changes
         if man.containers.is_some()
             && matches!(man.main, crate::procedure::PackageProcedure::Docker(_))
         {
