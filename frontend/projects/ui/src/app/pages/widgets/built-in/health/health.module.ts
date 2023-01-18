5d0696e2771788e15d0263710354c65dfa2e7dca
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-01-18 14:52:45 -0700

    update health widget with more accurate states and styling

--- frontend/projects/ui/src/app/pages/widgets/built-in/health/health.module.ts
+++ frontend/projects/ui/src/app/pages/widgets/built-in/health/health.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { HealthComponent } from './health.component'
-import { TuiPieChartModule } from '@taiga-ui/addon-charts'
+import { TuiRingChartModule } from '@taiga-ui/addon-charts'
 import { TuiHintModule } from '@taiga-ui/core'
 import { CommonModule } from '@angular/common'
 
 @NgModule({
-  imports: [CommonModule, TuiPieChartModule, TuiHintModule],
+  imports: [CommonModule, TuiRingChartModule, TuiHintModule],
   declarations: [HealthComponent],
   exports: [HealthComponent],
 })
