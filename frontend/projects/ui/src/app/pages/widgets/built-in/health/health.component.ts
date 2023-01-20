8742e8d21e3e1b0bc865ebcdc8dba9cb7dbeddb9
Lucy C <12953208+elvece@users.noreply.github.com>
2023-01-20 00:03:43 -0700

    update health widget with more accurate states and styling (#2127)

--- frontend/projects/ui/src/app/pages/widgets/built-in/health/health.component.ts
+++ frontend/projects/ui/src/app/pages/widgets/built-in/health/health.component.ts
@@ -5,6 +5,7 @@ import {
   PackageDataEntry,
   PackageMainStatus,
 } from 'src/app/services/patch-db/data-model'
+import { PrimaryStatus } from 'src/app/services/pkg-status-rendering.service'
 import { getPackageInfo } from '../../../../util/get-package-info'
 
 @Component({
@@ -14,26 +15,72 @@ import { getPackageInfo } from '../../../../util/get-package-info'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HealthComponent {
+  healthStates = {
+    transitioning: {
+      active: 0,
+      label: 'Transitioning',
+    },
+    warning: {
+      active: 0,
+      label: 'Needs Attention',
+    },
+    error: {
+      active: 0,
+      label: 'Error',
+    },
+    healthy: {
+      active: 0,
+      label: 'Healthy',
+    },
+    stopped: {
+      active: 0,
+      label: 'Stopped',
+    },
+  }
+  relevant: HealthDetails[] = []
+  labels: string[] = []
   readonly data$ = inject(PatchDB)
     .watch$('package-data')
     .pipe(
       map(data => {
         const pkgs = Object.values<PackageDataEntry>(data).map(getPackageInfo)
-        const transitioning = pkgs.filter(
+        this.healthStates.transitioning.active = pkgs.filter(
           ({ transitioning }) => transitioning,
         ).length
-        const error = pkgs.filter(({ error }) => error).length
-        const healthy = pkgs.length - transitioning - error
+        this.healthStates.error.active = pkgs.filter(
+          a => a.primaryStatus !== PrimaryStatus.Stopped && a.error,
+        ).length
+        this.healthStates.warning.active = pkgs.filter(
+          ({ warning }) => warning,
+        ).length
+        this.healthStates.stopped.active = pkgs.filter(
+          a => a.primaryStatus === PrimaryStatus.Stopped,
+        ).length
+        this.healthStates.healthy.active = Math.max(
+          0,
+          pkgs.length -
+            this.healthStates.transitioning.active -
+            this.healthStates.error.active -
+            this.healthStates.warning.active -
+            this.healthStates.stopped.active,
+        )
 
-        return [transitioning, error, healthy]
+        this.relevant = Object.values(this.healthStates).sort((a, b) =>
+          b.label.toLowerCase() > a.label.toLowerCase() ? -1 : 1,
+        )
+        this.labels = this.relevant.map(a => a.label)
+        return this.relevant.map(a => a.active)
       }),
     )
-
-  readonly labels = ['Transitioning', 'Error', 'Healthy']
 }
 
 function getStatus({
   installed,
 }: PackageDataEntry): PackageMainStatus | undefined {
   return installed?.status.main.status
+}
+
+interface HealthDetails {
+  active: number
+  label: string
 }
