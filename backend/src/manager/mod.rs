bd44926060b0e92ea09423112e14ea2b1188d98b
BluJ <mogulslayer@gmail.com>
2023-02-01 15:19:55 -0700

    wip: Moved over the config into the service manager

--- backend/src/manager/mod.rs
+++ backend/src/manager/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 use std::net::Ipv4Addr;
 use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 use std::sync::Arc;
@@ -10,9 +10,12 @@ use embassy_container_init::ProcessGroupId;
 use futures::future::BoxFuture;
 use futures::{FutureExt, TryFutureExt};
 use helpers::UnixRpcClient;
-use models::ErrorKind;
+use models::{ErrorKind, PackageId};
 use nix::sys::signal::Signal;
+use patch_db::DbHandle;
 use persistent_container::PersistentContainer;
+use rand::SeedableRng;
+use serde_json::Value;
 use start_stop::StartStop;
 use tokio::sync::{oneshot, Notify};
 use tokio::sync::{
@@ -25,9 +28,18 @@ use transition_state::TransitionState;
 
 use crate::backup::target::PackageBackupInfo;
 use crate::backup::PackageBackupReport;
+use crate::config::action::ConfigRes;
+use crate::config::spec::ValueSpecPointer;
+use crate::config::{not_found, ConfigReceipts, ConfigureContext};
 use crate::context::RpcContext;
+use crate::db::model::{CurrentDependencies, CurrentDependencyInfo};
+use crate::dependencies::{
+    add_dependent_to_current_dependents_lists, break_transitive, heal_all_dependents_transitive,
+    DependencyError, DependencyErrors, TaggedDependencyError,
+};
 use crate::disk::mount::backup::BackupMountGuard;
 use crate::disk::mount::guard::TmpMountGuard;
+use crate::install::cleanup::remove_from_current_dependents_lists;
 use crate::net::interface::InterfaceId;
 use crate::net::GeneratedCertificateMountPoint;
 use crate::procedure::docker::{DockerContainer, DockerProcedure, LongRunning};
@@ -150,8 +162,37 @@ impl Manager {
         }
         self._transition_replace(self._transition_restart());
     }
-    pub async fn configure(&self) -> Result<(), Error> {
-        todo!("BLUJ")
+    pub async fn configure(
+        &self,
+        configure_context: ConfigureContext,
+    ) -> Result<BTreeMap<PackageId, TaggedDependencyError>, Error> {
+        if self._is_transition_configure() {
+            return Ok(configure_context.breakages);
+        }
+        let context = self.seed.ctx.clone();
+        let id = self.seed.manifest.id.clone();
+
+        let (transition_state, done) = configure(context, id, configure_context).remote_handle();
+        self._transition_replace({
+            let seed = self.seed.clone();
+            let manage_container = self.manage_container.clone();
+
+            TransitionState::Configuring(tokio::spawn(async move {
+                let desired_state = manage_container.desired_state();
+                let state_reverter = DesiredStateReverter::new(manage_container.clone());
+                let starting_desired = desired_state.borrow().clone();
+                let mut current_state = manage_container.current_state();
+                manage_container.to_desired(StartStop::Stop);
+                while current_state.borrow().is_start() {
+                    current_state.changed().await;
+                }
+
+                transition_state.await;
+
+                state_reverter.revert().await;
+            }))
+        });
+        done.await
     }
     pub async fn backup(&self, backup_guard: BackupGuard) -> BackupReturn {
         if self._is_transition_backup() {
@@ -205,9 +246,13 @@ impl Manager {
             let mut current_state = manage_container.current_state();
             let _ = manage_container.set_override(Some(MainStatus::Restarting));
             manage_container.to_desired(StartStop::Stop);
-            while current_state.changed().await.is_ok() && current_state.borrow().is_start() {}
+            while current_state.borrow().is_start() {
+                current_state.changed().await;
+            }
             manage_container.to_desired(StartStop::Start);
-            while current_state.changed().await.is_ok() && current_state.borrow().is_stop() {}
+            while current_state.borrow().is_stop() {
+                current_state.changed().await;
+            }
             transition.send_replace(Default::default());
         }))
     }
@@ -231,8 +276,8 @@ impl Manager {
                         get_status(&mut tx, &seed.manifest).await?.backing_up(),
                     ));
                     manage_container.to_desired(StartStop::Stop);
-                    while current_state.changed().await.is_ok() && current_state.borrow().is_start()
-                    {
+                    while current_state.borrow().is_start() {
+                        current_state.changed().await;
                     }
 
                     let backup_guard = backup_guard.lock().await;
@@ -273,6 +318,283 @@ impl Manager {
         let transition = self.transition.borrow();
         matches!(**transition, TransitionState::BackingUp(_))
     }
+    fn _is_transition_configure(&self) -> bool {
+        let transition = self.transition.borrow();
+        matches!(**transition, TransitionState::Configuring(_))
+    }
+}
+
+#[instrument(skip(ctx, configure_context))]
+fn configure(
+    ctx: RpcContext,
+    id: PackageId,
+    mut configure_context: ConfigureContext,
+) -> BoxFuture<'static, Result<BTreeMap<PackageId, TaggedDependencyError>, Error>> {
+    async move {
+        let db = ctx.db.handle();
+        let tx = db.begin().await?;
+        let db = &mut tx;
+
+        let receipts = ConfigReceipts::new(db).await?;
+        let id = &id;
+        let ctx = &ctx;
+        let overrides = &mut configure_context.overrides;
+        // fetch data from db
+        let action = receipts
+            .config_actions
+            .get(db, id)
+            .await?
+            .ok_or_else(not_found)?;
+        let dependencies = receipts
+            .dependencies
+            .get(db, id)
+            .await?
+            .ok_or_else(not_found)?;
+        let volumes = receipts.volumes.get(db, id).await?.ok_or_else(not_found)?;
+        let is_needs_config = !receipts
+            .configured
+            .get(db, id)
+            .await?
+            .ok_or_else(not_found)?;
+        let version = receipts.version.get(db, id).await?.ok_or_else(not_found)?;
+
+        // get current config and current spec
+        let ConfigRes {
+            config: old_config,
+            spec,
+        } = action.get(ctx, id, &version, &volumes).await?;
+
+        // determine new config to use
+        let mut config =
+            if let Some(config) = configure_context.config.or_else(|| old_config.clone()) {
+                config
+            } else {
+                spec.gen(
+                    &mut rand::rngs::StdRng::from_entropy(),
+                    &configure_context.timeout,
+                )?
+            };
+
+        let manifest = receipts.manifest.get(db, id).await?.ok_or_else(not_found)?;
+
+        spec.validate(&manifest)?;
+        spec.matches(&config)?; // check that new config matches spec
+        spec.update(
+            ctx,
+            db,
+            &manifest,
+            overrides,
+            &mut config,
+            &receipts.config_receipts,
+        )
+        .await?; // dereference pointers in the new config
+
+        // create backreferences to pointers
+        let mut sys = receipts
+            .system_pointers
+            .get(db, &id)
+            .await?
+            .ok_or_else(not_found)?;
+        sys.truncate(0);
+        let mut current_dependencies: CurrentDependencies = CurrentDependencies(
+            dependencies
+                .0
+                .iter()
+                .filter_map(|(id, info)| {
+                    if info.requirement.required() {
+                        Some((id.clone(), CurrentDependencyInfo::default()))
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+        );
+        for ptr in spec.pointers(&config)? {
+            match ptr {
+                ValueSpecPointer::Package(pkg_ptr) => {
+                    if let Some(current_dependency) =
+                        current_dependencies.0.get_mut(pkg_ptr.package_id())
+                    {
+                        current_dependency.pointers.push(pkg_ptr);
+                    } else {
+                        current_dependencies.0.insert(
+                            pkg_ptr.package_id().to_owned(),
+                            CurrentDependencyInfo {
+                                pointers: vec![pkg_ptr],
+                                health_checks: BTreeSet::new(),
+                            },
+                        );
+                    }
+                }
+                ValueSpecPointer::System(s) => sys.push(s),
+            }
+        }
+        receipts.system_pointers.set(db, sys, &id).await?;
+
+        let signal = if !configure_context.dry_run {
+            // run config action
+            let res = action
+                .set(ctx, id, &version, &dependencies, &volumes, &config)
+                .await?;
+
+            // track dependencies with no pointers
+            for (package_id, health_checks) in res.depends_on.into_iter() {
+                if let Some(current_dependency) = current_dependencies.0.get_mut(&package_id) {
+                    current_dependency.health_checks.extend(health_checks);
+                } else {
+                    current_dependencies.0.insert(
+                        package_id,
+                        CurrentDependencyInfo {
+                            pointers: Vec::new(),
+                            health_checks,
+                        },
+                    );
+                }
+            }
+
+            // track dependency health checks
+            current_dependencies = current_dependencies.map(|x| {
+                x.into_iter()
+                    .filter(|(dep_id, _)| {
+                        if dep_id != id && !manifest.dependencies.0.contains_key(dep_id) {
+                            tracing::warn!("Illegal dependency specified: {}", dep_id);
+                            false
+                        } else {
+                            true
+                        }
+                    })
+                    .collect()
+            });
+            res.signal
+        } else {
+            None
+        };
+
+        // update dependencies
+        let prev_current_dependencies = receipts
+            .current_dependencies
+            .get(db, &id)
+            .await?
+            .unwrap_or_default();
+        remove_from_current_dependents_lists(
+            db,
+            id,
+            &prev_current_dependencies,
+            &receipts.current_dependents,
+        )
+        .await?; // remove previous
+        add_dependent_to_current_dependents_lists(
+            db,
+            id,
+            &current_dependencies,
+            &receipts.current_dependents,
+        )
+        .await?; // add new
+        current_dependencies.0.remove(id);
+        receipts
+            .current_dependencies
+            .set(db, current_dependencies.clone(), &id)
+            .await?;
+
+        let errs = receipts
+            .dependency_errors
+            .get(db, &id)
+            .await?
+            .ok_or_else(not_found)?;
+        tracing::warn!("Dependency Errors: {:?}", errs);
+        let errs = DependencyErrors::init(
+            ctx,
+            db,
+            &manifest,
+            &current_dependencies,
+            &receipts.dependency_receipt.try_heal,
+        )
+        .await?;
+        receipts.dependency_errors.set(db, errs, &id).await?;
+
+        // cache current config for dependents
+        configure_context
+            .overrides
+            .insert(id.clone(), config.clone());
+
+        // handle dependents
+        let dependents = receipts
+            .current_dependents
+            .get(db, id)
+            .await?
+            .ok_or_else(not_found)?;
+        let prev = if is_needs_config { None } else { old_config }
+            .map(Value::Object)
+            .unwrap_or_default();
+        let next = Value::Object(config.clone());
+        for (dependent, dep_info) in dependents.0.iter().filter(|(dep_id, _)| dep_id != &id) {
+            let dependent_container = receipts.docker_containers.get(db, &dependent).await?;
+            let dependent_container = &dependent_container;
+            // check if config passes dependent check
+            if let Some(cfg) = receipts
+                .manifest_dependencies_config
+                .get(db, (&dependent, &id))
+                .await?
+            {
+                let manifest = receipts
+                    .manifest
+                    .get(db, &dependent)
+                    .await?
+                    .ok_or_else(not_found)?;
+                if let Err(error) = cfg
+                    .check(
+                        ctx,
+                        dependent_container,
+                        dependent,
+                        &manifest.version,
+                        &manifest.volumes,
+                        id,
+                        &config,
+                    )
+                    .await?
+                {
+                    let dep_err = DependencyError::ConfigUnsatisfied { error };
+                    break_transitive(
+                        db,
+                        dependent,
+                        id,
+                        dep_err,
+                        &mut configure_context.breakages,
+                        &receipts.break_transitive_receipts,
+                    )
+                    .await?;
+                }
+
+                heal_all_dependents_transitive(ctx, db, id, &receipts.dependency_receipt).await?;
+            }
+        }
+
+        if let Some(signal) = signal {
+            match ctx.managers.get(&(id.clone(), version.clone())).await {
+                None => {
+                    // in theory this should never happen, which indicates this function should be moved behind the
+                    // Manager interface
+                    return Err(Error::new(
+                        eyre!("Manager Not Found for package being configured"),
+                        crate::ErrorKind::Incoherent,
+                    ));
+                }
+                Some(m) => {
+                    async move { m.signal(signal).await }.await?;
+                }
+            }
+        }
+        receipts.configured.set(db, true, &id).await?;
+
+        if configure_context.dry_run {
+            tx.abort().await?;
+        } else {
+            tx.commit().await?;
+        }
+
+        Ok(configure_context.breakages)
+    }
+    .boxed()
 }
 
 struct DesiredStateReverter {
@@ -290,9 +612,9 @@ impl DesiredStateReverter {
     }
     async fn revert(mut self) {
         if let Some(mut current_state) = self._revert() {
-            while current_state.changed().await.is_ok()
-                && &*current_state.borrow() != &self.starting_state
-            {}
+            while &*current_state.borrow() != &self.starting_state {
+                current_state.changed().await;
+            }
         }
     }
     fn _revert(&mut self) -> Option<watch::Receiver<StartStop>> {
@@ -396,7 +718,7 @@ async fn run_main(
     };
 
     if let Some(ip) = ip {
-        add_network_for_main(&seed, ip, interfaces, generated_certificate).await?;
+        add_network_for_main(&seed, ip, generated_certificate).await?;
     }
     started();
     let health = main_health_check_daemon(seed.clone());
@@ -481,10 +803,11 @@ async fn generate_certificate(
         TorSecretKeyV3,
     )>,
 ) -> Result<GeneratedCertificateMountPoint, Error> {
-    seed.ctx
-        .net_controller
-        .generate_certificate_mountpoint(&seed.manifest.id, interfaces)
-        .await
+    todo!()
+    // seed.ctx
+    //     .net_controller
+    //     .generate_certificate_mountpoint(&seed.manifest.id, interfaces)
+    //     .await
 }
 enum GetRunningIp {
     Ip(Ipv4Addr),
@@ -583,55 +906,6 @@ fn set_commit_health_true(state: &Arc<ManagerSharedState>) {
         .store(true, Ordering::SeqCst);
 }
 
-async fn add_network_for_main(
-    seed: &ManagerSeed,
-    ip: std::net::Ipv4Addr,
-    interfaces: Vec<(
-        InterfaceId,
-        &crate::net::interface::Interface,
-        TorSecretKeyV3,
-    )>,
-    generated_certificate: GeneratedCertificateMountPoint,
-) -> Result<(), Error> {
-    // seed.ctx
-    //     .net_controller
-    //     .add(
-    //         &mut seed.ctx.secret_store.acquire().await?,
-    //         &seed.manifest.id,
-    //         ip,
-    //         interfaces,
-    //         generated_certificate,
-    //     )
-    //     .await?;
-    todo!()
-}
-
-async fn remove_network_for_main(seed: &ManagerSeed, ip: std::net::Ipv4Addr) -> Result<(), Error> {
-    Ok(())
-}
-
-async fn main_health_check_daemon(seed: Arc<ManagerSeed>) {
-    tokio::time::sleep(Duration::from_secs(HEALTH_CHECK_GRACE_PERIOD_SECONDS)).await;
-    loop {
-        let mut db = seed.ctx.db.handle();
-        if let Err(e) = health::check(&seed.ctx, &mut db, &seed.manifest.id).await {
-            tracing::error!(
-                "Failed to run health check for {}: {}",
-                &seed.manifest.id,
-                e
-            );
-            tracing::debug!("{:?}", e);
-        }
-        tokio::time::sleep(Duration::from_secs(HEALTH_CHECK_COOLDOWN_SECONDS)).await;
-    }
-}
-
-fn set_commit_health_true(state: &Arc<ManagerSharedState>) {
-    state
-        .commit_health_check_results
-        .store(true, Ordering::SeqCst);
-}
-
 type RuntimeOfCommand = NonDetachingJoinHandle<Result<Result<NoOutput, (i32, String)>, Error>>;
 
 async fn get_running_ip(seed: &ManagerSeed, mut runtime: &mut RuntimeOfCommand) -> GetRunningIp {
@@ -683,28 +957,6 @@ async fn get_running_ip(seed: &ManagerSeed, mut runtime: &mut RuntimeOfCommand) 
                 }
             }
         }
-    }
-}
-
-async fn wait_for_status(shared: &ManagerSharedState, status: Status) {
-    let mut recv = shared.status.0.subscribe();
-    while {
-        let s = *recv.borrow();
-        s != status
-    } {
-        if recv.changed().await.is_ok() {
-            break;
-        }
-    }
-}
-
-fn sigterm_timeout(manifest: &Manifest) -> Option<Duration> {
-    if let PackageProcedure::Docker(d) = &manifest.main {
-        d.sigterm_timeout.map(|d| *d)
-    } else if let Some(c) = &manifest.containers {
-        c.main.sigterm_timeout.map(|d| *d)
-    } else {
-        None
     }
 }
 
