aa7540ebbd29638f9d987f7ec255dec96e3f9af6
Aiden McClelland <me@drbonez.dev>
2023-02-01 18:42:32 -0700

    make OsApi manager-based

--- libs/helpers/src/os_api.rs
+++ libs/helpers/src/os_api.rs
@@ -1,3 +1,4 @@
+use color_eyre::eyre::eyre;
 use models::Error;
 use models::PackageId;
 use serde_json::Value;
@@ -6,12 +7,22 @@ pub struct RuntimeDropped;
 
 pub type Callback = Box<dyn Fn(Value) -> Result<(), RuntimeDropped> + Send + Sync + 'static>; // bool indicating if
 
+fn method_not_available() -> Error {
+    Error::new(
+        eyre!("method not available"),
+        models::ErrorKind::InvalidRequest,
+    )
+}
+
 #[async_trait::async_trait]
+#[allow(unused_variables)]
 pub trait OsApi: Send + Sync + 'static {
     async fn get_service_config(
         &self,
         id: PackageId,
         path: &str,
         callback: Callback,
-    ) -> Result<Value, Error>;
+    ) -> Result<Value, Error> {
+        Err(method_not_available())
+    }
 }
