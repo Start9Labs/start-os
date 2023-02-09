af559edb95eadbde74333d502a36de74a521a370
BluJ <mogulslayer@gmail.com>
2023-02-09 11:16:55 -0700

    feat: Add in the ability to get configs

--- backend/src/manager/js_api.rs
+++ backend/src/manager/js_api.rs
@@ -1,11 +1,41 @@
-use color_eyre::{eyre::eyre, Report};
+use color_eyre::{
+    eyre::{bail, eyre},
+    Report,
+};
 use helpers::{AddressSchemaLocal, AddressSchemaOnion, Callback, OsApi};
 use models::{InterfaceId, PackageId};
+use serde_json::Value;
 use sqlx::Acquire;
 
 use crate::{manager::Manager, net::keys::Key};
 
 use super::try_get_running_ip;
+
+struct ConfigMapping(serde_json::Map<String, Value>);
+impl ConfigMapping {
+    fn with_path(&self, config_path: &ConfigPath) -> &Value {
+        let null_value = Value::Null;
+        let mut value: &Value = self.0.get(&config_path.paths[0]).unwrap_or(&null_value);
+        for path in config_path.paths.iter().skip(1) {
+            value = &value[&path];
+        }
+        value
+    }
+}
+struct ConfigPath {
+    paths: Vec<String>,
+}
+impl ConfigPath {
+    fn parse(value: &str) -> Self {
+        let paths = value
+            .split("/")
+            .into_iter()
+            .filter(|x| !x.is_empty())
+            .map(|x| x.to_string())
+            .collect();
+        Self { paths }
+    }
+}
 
 #[async_trait::async_trait]
 impl OsApi for Manager {
@@ -15,8 +45,36 @@ impl OsApi for Manager {
         path: &str,
         callback: Callback,
     ) -> Result<serde_json::Value, Report> {
-        todo!("BLUJ")
+        let found = match self
+            .seed
+            .manifest
+            .dependencies
+            .0
+            .iter()
+            .find(|x| x.0 == &id)
+        {
+            None => bail!("Cannot get a service that is not part of the dependencies"),
+            Some(a) => a,
+        };
+
+        let config = match crate::config::get(self.seed.ctx.clone(), id.clone(), None)
+            .await
+            .map(|x| x.config)
+        {
+            Ok(Some(a)) => ConfigMapping(a),
+            Ok(None) => bail!("No current config for the service"),
+            Err(err) => bail!("Could not fetch the config. {err}"),
+        };
+
+        let path = ConfigPath::parse(path);
+
+        let filtered_value = config.with_path(&path);
+        Ok(filtered_value.clone())
     }
+    // Get tor key - base 32
+
+    // Certificate + Certificate key for interface
+
     async fn bind_local(
         &self,
         internal_port: u16,
@@ -77,24 +135,6 @@ impl OsApi for Manager {
         tx.commit().await?;
         Ok(helpers::Address(key))
     }
-    async fn unbind_onion(&self, id: InterfaceId, external: u16) -> Result<(), Report> {
-        let ip = try_get_running_ip(&self.seed)
-            .await?
-            .ok_or_else(|| eyre!("No ip available"))?;
-        let mut svc = self
-            .seed
-            .ctx
-            .net_controller
-            .create_service(self.seed.manifest.id.clone(), ip)
-            .await
-            .map_err(|e| eyre!("Could not get to net controller: {e:?}"))?;
-        let mut secrets = self.seed.ctx.secret_store.acquire().await?;
-
-        svc.remove_tor(id, external)
-            .await
-            .map_err(|e| eyre!("Could not add to tor: {e:?}"))?;
-        Ok(())
-    }
     async fn unbind_local(&self, id: InterfaceId, external: u16) -> Result<(), Report> {
         let ip = try_get_running_ip(&self.seed)
             .await?
@@ -111,6 +151,24 @@ impl OsApi for Manager {
         svc.remove_lan(id, external)
             .await
             .map_err(|e| eyre!("Could not add to local: {e:?}"))?;
+        Ok(())
+    }
+    async fn unbind_onion(&self, id: InterfaceId, external: u16) -> Result<(), Report> {
+        let ip = try_get_running_ip(&self.seed)
+            .await?
+            .ok_or_else(|| eyre!("No ip available"))?;
+        let mut svc = self
+            .seed
+            .ctx
+            .net_controller
+            .create_service(self.seed.manifest.id.clone(), ip)
+            .await
+            .map_err(|e| eyre!("Could not get to net controller: {e:?}"))?;
+        let mut secrets = self.seed.ctx.secret_store.acquire().await?;
+
+        svc.remove_tor(id, external)
+            .await
+            .map_err(|e| eyre!("Could not add to tor: {e:?}"))?;
         Ok(())
     }
 }
