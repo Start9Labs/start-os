109b634925b5ce429b74ef739436499c6eac944f
waterplea <alexander@inkin.ru>
2023-03-21 14:46:04 +0800

    chore: break form-object file structure

---
+++ frontend/projects/ui/src/app/components/form-object/form-union/form-union.component.ts
@@ -0,0 +1,59 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { UntypedFormGroup } from '@angular/forms'
+import { v4 } from 'uuid'
+import { FormService } from 'src/app/services/form.service'
+import { ValueSpecUnion } from 'src/app/pkg-config/config-types'
+
+@Component({
+  selector: 'form-union',
+  templateUrl: './form-union.component.html',
+  styleUrls: ['./form-union.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormUnionComponent {
+  @Input() formGroup!: UntypedFormGroup
+  @Input() spec!: ValueSpecUnion
+  @Input() current?: Record<string, any>
+  @Input() original?: Record<string, any>
+
+  get unionValue() {
+    return this.formGroup.get(this.spec.tag.id)?.value
+  }
+
+  get isNew() {
+    return !this.original
+  }
+
+  get hasNewOptions() {
+    const tagId = this.spec.tag.id
+    return (
+      this.original?.[tagId] === this.current?.[tagId] &&
+      !!Object.keys(this.current || {}).find(
+        key => this.original![key] === undefined,
+      )
+    )
+  }
+
+  objectId = v4()
+
+  constructor(private readonly formService: FormService) {}
+
+  updateUnion(e: any): void {
+    const tagId = this.spec.tag.id
+
+    Object.keys(this.formGroup.controls).forEach(control => {
+      if (control === tagId) return
+      this.formGroup.removeControl(control)
+    })
+
+    const unionGroup = this.formService.getUnionObject(
+      this.spec as ValueSpecUnion,
+      e.detail.value,
+    )
+
+    Object.keys(unionGroup.controls).forEach(control => {
+      if (control === tagId) return
+      this.formGroup.addControl(control, unionGroup.controls[control])
+    })
+  }
+}
