f0b3702b53dd66679b10de6c7daa7b95d5ba2797
waterplea <alexander@inkin.ru>
2023-03-21 14:46:04 +0800

    chore: break form-object file structure

---
+++ frontend/projects/ui/src/app/components/form-object/form-label/form-label.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { AlertController } from '@ionic/angular'
+
+@Component({
+  selector: 'form-label',
+  templateUrl: './form-label.component.html',
+  styleUrls: ['./form-label.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormLabelComponent {
+  @Input() data!: {
+    name: string
+    new: boolean
+    edited: boolean
+    description?: string
+    required?: boolean
+    newOptions?: boolean
+  }
+
+  constructor(private readonly alertCtrl: AlertController) {}
+
+  async presentAlertDescription() {
+    const alert = await this.alertCtrl.create({
+      header: this.data.name,
+      message: this.data.description,
+      buttons: [
+        {
+          text: 'OK',
+          cssClass: 'enter-click',
+        },
+      ],
+    })
+    await alert.present()
+  }
+}
