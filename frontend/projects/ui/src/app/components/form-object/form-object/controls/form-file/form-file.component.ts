536d52a9a75d9808e379524163b2e93661985f50
waterplea <alexander@inkin.ru>
2023-03-22 17:05:24 +0800

    chore: break down form-object by type, part 1

---
+++ frontend/projects/ui/src/app/components/form-object/form-object/controls/form-file/form-file.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core'
+import { AbstractControl } from '@angular/forms'
+import { ValueSpecOf } from 'start-sdk/types/config-types'
+
+@Component({
+  selector: 'form-file',
+  templateUrl: './form-file.component.html',
+  styleUrls: ['./form-file.component.scss'],
+})
+export class FormFileComponent {
+  @Input() spec!: ValueSpecOf<'file'>
+  @Input() control!: AbstractControl
+
+  handleFileInput(e: any) {
+    this.control.patchValue(e.target.files[0])
+  }
+
+  clearFile() {
+    this.control.patchValue(null)
+  }
+}
