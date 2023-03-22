39a4628afd8fb78e493c60069c6f16b88d183197
waterplea <alexander@inkin.ru>
2023-03-22 17:05:24 +0800

    chore: break down form-object by type, part 1

---
+++ frontend/projects/ui/src/app/components/form-object/form-warning.directive.ts
@@ -0,0 +1,51 @@
+import { Directive } from '@angular/core'
+import { ValueSpec, ValueSpecUnion } from 'start-sdk/types/config-types'
+import { AlertButton, AlertController } from '@ionic/angular'
+
+@Directive({
+  selector: '[formWarning]',
+  exportAs: 'formWarning',
+})
+export class FormWarningDirective {
+  private warned = false
+
+  constructor(private readonly alertCtrl: AlertController) {}
+
+  async onChange<T extends ValueSpec>(
+    key: string,
+    spec: T extends ValueSpecUnion ? never : T,
+    okFn?: Function,
+    cancelFn?: Function,
+  ) {
+    if (!spec.warning || this.warned) return okFn ? okFn() : null
+
+    this.warned = true
+
+    const buttons: AlertButton[] = [
+      {
+        text: 'Ok',
+        handler: () => {
+          if (okFn) okFn()
+        },
+        cssClass: 'enter-click',
+      },
+    ]
+
+    if (okFn || cancelFn) {
+      buttons.unshift({
+        text: 'Cancel',
+        handler: () => {
+          if (cancelFn) cancelFn()
+        },
+      })
+    }
+
+    const alert = await this.alertCtrl.create({
+      header: 'Warning',
+      subHeader: `Editing ${spec.name} has consequences:`,
+      message: spec.warning,
+      buttons,
+    })
+    await alert.present()
+  }
+}
