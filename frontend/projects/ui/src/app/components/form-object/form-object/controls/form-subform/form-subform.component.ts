4374b89efd138057bf3c6494241f80dbaa9b1e75
waterplea <alexander@inkin.ru>
2023-03-23 17:54:36 +0800

    chore: break down form-object by type, part 2

---
+++ frontend/projects/ui/src/app/components/form-object/form-object/controls/form-subform/form-subform.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { AbstractControl } from '@angular/forms'
+import { ValueSpecOf } from 'start-sdk/types/config-types'
+
+@Component({
+  selector: 'form-subform',
+  templateUrl: './form-subform.component.html',
+  styleUrls: ['./form-subform.component.scss'],
+})
+export class FormSubformComponent {
+  @Input() spec!: ValueSpecOf<'object'>
+  @Input() control!: AbstractControl
+  @Input() hasNewOptions = false
+
+  expanded = false
+}
