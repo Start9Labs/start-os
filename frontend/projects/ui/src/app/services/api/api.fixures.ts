c2a59d1eab15dbd039e267dab014bc1cf147080e
Matt Hill <matthewonthemoon@gmail.com>
2023-04-03 14:16:41 -0600

    remove subtype from list spec

--- frontend/projects/ui/src/app/services/api/api.fixures.ts
+++ frontend/projects/ui/src/app/services/api/api.fixures.ts
@@ -745,7 +745,6 @@ export module Mock {
                 },
                 'p2p-port': {
                   type: 'number',
-                  inputmode: 'numeric',
                   name: 'P2P Port',
                   description:
                     'The port that your Bitcoin Core P2P server is bound to',
@@ -865,7 +864,6 @@ export module Mock {
     'object-list': {
       name: 'Object List',
       type: 'list',
-      subtype: 'object',
       description: 'This is a list of objects, like users or something',
       warning: null,
       range: '[0,4]',
@@ -884,6 +882,7 @@ export module Mock {
       // the outer spec here, at the list level, says that what's inside (the inner spec) pertains to its inner elements.
       // it just so happens that ValueSpecObject's have the field { spec: InputSpec }
       spec: {
+        type: 'object',
         uniqueBy: 'last-name',
         displayAs: `I'm {{last-name}}, {{first-name}} {{last-name}}`,
         spec: {
@@ -918,7 +917,6 @@ export module Mock {
           },
           age: {
             name: 'Age',
-            inputmode: 'numeric',
             type: 'number',
             description: 'The age of the user',
             required: false,
@@ -962,7 +960,6 @@ export module Mock {
     },
     'favorite-number': {
       name: 'Favorite Number',
-      inputmode: 'decimal',
       type: 'number',
       integral: false,
       description: 'Your favorite number of all time',
@@ -977,11 +974,10 @@ export module Mock {
     'unlucky-numbers': {
       name: 'Unlucky Numbers',
       type: 'list',
-      subtype: 'number',
       description: 'Numbers that you like but are not your top favorite.',
       warning: null,
       spec: {
-        inputmode: 'decimal',
+        type: 'number',
         integral: false,
         range: '[-100,200)',
         units: null,
@@ -1033,12 +1029,12 @@ export module Mock {
         rulemakers: {
           name: 'Rule Makers',
           type: 'list',
-          subtype: 'object',
           description: 'the people who make the rules',
           warning: null,
           range: '[0,2]',
           default: [],
           spec: {
+            type: 'object',
             uniqueBy: null,
             displayAs: null,
             spec: {
@@ -1184,7 +1180,6 @@ export module Mock {
     },
     port: {
       name: 'Port',
-      inputmode: 'numeric',
       type: 'number',
       integral: true,
       description:
@@ -1213,7 +1208,6 @@ export module Mock {
     rpcallowip: {
       name: 'RPC Allowed IPs',
       type: 'list',
-      subtype: 'string',
       description:
         'external ip addresses that are authorized to access your Bitcoin node',
       warning:
@@ -1221,6 +1215,7 @@ export module Mock {
       range: '[1,10]',
       default: ['192.168.1.1'],
       spec: {
+        type: 'string',
         inputmode: 'text',
         masked: false,
         placeholder: null,
@@ -1232,12 +1227,12 @@ export module Mock {
     rpcauth: {
       name: 'RPC Auth',
       type: 'list',
-      subtype: 'string',
       description: 'api keys that are authorized to access your Bitcoin node.',
       warning: null,
       range: '[0,*)',
       default: [],
       spec: {
+        type: 'string',
         inputmode: 'text',
         masked: false,
         placeholder: null,
@@ -1306,12 +1301,12 @@ export module Mock {
                 law3: {
                   name: 'Third Law',
                   type: 'list',
-                  subtype: 'object',
                   description: 'the third law',
                   warning: null,
                   range: '[0,2]',
                   default: [],
                   spec: {
+                    type: 'object',
                     uniqueBy: null,
                     displayAs: null,
                     spec: {
@@ -1367,12 +1362,12 @@ export module Mock {
             rulemakers: {
               name: 'Rule Makers',
               type: 'list',
-              subtype: 'object',
               description: 'the people who make the rules',
               warning: null,
               range: '[0,2]',
               default: [],
               spec: {
+                type: 'object',
                 uniqueBy: null,
                 displayAs: null,
                 spec: {
