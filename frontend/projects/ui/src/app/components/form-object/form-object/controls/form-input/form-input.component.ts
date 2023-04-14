7a4d53b0b453934e22496c729a628a653a3b2ea3
Alex Inkin <alexander@inkin.ru>
2023-04-14 21:52:21 +0800

    feat: use Taiga UI for config modal (#2250)
    * feat: use Taiga UI for config modal
    
    * chore: finish remaining changes
    
    * chore: address comments
    
    * bump sdk version
    
    ---------
    
    Co-authored-by: Matt Hill <matthewonthemoon@gmail.com>

--- frontend/projects/ui/src/app/components/form-object/form-object/controls/form-input/form-input.component.ts
+++ frontend/projects/ui/src/app/components/form-object/form-object/controls/form-input/form-input.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, inject, Output, EventEmitter } from '@angular/core'
 import { FormControl } from '@angular/forms'
-import { ValueSpecOf } from 'start-sdk/lib/config/config-types'
+import { ValueSpecOf } from 'start-sdk/lib/config/configTypes'
 import { THEME } from '@start9labs/shared'
 
 @Component({
