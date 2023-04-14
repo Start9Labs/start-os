michelkk96/embassy-os

integration/refactors
wip: Refactoring the service

-> Made new skeleton
-> Added service manager
-> Manager Refactored
-> Cleanup
-> Add gid struct
-> remove synchronizer
-> Added backup into manager
-> Fix the configure signal not send
-> Fixes around backup and sync

wip: Moved over the config into the service manager

js effect for subscribing to config

js effect for subscribing to config

fix errors

chore: Fix some things in the manager for clippy

add interfaces from manifest automatically

make OsApi manager-based

wip: Starting down the bind for the effects

todo: complete a ip todo

chore: Fix the result type on something

todo: Address returning

chore: JS with callbacks

chore: Add in the chown and permissions

chore: Add in the binds and unbinds in

feat: Add in the ability to get configs

makefile changes

add start/stop/restart to effects

config hooks

fix: add a default always to the get status

chore: Only do updates when the thing is installed.

use nistp256 to satisfy firefox

use ed25519 if available

chore: Make the thing buildable for testing

chore: Add in the debugging

fix ip signing

chore: Remove the bluj tracing

fix SQL error

chore: Fix the build

update prettytable to fix segfault

Chore: Make these fn's instead of allways ran.

chore: Fix the testing

fix: The stopping/ restarting service

fix: Fix the restarting.

remove current-dependents, derive instead

remove pointers from current-dependencies

remove pointers and system pointers from FE

v0.3.4

remove health checks from manifest

remove "restarting" bool on "starting" status

remove restarting attr

update makefile

fix

add efi support

fix efi

add redirect if connecting to https over http

clean up

lan port forwarding

add `make update` and `make update-overlay`

fix migration

more protections

fix: Fix a lint

chore: remove the limit on the long-running

fix: Starting sometimes.

fix: Make it so the stop of the main works

fix: Bind local and tor with package.

wip: env

backend/src/version/mod.rs
@@ -35,15 +35,7 @@ impl Version {
     #[cfg(test)]
     fn as_sem_ver(&self) -> emver::Version {
         match self {
-            Version::V0_3_0(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_3(Wrapper(x)) => x.semver(),
-            Version::V0_3_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_1_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_1_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_2_1(Wrapper(x)) => x.semver(),
+            Version::LT0_3_3(LTWrapper(_, x)) => x.clone(),
             Version::V0_3_3(Wrapper(x)) => x.semver(),
             Version::V0_3_4(Wrapper(x)) => x.semver(),
             Version::Other(x) => x.clone(),
@@ -195,7 +187,7 @@ where
         let v = crate::util::Version::deserialize(deserializer)?;
         let version = T::new();
         if *v == version.semver() {
-            Ok(Wrapper(version))
+            Ok(Self(version))
         } else {
             Err(serde::de::Error::custom("Mismatched Version"))
         }
@@ -255,15 +247,14 @@ mod tests {
 
     fn versions() -> impl Strategy<Value = Version> {
         prop_oneof![
-            Just(Version::V0_3_0(Wrapper(v0_3_0::Version::new()))),
-            Just(Version::V0_3_0_1(Wrapper(v0_3_0_1::Version::new()))),
-            Just(Version::V0_3_0_2(Wrapper(v0_3_0_2::Version::new()))),
-            Just(Version::V0_3_0_3(Wrapper(v0_3_0_3::Version::new()))),
-            Just(Version::V0_3_1(Wrapper(v0_3_1::Version::new()))),
-            Just(Version::V0_3_1_1(Wrapper(v0_3_1_1::Version::new()))),
-            Just(Version::V0_3_1_2(Wrapper(v0_3_1_2::Version::new()))),
-            Just(Version::V0_3_2(Wrapper(v0_3_2::Version::new()))),
-            Just(Version::V0_3_2_1(Wrapper(v0_3_2_1::Version::new()))),
+            em_version().prop_map(|v| if v < v0_3_3::Version::new().semver() {
+                Version::LT0_3_3(LTWrapper(v0_3_3::Version::new(), v))
+            } else {
+                Version::LT0_3_3(LTWrapper(
+                    v0_3_3::Version::new(),
+                    emver::Version::new(0, 3, 0, 0),
+                ))
+            }),
             Just(Version::V0_3_3(Wrapper(v0_3_3::Version::new()))),
             Just(Version::V0_3_4(Wrapper(v0_3_4::Version::new()))),
             em_version().prop_map(Version::Other),

Start9Labs/start-os

backend/src/version/mod.rs
@@ -35,15 +35,7 @@ impl Version {
     #[cfg(test)]
     fn as_sem_ver(&self) -> emver::Version {
         match self {
-            Version::V0_3_0(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_0_3(Wrapper(x)) => x.semver(),
-            Version::V0_3_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_1_1(Wrapper(x)) => x.semver(),
-            Version::V0_3_1_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_2(Wrapper(x)) => x.semver(),
-            Version::V0_3_2_1(Wrapper(x)) => x.semver(),
+            Version::LT0_3_3(LTWrapper(_, x)) => x.clone(),
             Version::V0_3_3(Wrapper(x)) => x.semver(),
             Version::V0_3_4(Wrapper(x)) => x.semver(),
             Version::V0_3_4_1(Wrapper(x)) => x.semver(),
@@ -196,7 +188,7 @@ where
         let v = crate::util::Version::deserialize(deserializer)?;
         let version = T::new();
         if *v == version.semver() {
-            Ok(Wrapper(version))
+            Ok(Self(version))
         } else {
             Err(serde::de::Error::custom("Mismatched Version"))
         }
@@ -256,6 +248,14 @@ mod tests {
 
     fn versions() -> impl Strategy<Value = Version> {
         prop_oneof![
+            em_version().prop_map(|v| if v < v0_3_4_1::Version::new().semver() {
+                Version::LT0_3_4_1(LTWrapper(v0_3_4_1::Version::new(), v))
+            } else {
+                Version::LT0_3_4_1(LTWrapper(
+                    v0_3_4_1::Version::new(),
+                    emver::Version::new(0, 3, 0, 0),
+                ))
+            }),
             Just(Version::V0_3_4_1(Wrapper(v0_3_4_1::Version::new()))),
             em_version().prop_map(Version::Other),
         ]