0d8470a878ddd8794fb51817f62df8730c1ae30b
Matt Hill <matthewonthemoon@gmail.com>
2023-04-21 09:21:35 -0600

    update form to latest sdk

---
+++ frontend/projects/ui/src/app/components/form/form-toggle/form-toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { ValueSpecToggle } from 'start-sdk/lib/config/configTypes'
+import { Control } from '../control'
+
+@Component({
+  selector: 'form-toggle',
+  templateUrl: './form-toggle.component.html',
+  styleUrls: ['./form-toggle.component.scss'],
+})
+export class FormToggleComponent extends Control<ValueSpecToggle, boolean> {}
