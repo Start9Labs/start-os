048175cf3f48eea74f4faf1f1ed59148effac532
Aiden McClelland <me@drbonez.dev>
2023-05-01 16:01:04 -0600

    pass through OS_ARCH on compat build

--- system-images/compat/build.sh
+++ system-images/compat/build.sh
@@ -3,6 +3,11 @@
 set -e
 shopt -s expand_aliases
 
+if [ -z "$OS_ARCH" ]; then
+	>&2 echo '$OS_ARCH is required'
+	exit 1
+fi
+
 if [ "$0" != "./build.sh" ]; then
 	>&2 echo "Must be run from compat directory"
 	exit 1
@@ -13,8 +18,8 @@ if tty -s; then
 	USE_TTY="-it"
 fi
 
-alias 'rust-arm64-musl-builder'='docker run $USE_TTY --rm -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src messense/rust-musl-cross:aarch64-musl'
-alias 'rust-x86_64-musl-builder'='docker run $USE_TTY --rm -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl'
+alias 'rust-arm64-musl-builder'='docker run $USE_TTY --rm -e "OS_ARCH=$OS_ARCH" -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src messense/rust-musl-cross:aarch64-musl'
+alias 'rust-x86_64-musl-builder'='docker run $USE_TTY --rm -e "OS_ARCH=$OS_ARCH" -v "$HOME"/.cargo/registry:/root/.cargo/registry -v "$(pwd)":/home/rust/src messense/rust-musl-cross:x86_64-musl'
 
 cd ../..
 rust-arm64-musl-builder sh -c "(git config --global --add safe.directory '*'; cd system-images/compat && cargo build --release --target=aarch64-unknown-linux-musl --no-default-features)"
