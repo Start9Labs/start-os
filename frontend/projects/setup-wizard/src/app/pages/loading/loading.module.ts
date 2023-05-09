873f2b2814dff9ac3ffb490ede4fc033249c59d6
Matt Hill <MattDHill@users.noreply.github.com>
2023-05-09 14:58:50 -0600

    Add logs window to setup wizard loading screen (#2076)
    * add logs window to setup wizard loading screen
    
    * fix type error
    
    * Update frontend/projects/setup-wizard/src/app/services/api/live-api.service.ts
    
    Co-authored-by: Lucy C <12953208+elvece@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lucy C <12953208+elvece@users.noreply.github.com>

--- frontend/projects/setup-wizard/src/app/pages/loading/loading.module.ts
+++ frontend/projects/setup-wizard/src/app/pages/loading/loading.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 import { IonicModule } from '@ionic/angular'
 import { FormsModule } from '@angular/forms'
 import { LoadingPage, ToMessagePipe } from './loading.page'
+import { LogsWindowComponent } from './logs-window/logs-window.component'
 import { LoadingPageRoutingModule } from './loading-routing.module'
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, LoadingPageRoutingModule],
-  declarations: [LoadingPage, ToMessagePipe],
+  declarations: [LoadingPage, ToMessagePipe, LogsWindowComponent],
 })
 export class LoadingPageModule {}
