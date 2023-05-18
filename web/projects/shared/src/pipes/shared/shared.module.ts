e964456485bb2887b661df77bf3c587e99938db2
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-05-18 13:50:48 -0600

    cleanup and remove unnecessary sort pipe

--- web/projects/shared/src/pipes/shared/shared.module.ts
+++ web/projects/shared/src/pipes/shared/shared.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core'
 import { IncludesPipe } from './includes.pipe'
 import { EmptyPipe } from './empty.pipe'
 import { TrustUrlPipe } from './trust.pipe'
-import { SortPipe } from './sort.pipe'
 
 @NgModule({
-  declarations: [IncludesPipe, EmptyPipe, TrustUrlPipe, SortPipe],
-  exports: [IncludesPipe, EmptyPipe, TrustUrlPipe, SortPipe],
+  declarations: [IncludesPipe, EmptyPipe, TrustUrlPipe],
+  exports: [IncludesPipe, EmptyPipe, TrustUrlPipe],
 })
 export class SharedPipesModule {}
