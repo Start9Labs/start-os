8f3e427234de4df4017630b0df6f5180d89054cf
waterplea <alexander@inkin.ru>
2023-05-22 19:53:55 +0400

    chore: add generate to textarea and implement immutable

--- frontend/projects/ui/src/app/components/form/form-textarea/form-textarea.component.ts
+++ frontend/projects/ui/src/app/components/form/form-textarea/form-textarea.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core'
 import { ValueSpecTextarea } from '@start9labs/start-sdk/lib/config/configTypes'
 import { Control } from '../control'
+import { getDefaultString } from '../../../util/config-utilities'
 
 @Component({
   selector: 'form-textarea',
   templateUrl: './form-textarea.component.html',
 })
-export class FormTextareaComponent extends Control<ValueSpecTextarea, string> {}
+export class FormTextareaComponent extends Control<ValueSpecTextarea, string> {
+  generate() {
+    this.value = getDefaultString(this.spec.generate || '')
+  }
+}
