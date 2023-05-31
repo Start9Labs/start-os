e7bc377c8366c8bf197487af1ef249b94316bb10
Matt Hill <MattDHill@users.noreply.github.com>
2023-05-31 12:29:48 -0600

    Feat/credentials (#2290)
    add credentials and remove properties

--- frontend/projects/ui/src/app/services/api/embassy-api.service.ts
+++ frontend/projects/ui/src/app/services/api/embassy-api.service.ts
@@ -234,9 +234,9 @@ export abstract class ApiService {
 
   // package
 
-  abstract getPackageProperties(
-    params: RR.GetPackagePropertiesReq,
-  ): Promise<RR.GetPackagePropertiesRes<2>['data']>
+  abstract getPackageCredentials(
+    params: RR.GetPackageCredentialsReq,
+  ): Promise<RR.GetPackageCredentialsRes>
 
   abstract getPackageLogs(
     params: RR.GetPackageLogsReq,
