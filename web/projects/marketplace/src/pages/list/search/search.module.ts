f61366995e84acf03483787127fcf6b2ebf01e24
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-06-07 14:01:05 -0600

    formatting

--- web/projects/marketplace/src/pages/list/search/search.module.ts
+++ web/projects/marketplace/src/pages/list/search/search.module.ts
@@ -1,13 +1,11 @@
-import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { IonicModule } from '@ionic/angular'
-import { ResponsiveColDirective } from '@start9labs/shared'
-
-import { SearchComponent } from './search.component'
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ResponsiveColDirective } from "@start9labs/shared";
+import { SearchComponent } from "./search.component";
 
 @NgModule({
-  imports: [IonicModule, FormsModule, CommonModule, ResponsiveColDirective],
+  imports: [FormsModule, CommonModule, ResponsiveColDirective],
   declarations: [SearchComponent],
   exports: [SearchComponent],
 })
