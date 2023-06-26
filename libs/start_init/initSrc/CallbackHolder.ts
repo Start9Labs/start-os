bfc846b2f7cc4bcdc429004accaac188e0c88610
BluJ <mogulslayer@gmail.com>
2023-06-26 14:34:06 -0600

    feat: Add in the removal of a callback after a call

--- libs/start_init/initSrc/CallbackHolder.ts
+++ libs/start_init/initSrc/CallbackHolder.ts
@@ -4,12 +4,19 @@ export class CallbackHolder {
     constructor() {
 
     }
-    private callbacks: Function[] = []
-
-    addCallback(callback: Function) {
-        return this.callbacks.push(callback) - 1
+    private root = (Math.random() + 1).toString(36).substring(7);
+    private inc = 0
+    private callbacks = new Map<string, Function>()
+    private newId() {
+        return this.root + (this.inc++).toString(36)
     }
-    callCallback(index: number, args: any[]): Promise<unknown> {
-        return Promise.resolve().then(() => this.callbacks[index](...args))
+    addCallback(callback: Function) {
+        return this.callbacks.set(this.newId(), callback);
+    }
+    callCallback(index: string, args: any[]): Promise<unknown> {
+        const callback = this.callbacks.get(index)
+        if (!callback) throw new Error(`Callback ${index} does not exist`)        
+        this.callbacks.delete(index)
+        return Promise.resolve().then(() => callback(...args))
     }
 }
