dc04b945ab47ec583c0b51c61e9bc3e341ae95e4
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-08-10 17:46:34 -0400

    add and style additional details

--- web/projects/marketplace/src/pages/show/additional/additional.module.ts
+++ web/projects/marketplace/src/pages/show/additional/additional.module.ts
@@ -1,26 +1,29 @@
-import { CommonModule } from '@angular/common'
-import { NgModule } from '@angular/core'
-import { IonicModule } from '@ionic/angular'
-import { MarkdownModule, ResponsiveColDirective } from '@start9labs/shared'
-
-import { AdditionalComponent } from './additional.component'
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MarkdownModule, ResponsiveColDirective } from "@start9labs/shared";
+import { AdditionalComponent } from "./additional.component";
 import {
   TuiRadioListModule,
   TuiStringifyContentPipeModule,
-} from '@taiga-ui/kit'
-import { FormsModule } from '@angular/forms'
-import { TuiButtonModule } from '@taiga-ui/core'
+  TuiTagModule,
+} from "@taiga-ui/kit";
+import { FormsModule } from "@angular/forms";
+import { TuiButtonModule, TuiLabelModule, TuiSvgModule } from "@taiga-ui/core";
+import { AdditionalLinkModule } from "./additional-link/additional-link.component.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule,
     MarkdownModule,
     ResponsiveColDirective,
     TuiRadioListModule,
     FormsModule,
     TuiStringifyContentPipeModule,
     TuiButtonModule,
+    TuiLabelModule,
+    TuiSvgModule,
+    TuiTagModule,
+    AdditionalLinkModule,
   ],
   declarations: [AdditionalComponent],
   exports: [AdditionalComponent],
