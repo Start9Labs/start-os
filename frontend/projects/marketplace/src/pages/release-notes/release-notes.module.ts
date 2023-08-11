94c3e3a9dbc93b1e0649cb96fb59a10a9d110801
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-08-11 19:48:36 -0400

    implement release notes modal

--- frontend/projects/marketplace/src/pages/release-notes/release-notes.module.ts
+++ frontend/projects/marketplace/src/pages/release-notes/release-notes.module.ts
@@ -1,29 +1,26 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { IonicModule } from '@ionic/angular'
 import {
   EmverPipesModule,
   MarkdownPipeModule,
   SafeLinksModule,
   TextSpinnerComponentModule,
 } from '@start9labs/shared'
-import { TuiElementModule } from '@taiga-ui/cdk'
 import { NgDompurifyModule } from '@tinkoff/ng-dompurify'
-
 import { ReleaseNotesComponent } from './release-notes.component'
 import { TuiButtonModule } from '@taiga-ui/core'
+import { TuiAccordionModule } from '@taiga-ui/kit'
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule,
     TextSpinnerComponentModule,
     EmverPipesModule,
     MarkdownPipeModule,
-    TuiElementModule,
     NgDompurifyModule,
     SafeLinksModule,
     TuiButtonModule,
+    TuiAccordionModule,
   ],
   declarations: [ReleaseNotesComponent],
   exports: [ReleaseNotesComponent],
