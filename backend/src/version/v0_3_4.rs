a1d75005a97eebe90b582aeddcc9ae9b335c8d1a
J H <2364004+Blu-J@users.noreply.github.com>
2023-08-29 12:42:28 -0600

    wiip 034

--- backend/src/version/v0_3_4.rs
+++ backend/src/version/v0_3_4.rs
@@ -8,7 +8,8 @@ use ssh_key::public::Ed25519PublicKey;
 use super::v0_3_0::V0_3_0_COMPAT;
 use super::*;
 use crate::account::AccountInfo;
-use crate::hostname::{generate_hostname, sync_hostname, Hostname};
+use crate::hostname::{sync_hostname, Hostname};
+use crate::prelude::*;
 
 const V0_3_4: emver::Version = emver::Version::new(0, 3, 4, 0);
 
@@ -42,105 +43,84 @@ impl VersionT for Version {
     fn compat(&self) -> &'static VersionRange {
         &*V0_3_0_COMPAT
     }
-    async fn up<Db: DbHandle>(&self, db: &mut Db, secrets: &PgPool) -> Result<(), Error> {
+    async fn up(&self, db: PatchDb, secrets: &PgPool) -> Result<(), Error> {
         let mut account = AccountInfo::load(secrets).await?;
-        crate::db::DatabaseModel::new()
-            .server_info()
-            .pubkey()
-            .put(
-                db,
-                &ssh_key::PublicKey::from(Ed25519PublicKey::from(&account.key.ssh_key()))
-                    .to_openssh()?,
-            )
+        let account = db
+            .mutate(|d| {
+                d.as_server_info_mut().as_pub_key_mut().ser(
+                    &ssh_key::PublicKey::from(Ed25519PublicKey::from(&account.key.ssh_key()))
+                        .to_openssh()?,
+                );
+                d.as_server_info_mut().as_ca_fingerprint_mut().ser(
+                    &account
+                        .root_ca_cert
+                        .digest(MessageDigest::sha256())
+                        .unwrap()
+                        .iter()
+                        .map(|x| format!("{x:X}"))
+                        .join(":"),
+                );
+                let server_info = d.as_server_info();
+                account.hostname = server_info.as_hostname().de().map(Hostname)?;
+                account.server_id = server_info.as_id().de()?;
+
+                Ok(account)
+            })
             .await?;
-        crate::db::DatabaseModel::new()
-            .server_info()
-            .ca_fingerprint()
-            .put(
-                db,
-                &account
-                    .root_ca_cert
-                    .digest(MessageDigest::sha256())
-                    .unwrap()
-                    .iter()
-                    .map(|x| format!("{x:X}"))
-                    .join(":"),
-            )
-            .await?;
-        let server_info = crate::db::DatabaseModel::new()
-            .server_info()
-            .get(db)
-            .await?
-            .into_owned();
-        account.hostname = server_info
-            .hostname
-            .map(Hostname)
-            .unwrap_or_else(generate_hostname);
-        account.server_id = server_info.id;
         account.save(secrets).await?;
+        let peek = db.peek().await?;
         sync_hostname(&account.hostname).await?;
 
         let parsed_url = Some(COMMUNITY_URL.parse().unwrap());
-        let mut ui = crate::db::DatabaseModel::new().ui().get_mut(db).await?;
-        ui["marketplace"]["known-hosts"][COMMUNITY_URL] = json!({});
-        ui["marketplace"]["known-hosts"][MAIN_REGISTRY] = json!({});
-        for package_id in crate::db::DatabaseModel::new()
-            .package_data()
-            .keys(db)
-            .await?
-        {
-            if !COMMUNITY_SERVICES.contains(&&*package_id.to_string()) {
-                continue;
+        db.mutate(|d| {
+            let mut ui = d.as_ui().de()?;
+            ui["marketplace"]["known-hosts"][COMMUNITY_URL] = json!({});
+            ui["marketplace"]["known-hosts"][MAIN_REGISTRY] = json!({});
+            for package_id in d.as_package_data().keys()? {
+                if !COMMUNITY_SERVICES.contains(&&*package_id.to_string()) {
+                    continue;
+                }
+                d.as_package_data_mut()
+                    .as_idx_model_mut(&package_id)
+                    .or_not_found(&package_id)?
+                    .as_installed_mut()
+                    .or_not_found(&package_id)?
+                    .as_marketplace_url_mut()
+                    .ser(&parsed_url)?;
             }
-            crate::db::DatabaseModel::new()
-                .package_data()
-                .idx_model(&package_id)
-                .expect(db)
-                .await?
-                .installed()
-                .expect(db)
-                .await?
-                .marketplace_url()
-                .put(db, &parsed_url)
-                .await?;
-        }
-        ui["theme"] = json!("Dark".to_string());
-        ui["widgets"] = json!([]);
-        ui.save(db).await?;
-        Ok(())
+            ui["theme"] = json!("Dark".to_string());
+            ui["widgets"] = json!([]);
+
+            d.as_ui_mut().ser(&ui)
+        })
+        .await
     }
-    async fn down<Db: DbHandle>(&self, db: &mut Db, _secrets: &PgPool) -> Result<(), Error> {
-        let mut ui = crate::db::DatabaseModel::new().ui().get_mut(db).await?;
-        let parsed_url = Some(MAIN_REGISTRY.parse().unwrap());
-        for package_id in crate::db::DatabaseModel::new()
-            .package_data()
-            .keys(db)
-            .await?
-        {
-            if !COMMUNITY_SERVICES.contains(&&*package_id.to_string()) {
-                continue;
+    async fn down(&self, db: PatchDb, _secrets: &PgPool) -> Result<(), Error> {
+        db.mutate(|d| {
+            let mut ui = d.as_ui().de()?;
+            let parsed_url = Some(MAIN_REGISTRY.parse().unwrap());
+            for package_id in db.as_package_data().keys()? {
+                if !COMMUNITY_SERVICES.contains(&&*package_id.to_string()) {
+                    continue;
+                }
+                d.as_package_data_mut()
+                    .as_idx_model_mut(&package_id)
+                    .or_not_found(&package_id)?
+                    .as_installed_mut()
+                    .or_not_found(&package_id)?
+                    .as_marketplace_url_mut()
+                    .ser(&parsed_url)?;
             }
-            crate::db::DatabaseModel::new()
-                .package_data()
-                .idx_model(&package_id)
-                .expect(db)
-                .await?
-                .installed()
-                .expect(db)
-                .await?
-                .marketplace_url()
-                .put(db, &parsed_url)
-                .await?;
-        }
 
-        if let Value::Object(ref mut obj) = *ui {
-            obj.remove("theme");
-            obj.remove("widgets");
-        }
+            if let Value::Object(ref mut obj) = *ui {
+                obj.remove("theme");
+                obj.remove("widgets");
+            }
 
-        ui["marketplace"]["known-hosts"][COMMUNITY_URL].take();
-        ui["marketplace"]["known-hosts"][MAIN_REGISTRY].take();
-        ui.save(db).await?;
-        Ok(())
+            ui["marketplace"]["known-hosts"][COMMUNITY_URL].take();
+            ui["marketplace"]["known-hosts"][MAIN_REGISTRY].take();
+            d.as_ui_mut().ser(&ui)
+        })
+        .await
     }
 }
