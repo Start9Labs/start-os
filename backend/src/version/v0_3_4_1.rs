09bd4b74c6baf9d1657f60a40a5353853993bf28
J H <2364004+Blu-J@users.noreply.github.com>
2023-08-29 12:49:12 -0600

    wip some more versions converted

--- backend/src/version/v0_3_4_1.rs
+++ backend/src/version/v0_3_4_1.rs
@@ -4,6 +4,8 @@ use emver::VersionRange;
 use super::v0_3_0::V0_3_0_COMPAT;
 use super::*;
 
+use crate::prelude::*;
+
 const V0_3_4_1: emver::Version = emver::Version::new(0, 3, 4, 1);
 
 #[derive(Clone, Debug)]
@@ -21,10 +23,10 @@ impl VersionT for Version {
     fn compat(&self) -> &'static VersionRange {
         &*V0_3_0_COMPAT
     }
-    async fn up<Db: DbHandle>(&self, _db: &mut Db, _secrets: &PgPool) -> Result<(), Error> {
+    async fn up(&self, _db: PatchDb, _secrets: &PgPool) -> Result<(), Error> {
         Ok(())
     }
-    async fn down<Db: DbHandle>(&self, _db: &mut Db, _secrets: &PgPool) -> Result<(), Error> {
+    async fn down(&self, _db: PatchDb, _secrets: &PgPool) -> Result<(), Error> {
         Ok(())
     }
 }
--- backend/src/version/v0_3_4_3.rs
+++ backend/src/version/v0_3_4_3.rs
@@ -4,6 +4,8 @@ use emver::VersionRange;
 use super::v0_3_0::V0_3_0_COMPAT;
 use super::*;
 
+use crate::prelude::*;
+
 const V0_3_4_3: emver::Version = emver::Version::new(0, 3, 4, 3);
 
 #[derive(Clone, Debug)]
@@ -21,16 +23,10 @@ impl VersionT for Version {
     fn compat(&self) -> &'static VersionRange {
         &*V0_3_0_COMPAT
     }
-    async fn up<Db: DbHandle>(&self, db: &mut Db, _secrets: &PgPool) -> Result<(), Error> {
-        crate::db::DatabaseModel::new()
-            .server_info()
-            .get_mut(db)
-            .await?
-            .save(db)
-            .await?;
+    async fn up(&self, db: PatchDb, _secrets: &PgPool) -> Result<(), Error> {
         Ok(())
     }
-    async fn down<Db: DbHandle>(&self, _db: &mut Db, _secrets: &PgPool) -> Result<(), Error> {
+    async fn down(&self, _db: PatchDb, _secrets: &PgPool) -> Result<(), Error> {
         Ok(())
     }
 }
