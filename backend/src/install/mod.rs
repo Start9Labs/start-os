0e1000032d7ac5809aa83e61990d709edf54fe9a
J H <2364004+Blu-J@users.noreply.github.com>
2023-09-28 15:51:00 -0600

    chore: Something about fixing the build

--- backend/src/install/mod.rs
+++ backend/src/install/mod.rs
@@ -1105,7 +1105,6 @@ pub async fn install_s9pk<R: AsyncRead + AsyncSeek + Unpin + Send + Sync>(
         status: Status {
             configured: manifest.config.is_none(),
             main: MainStatus::Stopped,
-            dependency_errors: Default::default(),
             dependency_config_errors: compute_dependency_config_errs(
                 &ctx,
                 &peek,
