8a496cf6b2c7bcd53f192ca369d4533a03464e94
Aiden McClelland <me@drbonez.dev>
2023-10-27 16:58:02 -0600

    use old secret key derivation function

--- backend/src/util/crypto.rs
+++ backend/src/util/crypto.rs
@@ -1,13 +1,9 @@
-use ed25519_dalek::hazmat::ExpandedSecretKey;
 use ed25519_dalek::{SecretKey, EXPANDED_SECRET_KEY_LENGTH};
 
 #[inline]
 pub fn ed25519_expand_key(key: &SecretKey) -> [u8; EXPANDED_SECRET_KEY_LENGTH] {
-    let key = ExpandedSecretKey::from(key);
-
-    let mut bytes: [u8; 64] = [0u8; 64];
-
-    bytes[..32].copy_from_slice(key.scalar.as_bytes());
-    bytes[32..].copy_from_slice(&key.hash_prefix[..]);
-    bytes
+    ed25519_dalek_v1::ExpandedSecretKey::from(
+        &ed25519_dalek_v1::SecretKey::from_bytes(key).unwrap(),
+    )
+    .to_bytes()
 }
