39de098461833e4c56bd3509644ddf7f1a0fc4ca
Aiden McClelland <me@drbonez.dev>
2023-11-21 08:44:14 -0700

    fix sdk build script

--- core/install-sdk.sh
+++ core/install-sdk.sh
@@ -12,7 +12,7 @@ if [ -z "$PLATFORM" ]; then
   export PLATFORM=$(uname -m)
 fi
 
-cargo install --path=./startos --no-default-features --features=js_engine,sdk,cli --locked
+cargo install --path=./startos --no-default-features --features=js-engine,sdk,cli --locked
 startbox_loc=$(which startbox)
 ln -sf $startbox_loc $(dirname $startbox_loc)/start-cli
 ln -sf $startbox_loc $(dirname $startbox_loc)/start-sdk
