89a8bd2ba082b898faed6b926170402d5a0f599b
J H <dragondef@gmail.com>
2023-12-19 10:37:22 -0700

    chore: Initial commit for the bump

--- web/projects/ui/src/app/services/api/api.fixures.ts
+++ web/projects/ui/src/app/services/api/api.fixures.ts
@@ -21,9 +21,10 @@ export module Mock {
     'shutting-down': false,
   }
   export const MarketplaceEos: RR.GetMarketplaceEosRes = {
-    version: '0.3.5.1',
+    version: '0.3.5.2',
     headline: 'Our biggest release ever.',
     'release-notes': {
+      '0.3.5.2': 'Some **Markdown** release _notes_ for 0.3.5.2',
       '0.3.5.1': 'Some **Markdown** release _notes_ for 0.3.5.1',
       '0.3.4.4': 'Some **Markdown** release _notes_ for 0.3.4.4',
       '0.3.4.3': 'Some **Markdown** release _notes_ for 0.3.4.3',
