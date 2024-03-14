9c6068ef0557cd52f7fadc0183bfc544aa7a277a
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2024-03-14 16:56:31 -0600

    convert tailwindcss to scss in marketplace additional component

--- web/projects/marketplace/src/pages/show/additional/additional-link/additional-link.component.module.ts
+++ web/projects/marketplace/src/pages/show/additional/additional-link/additional-link.component.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { AdditionalLinkComponent } from './additional-link.component'
-import { TuiLabelModule } from '@taiga-ui/core'
+import { TuiLabelModule, TuiSvgModule } from '@taiga-ui/core'
 import { TuiLineClampModule } from '@taiga-ui/kit'
 
 @NgModule({
-  imports: [CommonModule, TuiLabelModule, TuiLineClampModule],
+  imports: [CommonModule, TuiLabelModule, TuiLineClampModule, TuiSvgModule],
   declarations: [AdditionalLinkComponent],
   exports: [AdditionalLinkComponent],
 })
