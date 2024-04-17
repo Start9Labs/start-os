3d4e74c2aa176f5f399f8c241a00689d00b238b0
Aiden McClelland <me@drbonez.dev>
2024-04-17 11:53:52 -0600

    limit log size

--- container-runtime/deb-install.sh
+++ container-runtime/deb-install.sh
@@ -11,8 +11,12 @@ apt-get install -y curl rsync
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 nvm install 20
-
 ln -s $(which node) /usr/bin/node
+
+sed -i '/\(^\|#\)Storage=/c\Storage=persistent' /etc/systemd/journald.conf
+sed -i '/\(^\|#\)Compress=/c\Compress=yes' /etc/systemd/journald.conf
+sed -i '/\(^\|#\)SystemMaxUse=/c\SystemMaxUse=1G' /etc/systemd/journald.conf
+sed -i '/\(^\|#\)ForwardToSyslog=/c\ForwardToSyslog=no' /etc/systemd/journald.conf
 
 systemctl enable container-runtime.service
 
