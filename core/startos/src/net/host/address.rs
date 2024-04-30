97edc0d8420a4b3ee64fd8000c99d210ce6501ef
Dominion5254 <musashidisciple@proton.me>
2024-04-30 11:39:25 -0600

    Remove 'Copy' implementation from `HostAddress`
    Co-authored-by: Aiden McClelland <3732071+dr-bonez@users.noreply.github.com>

--- core/startos/src/net/host/address.rs
+++ core/startos/src/net/host/address.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use torut::onion::OnionAddressV3;
 use ts_rs::TS;
 
-#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord, TS)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord, TS)]
 #[serde(rename_all = "camelCase")]
 #[serde(tag = "kind")]
 #[ts(export)]
