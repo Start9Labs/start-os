be3952152b94aaebd213a4dafffabec9c8f70dc1
Shadowy Super Coder <musashidisciple@proton.me>
2024-07-03 12:50:57 -0600

    parse ValidExVer to string

--- sdk/lib/exver/mod.ts
+++ sdk/lib/exver/mod.ts
@@ -10,3 +10,44 @@ export type Version = {
 }
 
 export type PreReleaseSegment = number | string;
+
+export class ExtendedVersion {
+  to_string(exver: ValidExtendedVersion): string {
+    let exver_str = ""
+
+    if (exver.flavor) {
+      exver_str += '#';
+      exver_str += exver.flavor;
+      exver_str += ':';
+    }
+
+    exver_str = append_version(exver.upstream, exver_str);
+    exver_str += ':';
+    exver_str = append_version(exver.downstream, exver_str);
+
+    return exver_str;
+  }
+}
+
+function append_version(version: Version, str: string): string {
+  version.number.forEach((n, i) => {
+    str += n;
+    if (i < version.number.length) {
+      str += '.';
+    }
+  });
+
+  if (version.prerelease.length > 0) {
+    version.prerelease.forEach((n, i) => {
+      if (typeof n === 'string') {
+        str += '-';
+      }
+      str += n;
+      if (i < version.prerelease.length) {
+        str += '.';
+      }
+    })
+  }
+
+  return str;
+}
