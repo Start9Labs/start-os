c9b962294434b8749a423742392c1c0df746d5ce
Shadowy Super Coder <musashidisciple@proton.me>
2024-07-05 21:19:25 -0600

    add VersionRange and comparative functions

--- sdk/lib/exver/mod.ts
+++ sdk/lib/exver/mod.ts
@@ -1,3 +1,5 @@
+const exverParser = require('./exver.js');
+
 export interface ValidExtendedVersion {
   flavor: string | null,
   upstream: Version,
@@ -9,7 +11,65 @@ export interface Version {
   prerelease: (string | number)[],
 }
 
-export class ExtendedVersion {
+export class VersionRange  {
+  operator: string;
+  anchor: ExtendedVersion;
+
+  constructor(range: string) {
+    const operator = (() => {
+      switch (range.substring(0, 2)) {
+        case ">=": return ">="
+        case "<=": return "<="
+      }
+      switch (range.substring(0, 1)) {
+        case "=": return "="
+        case "<": return "<"
+        case ">": return ">"
+      }
+      throw new Error("Error parsing range:" + range)
+    })();
+    const comparisonOperators = /[><=]/g;
+    const version = range.replace(comparisonOperators, "");
+    this.operator = operator
+    this.anchor = exverParser.parse(version)
+  }
+
+  public satisfiedBy(thisVersion: ExtendedVersion): boolean {
+    switch (this.operator) {
+      case ">=": {
+        return thisVersion.greaterThan(this.anchor) || thisVersion.equals(this.anchor)
+      }
+      case "<=": {
+        return thisVersion.lessThan(this.anchor) || thisVersion.equals(this.anchor)
+      }
+      case ">": {
+        return thisVersion.greaterThan(this.anchor)
+      }
+      case "<": {
+        return thisVersion.lessThan(this.anchor)
+      }
+      case "=": {
+        return thisVersion.equals(this.anchor)
+      }
+    }
+    throw new Error("Error parsing range" + this.operator + this.anchor)
+  }
+}
+
+// #flavor:0.1.2-beta.1:0
+export class ExtendedVersion implements ValidExtendedVersion {
+  flavor: string | null;
+  upstream: Version;
+  downstream: Version;
+
+  constructor(
+    exver: ValidExtendedVersion
+  ) {
+    this.flavor = exver.flavor;
+    this.upstream = exver.upstream;
+    this.downstream = exver.downstream;
+  }
+  
   to_string(exver: ValidExtendedVersion): string {
     let exver_str = ""
 
@@ -25,6 +85,54 @@ export class ExtendedVersion {
 
     return exver_str;
   }
+
+  // TODO: Address prerelease
+  public greaterThan(other: ExtendedVersion): boolean {
+    return greaterThan(this, other)
+  }
+
+  public equals(other: ExtendedVersion): boolean {
+    return equals(this.upstream, other.upstream) && equals(this.downstream, other.downstream)
+  }
+
+  public lessThan(other: ExtendedVersion): boolean {
+    return !this.greaterThan(other)
+  }
+}
+
+function greaterThan(thisVersion: ExtendedVersion, otherVersion: ExtendedVersion): boolean {
+  for (const i in thisVersion.upstream.number) {
+    if (otherVersion.upstream.number[i] == null) {
+      return true
+    }
+    if (thisVersion.upstream.number[i] > otherVersion.upstream.number[i]) {
+      return true
+    }
+    if (thisVersion.upstream.number[i] < otherVersion.upstream.number[i]) {
+      return false
+    }
+  }
+  for (const i in thisVersion.downstream.number) {
+    if (otherVersion.downstream.number[i] == null) {
+      return true
+    }
+    if (thisVersion.downstream.number[i] > otherVersion.downstream.number[i]) {
+      return true
+    }
+    if (thisVersion.downstream.number[i] < otherVersion.downstream.number[i]) {
+      return false
+    }
+  }
+  return false
+}
+
+function equals(thisVersion: Version, otherVersion: Version): boolean {
+  for (const i in thisVersion.number) {
+    if (thisVersion.number[i] !== otherVersion.number[i]) {
+      return false
+    }
+  }
+  return true
 }
 
 function append_version(version: Version, str: string): string {
