29726681d1b521d9f57a237e056169080e10da45
Mariusz Kogen <k0gen@pm.me>
2024-07-27 17:31:32 +0200

    fix SSH Key message

--- web/projects/ui/src/app/pages/server-routes/ssh-keys/ssh-keys.page.ts
+++ web/projects/ui/src/app/pages/server-routes/ssh-keys/ssh-keys.page.ts
@@ -80,7 +80,7 @@ const ADD_OPTIONS: Partial<TuiDialogOptions<{ message: string }>> = {
   label: 'SSH Key',
   data: {
     message:
-      'Enter the SSH public key you would like to authorize for root access to your Embassy.',
+      'Enter the SSH public key you would like to authorize for root access to your StartOS.',
   },
 }
 
