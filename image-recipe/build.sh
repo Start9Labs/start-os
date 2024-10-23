4b64f604d80adac0d9b7cb0a761ec43adcef8636
Aiden McClelland <3732071+dr-bonez@users.noreply.github.com>
2024-10-23 14:21:57 -0600

    Update build.sh

--- image-recipe/build.sh
+++ image-recipe/build.sh
@@ -201,7 +201,7 @@ fi
 
 if [ "${IB_TARGET_PLATFORM}" = "raspberrypi" ]; then
 	ln -sf /usr/bin/pi-beep /usr/local/bin/beep
-	SKIP_WARNING=1 rpi-update stable
+	SKIP_WARNING=1 SKIP_BOOTLOADER=1 rpi-update stable
 fi
 
 useradd --shell /bin/bash -G embassy -m start9
