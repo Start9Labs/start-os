e9d851e4d39d00416bd1d827c60eab4d6ea6498f
Alex Inkin <alexander@inkin.ru>
2024-12-12 03:14:01 +0400

    fix: reset sideload service after websocket completes (#2798)
    * fix: reset sideload service after websocket completes
    
    * chore: fix comment

--- web/projects/ui/src/app/pages/server-routes/sideload/sideload.service.ts
+++ web/projects/ui/src/app/pages/server-routes/sideload/sideload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { T } from '@start9labs/start-sdk'
-import { endWith, ReplaySubject, shareReplay, Subject, switchMap } from 'rxjs'
+import { BehaviorSubject, endWith, shareReplay, Subject, switchMap } from 'rxjs'
 import { ApiService } from 'src/app/services/api/embassy-api.service'
 
 @Injectable({
@@ -9,17 +9,9 @@ import { ApiService } from 'src/app/services/api/embassy-api.service'
 export class SideloadService {
   private readonly guid$ = new Subject<string>()
 
-  readonly websocketConnected$ = new ReplaySubject()
-
   readonly progress$ = this.guid$.pipe(
     switchMap(guid =>
-      this.api
-        .openWebsocket$<T.FullProgress>(guid, {
-          openObserver: {
-            next: () => this.websocketConnected$.next(''),
-          },
-        })
-        .pipe(endWith(null)),
+      this.api.openWebsocket$<T.FullProgress>(guid).pipe(endWith(null)),
     ),
     shareReplay(1),
   )
