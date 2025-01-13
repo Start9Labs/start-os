752e907654f854682834f900a0848d7b2044b0a5
Matt Hill <mattnine@protonmail.com>
2025-01-13 15:53:34 -0700

    show error if sideload fails

--- web/projects/ui/src/app/pages/server-routes/sideload/sideload.service.ts
+++ web/projects/ui/src/app/pages/server-routes/sideload/sideload.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core'
+import { inject, Injectable } from '@angular/core'
+import { ErrorService } from '@start9labs/shared'
 import { T } from '@start9labs/start-sdk'
-import { endWith, shareReplay, Subject, switchMap } from 'rxjs'
+import {
+  catchError,
+  EMPTY,
+  endWith,
+  shareReplay,
+  Subject,
+  switchMap,
+} from 'rxjs'
 import { ApiService } from 'src/app/services/api/embassy-api.service'
 
 @Injectable({
@@ -8,11 +16,16 @@ import { ApiService } from 'src/app/services/api/embassy-api.service'
 })
 export class SideloadService {
   private readonly guid$ = new Subject<string>()
+  private readonly errorService = inject(ErrorService)
 
   readonly progress$ = this.guid$.pipe(
     switchMap(guid =>
       this.api.openWebsocket$<T.FullProgress>(guid).pipe(endWith(null)),
     ),
+    catchError(e => {
+      this.errorService.handleError(e)
+      return EMPTY
+    }),
     shareReplay(1),
   )
 
