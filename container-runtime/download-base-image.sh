02d2c56de3adafdc045319044dacff6aacbe53e1
Aiden McClelland <me@drbonez.dev>
2025-04-23 15:20:11 -0600

    fix script

--- container-runtime/download-base-image.sh
+++ container-runtime/download-base-image.sh
@@ -7,11 +7,16 @@ VERSION=bookworm
 ARCH=${ARCH:-$(uname -m)}
 FLAVOR=default
 
-_ARCH=$([[ "$ARCH" = "x86_64" ]] && echo "amd64" || [[ "$ARCH" = "aarch64" ]] && echo "arm64" || echo "$ARCH")
+_ARCH=$ARCH
+if [ "$_ARCH" = "x86_64" ]; then
+    _ARCH=amd64
+elif [ "$_ARCH" = "aarch64" ]; then
+    _ARCH=arm64
+fi
 
 BASE_URL="https://images.linuxcontainers.org$(curl -fsSL https://images.linuxcontainers.org/meta/1.0/index-system | grep "^$DISTRO;$VERSION;$_ARCH;$FLAVOR;" | head -n1 | sed 's/^.*;//g')"
 OUTPUT_FILE="debian.${ARCH}.squashfs"
 
 echo "Downloading ${BASE_URL}/rootfs.squashfs to $OUTPUT_FILE"
 curl -fsSL "${BASE_URL}/rootfs.squashfs" > "$OUTPUT_FILE"
-curl -fsSL "$BASE_URL/SHA256SUMS" | grep 'rootfs.squashfs' | awk '{print $1"  '"$OUTPUT_FILE"'"}' | shasum -a 256 -c
+curl -fsSL "$BASE_URL/SHA256SUMS" | grep 'rootfs\.squashfs' | awk '{print $1"  '"$OUTPUT_FILE"'"}' | shasum -a 256 -c
