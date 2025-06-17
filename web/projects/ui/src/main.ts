c795ddb7b296e50b32666bd3efce8bf6576eead0
Matt Hill <mattnine@protonmail.com>
2025-06-17 11:12:36 -0600

    fix up pwa stuff

--- web/projects/ui/src/main.ts
+++ web/projects/ui/src/main.ts
@@ -1,10 +1,7 @@
 import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-
 import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
-
-; (window as any).global = window
 
 if (environment.production) {
   enableProdMode()
