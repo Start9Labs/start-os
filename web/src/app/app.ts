404f4ef963cb7c97999aaa2bdddcf6031bb27430
waterplea <alexander@inkin.ru>
2025-09-13 19:04:44 +0400

    add Angular

---
+++ web/src/app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
