7e78e520f64980336e1f39441968c47f6697cb37
Aiden McClelland <me@drbonez.dev>
2025-11-19 11:07:54 -0700

    version bump

--- core/startos/src/version/mod.rs
+++ core/startos/src/version/mod.rs
@@ -53,8 +53,9 @@ mod v0_4_0_alpha_10;
 mod v0_4_0_alpha_11;
 mod v0_4_0_alpha_12;
 mod v0_4_0_alpha_13;
+mod v0_4_0_alpha_14;
 
-pub type Current = v0_4_0_alpha_13::Version; // VERSION_BUMP
+pub type Current = v0_4_0_alpha_14::Version; // VERSION_BUMP
 
 impl Current {
     #[instrument(skip(self, db))]
@@ -169,7 +170,8 @@ enum Version {
     V0_4_0_alpha_10(Wrapper<v0_4_0_alpha_10::Version>),
     V0_4_0_alpha_11(Wrapper<v0_4_0_alpha_11::Version>),
     V0_4_0_alpha_12(Wrapper<v0_4_0_alpha_12::Version>),
-    V0_4_0_alpha_13(Wrapper<v0_4_0_alpha_13::Version>), // VERSION_BUMP
+    V0_4_0_alpha_13(Wrapper<v0_4_0_alpha_13::Version>),
+    V0_4_0_alpha_14(Wrapper<v0_4_0_alpha_14::Version>), // VERSION_BUMP
     Other(exver::Version),
 }
 
@@ -225,7 +227,8 @@ impl Version {
             Self::V0_4_0_alpha_10(v) => DynVersion(Box::new(v.0)),
             Self::V0_4_0_alpha_11(v) => DynVersion(Box::new(v.0)),
             Self::V0_4_0_alpha_12(v) => DynVersion(Box::new(v.0)),
-            Self::V0_4_0_alpha_13(v) => DynVersion(Box::new(v.0)), // VERSION_BUMP
+            Self::V0_4_0_alpha_13(v) => DynVersion(Box::new(v.0)),
+            Self::V0_4_0_alpha_14(v) => DynVersion(Box::new(v.0)), // VERSION_BUMP
             Self::Other(v) => {
                 return Err(Error::new(
                     eyre!("unknown version {v}"),
@@ -273,7 +276,8 @@ impl Version {
             Version::V0_4_0_alpha_10(Wrapper(x)) => x.semver(),
             Version::V0_4_0_alpha_11(Wrapper(x)) => x.semver(),
             Version::V0_4_0_alpha_12(Wrapper(x)) => x.semver(),
-            Version::V0_4_0_alpha_13(Wrapper(x)) => x.semver(), // VERSION_BUMP
+            Version::V0_4_0_alpha_13(Wrapper(x)) => x.semver(),
+            Version::V0_4_0_alpha_14(Wrapper(x)) => x.semver(), // VERSION_BUMP
             Version::Other(x) => x.clone(),
         }
     }
