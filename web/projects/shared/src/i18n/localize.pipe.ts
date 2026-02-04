201965b809f3410b9603779678b39f9ea330dae4
Matt Hill <mattnine@protonmail.com>
2026-02-04 14:00:20 -0700

    trigger chnage detection for localize pipe and round out implementing localize pipe for consistency even though not needed

--- web/projects/shared/src/i18n/localize.pipe.ts
+++ web/projects/shared/src/i18n/localize.pipe.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable, Pipe, PipeTransform } from '@angular/core'
 import { i18nService } from './i18n.service'
+import { I18N } from './i18n.providers'
 import { T } from '@start9labs/start-sdk'
 
 @Pipe({
@@ -9,8 +10,10 @@ import { T } from '@start9labs/start-sdk'
 @Injectable({ providedIn: 'root' })
 export class LocalizePipe implements PipeTransform {
   private readonly i18nService = inject(i18nService)
+  private readonly i18n = inject(I18N)
 
   transform(string: T.LocaleString): string {
+    this.i18n() // read signal to trigger change detection on language switch
     return this.i18nService.localize(string)
   }
 }
