Start9Labs/start-os

commit cca6b7b7c5bb091e529b5410e26346ced005a99c

feat: implement transition for dynamic form fields

web/src/app/routes/lan/routes/ipv6/index.ts
@@ -1,16 +1,23 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   effect,
   inject,
-  signal,
 } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
-import { tuiMarkControlAsTouchedAndValidate } from '@taiga-ui/cdk'
-import { tuiNumberFormatProvider, TuiTitle } from '@taiga-ui/core'
-import { TuiHeader } from '@taiga-ui/layout'
+import { MaskitoDirective } from '@maskito/angular'
+import { TuiAnimated, tuiMarkControlAsTouchedAndValidate } from '@taiga-ui/cdk'
+import {
+  TuiError,
+  TuiHint,
+  TuiInput,
+  TuiLabel,
+  tuiNumberFormatProvider,
+  TuiTitle,
+} from '@taiga-ui/core'
+import { TuiSwitch } from '@taiga-ui/kit'
+import { TuiElasticContainer, TuiHeader } from '@taiga-ui/layout'
 import { startWith } from 'rxjs'
 import { Footer } from 'src/app/components/footer'
 import { Form } from 'src/app/components/form'
@@ -19,7 +26,8 @@ import {
   injectFormService,
   provideFormService,
 } from 'src/app/services/form.service'
-import { LanIpv6Form } from './form'
+import { PREFIX } from 'src/app/utils/masks'
+
 import { LanIpv6Service } from './service'
 import { LanIpv6Summary } from './summary'
 import { LanIpv6Data } from './uci/service'
@@ -36,15 +44,31 @@ import { getLanIpv6Form, updateLanIpv6Validators } from './utils'
       (reset.prevent)="form.reset(service.data())"
       (ngSubmit)="onSave()"
     >
-      <lan-ipv6-form
-        [enabled]="slaacEnabled()"
-        [locked]="slaacLocked()"
-        [lockedReason]="slaacLockedReason()"
-      />
+      <ng-container formGroupName="strategy">
+        <label tuiLabel>
+          <input type="checkbox" tuiSwitch formControlName="slaac" />
+          Enable
+          <i tuiHint="Cannot disable: published ports using IPv6 exist"></i>
+        </label>
+      </ng-container>
+      <tui-elastic-container formGroupName="subnet">
+        @if (slaacEnabled()) {
+          <tui-textfield tuiAnimated>
+            <label tuiLabel>Prefix Length</label>
+            <input tuiInput formControlName="prefix" [maskito]="mask" />
+          </tui-textfield>
+          <tui-error formControlName="prefix" />
+        }
+      </tui-elastic-container>
       @if (service.data()) {
         <footer appFooter></footer>
       }
     </form>
+  `,
+  styles: `
+    tui-textfield {
+      max-width: 10rem;
+    }
   `,
   host: { class: 'g-page' },
   imports: [
@@ -54,7 +78,14 @@ import { getLanIpv6Form, updateLanIpv6Validators } from './utils'
     Footer,
     Form,
     LanIpv6Summary,
-    LanIpv6Form,
+    TuiLabel,
+    TuiSwitch,
+    TuiHint,
+    TuiError,
+    TuiInput,
+    TuiElasticContainer,
+    MaskitoDirective,
+    TuiAnimated,
   ],
   providers: [
     provideFormService(LanIpv6Service),
@@ -63,31 +94,18 @@ import { getLanIpv6Form, updateLanIpv6Validators } from './utils'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class LanIpv6 {
-  protected readonly builder = inject(NonNullableFormBuilder)
-  protected readonly service = injectFormService<LanIpv6Data>()
+  private readonly builder = inject(NonNullableFormBuilder)
   private readonly publishedPortsUci = inject(PublishedPortsUciService)
 
-  readonly form = getLanIpv6Form(this.builder)
-
-  readonly slaacEnabled = toSignal(
+  protected readonly service = injectFormService<LanIpv6Data>()
+  protected readonly form = getLanIpv6Form(this.builder)
+  protected readonly mask = PREFIX
+  protected readonly slaacEnabled = toSignal(
     this.form.controls.strategy.controls.slaac.valueChanges.pipe(
       startWith(this.form.controls.strategy.controls.slaac.value),
     ),
     { requireSync: true },
   )
-
-  readonly wanPrefix = computed(() => this.service.data()?.wanPrefix ?? 48)
-
-  // Track if published ports use IPv6
-  readonly hasIpv6Ports = signal(false)
-
-  // SLAAC is locked if there are published ports using IPv6
-  readonly slaacLocked = computed(() => this.hasIpv6Ports())
-
-  readonly slaacLockedReason = computed(() => {
-    if (!this.slaacLocked()) return null
-    return 'Cannot disable: published ports using IPv6 exist'
-  })
 
   constructor() {
     // Load IPv6 dependencies
@@ -108,15 +126,18 @@ export default class LanIpv6 {
 
     // Update validators when SLAAC mode changes
     effect(() => {
-      const slaac = this.slaacEnabled()
-      const wanPrefix = this.wanPrefix()
-      updateLanIpv6Validators(this.form, slaac, wanPrefix)
+      updateLanIpv6Validators(
+        this.form,
+        this.slaacEnabled(),
+        this.service.data()?.wanPrefix ?? 48,
+      )
     })
   }
 
   private async loadIpv6Dependencies() {
-    const hasPorts = await this.publishedPortsUci.hasIpv6Ports()
-    this.hasIpv6Ports.set(hasPorts)
+    if ((await this.publishedPortsUci.hasIpv6Ports()) && this.slaacEnabled()) {
+      this.form.controls.strategy.controls.slaac.disable()
+    }
   }
 
   async onSave() {

web/src/app/routes/wan/routes/ipv6/form/ip.ts
@@ -1,152 +1,85 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
-  input,
 } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
 import { MaskitoDirective } from '@maskito/angular'
-import { MaskitoOptions } from '@maskito/core'
+import { TUI_FALSE_HANDLER, TuiAnimated } from '@taiga-ui/cdk'
 import {
   TuiError,
   TuiHint,
   TuiInput,
-  TuiRadio,
   TuiTextfield,
   TuiTitle,
   tuiValidationErrorsProvider,
 } from '@taiga-ui/core'
-import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout'
-import { FORM } from 'src/app/components/form'
+import { TuiRadioList } from '@taiga-ui/kit'
 import {
-  IPV6_MODES,
+  TuiCardLarge,
+  TuiElasticContainer,
+  TuiForm,
+  TuiHeader,
+} from '@taiga-ui/layout'
+import { FORM } from 'src/app/components/form'
+import { PREFIX } from 'src/app/utils/masks'
+import WanIpv6 from '../'
+import {
+  IPV6_ALL_CONTROLS,
+  IPV6_CONTROLS,
   IPV6_LABELS,
-  IPV6_DHCPV6_CONTROLS,
-  IPV6_SIXRD_CONTROLS,
-  IPV6_SLAAC_CONTROLS,
-  IPV6_STATIC_CONTROLS,
+  IPV6_MODES,
   IPV6_VALIDATION_ERRORS,
 } from '../utils'
-import WanIpv6 from '../'
 
 @Component({
   selector: 'wan-ipv6-ip',
   template: `
     <header tuiHeader="body-l"><h2 tuiTitle>IP Address</h2></header>
-    <section>
-      @for (mode of modes; track $index) {
-        <label
-          tuiLabel
-          [class.locked]="
-            mode === 'disabled' &&
-            disabledLocked() &&
-            parent.ipMode() !== 'disabled'
-          "
-          [tuiHint]="
-            mode === 'disabled' &&
-            disabledLocked() &&
-            parent.ipMode() !== 'disabled'
-              ? 'Published ports are using IPv6'
-              : null
-          "
-          tuiHintAppearance="error"
-        >
-          <input
-            type="radio"
-            tuiRadio
-            formControlName="mode"
-            [value]="mode"
-            (click)="onModeClick($event, mode)"
-          />
-          {{ labels[mode] }}{{ $index ? '' : ' (Default)' }}
-        </label>
+    <tui-radio-list
+      size="s"
+      formControlName="mode"
+      [items]="modes"
+      [itemContent]="template"
+      [disabledItemHandler]="handler()"
+    />
+    <ng-template #template let-item>
+      {{ $any(labels)[item] }}{{ item === 'slaac' ? ' (Default)' : '' }}
+      <i tuiHint="Published ports are using IPv6"></i>
+    </ng-template>
+    <tui-elastic-container>
+      @if (parent.ipMode() !== 'disabled') {
+        <section tuiAnimated>
+          @for (name of all; track name) {
+            @let auto = ['slaac', 'dhcpv6'].includes(parent.ipMode());
+            @let optional = name === 'prefix' && auto;
+
+            @if (controls[parent.ipMode()]?.includes(name)) {
+              <div>
+                <tui-textfield>
+                  <label tuiLabel>
+                    {{ labels[name] }}{{ optional ? ' (optional)' : '' }}
+                  </label>
+                  <input
+                    tuiInput
+                    [formControlName]="name"
+                    [maskito]="['prefix', 'mask'].includes(name) ? mask : null"
+                    [placeholder]="optional ? 'Auto' : ''"
+                  />
+                </tui-textfield>
+                <tui-error [formControlName]="name" />
+              </div>
+            }
+          }
+        </section>
       }
-    </section>
-
-    @if (parent.ipMode() === 'slaac') {
-      <section>
-        @for (control of slaacControls; track control) {
-          <div>
-            <tui-textfield>
-              <label tuiLabel>{{ labels[control] }}</label>
-              <input
-                tuiInput
-                [formControlName]="control"
-                [maskito]="control === 'prefix' ? prefixMask : null"
-                [placeholder]="control === 'prefix' ? 'Auto' : ''"
-              />
-            </tui-textfield>
-            <tui-error [formControlName]="control" />
-          </div>
-        }
-      </section>
-    }
-
-    @if (parent.ipMode() === 'dhcpv6') {
-      <section>
-        @for (control of dhcpv6Controls; track control) {
-          <div>
-            <tui-textfield>
-              <label tuiLabel>{{ labels[control] }}</label>
-              <input
-                tuiInput
-                [formControlName]="control"
-                [maskito]="control === 'prefix' ? prefixMask : null"
-                [placeholder]="control === 'prefix' ? 'Auto' : ''"
-              />
-            </tui-textfield>
-            <tui-error [formControlName]="control" />
-          </div>
-        }
-      </section>
-    }
-
-    @if (parent.ipMode() === 'static') {
-      <section>
-        @for (control of staticControls; track control) {
-          <div>
-            <tui-textfield>
-              <label tuiLabel>{{ labels[control] }}*</label>
-              <input
-                tuiInput
-                [formControlName]="control"
-                [maskito]="control === 'prefix' ? prefixMask : null"
-              />
-            </tui-textfield>
-            <tui-error [formControlName]="control" />
-          </div>
-        }
-      </section>
-    }
-
-    @if (parent.ipMode() === '6rd') {
-      <section>
-        @for (control of sixrdControls; track control) {
-          <div>
-            <tui-textfield>
-              <label tuiLabel>{{ labels[control] }}*</label>
-              <input
-                tuiInput
-                [formControlName]="control"
-                [maskito]="
-                  control === 'prefix' || control === 'mask' ? prefixMask : null
-                "
-              />
-            </tui-textfield>
-            <tui-error [formControlName]="control" />
-          </div>
-        }
-      </section>
-    }
+    </tui-elastic-container>
   `,
   styles: `
-    label.locked {
-      opacity: 0.6;
-      cursor: not-allowed;
-
-      input {
-        pointer-events: none;
-      }
+    tui-radio-list {
+      flex-direction: row;
+      gap: inherit;
     }
   `,
   viewProviders: [FORM],
@@ -158,38 +91,25 @@ import WanIpv6 from '../'
     TuiHeader,
     TuiTitle,
     TuiTextfield,
-    TuiRadio,
+    TuiRadioList,
     TuiError,
     TuiInput,
     TuiHint,
     MaskitoDirective,
+    TuiAnimated,
+    TuiElasticContainer,
   ],
 })
 export class WanIpv6Ip {
   protected readonly parent = inject(WanIpv6)
-
-  readonly disabledLocked = input(false)
-
   protected readonly modes = IPV6_MODES
   protected readonly labels = IPV6_LABELS
-  protected readonly slaacControls = IPV6_SLAAC_CONTROLS
-  protected readonly dhcpv6Controls = IPV6_DHCPV6_CONTROLS
-  protected readonly staticControls = IPV6_STATIC_CONTROLS
-  protected readonly sixrdControls = IPV6_SIXRD_CONTROLS
-
-  protected readonly prefixMask: MaskitoOptions = {
-    mask: ['/', /\d/, /\d/, /\d/],
-  }
-
-  onModeClick(event: Event, mode: string) {
-    // Prevent selecting 'disabled' if locked and not already disabled
-    if (
-      mode === 'disabled' &&
-      this.disabledLocked() &&
-      this.parent.ipMode() !== 'disabled'
-    ) {
-      event.preventDefault()
-      event.stopPropagation()
-    }
-  }
+  protected readonly controls = IPV6_CONTROLS
+  protected readonly all = IPV6_ALL_CONTROLS
+  protected readonly mask = PREFIX
+  protected readonly handler = computed(() =>
+    this.parent.hasIpv6Ports()
+      ? (item: string) => item === 'ddisabled'
+      : TUI_FALSE_HANDLER,
+  )
 }

web/src/app/routes/devices/routes/device/index.ts
@@ -4,18 +4,20 @@ import {
   computed,
   effect,
   inject,
-  signal,
 } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
 import { ActivatedRoute, Router, RouterLink } from '@angular/router'
 import {
   TuiButton,
+  TuiError,
+  TuiHintDirective,
+  TuiInput,
   TuiLink,
   TuiTitle,
   tuiValidationErrorsProvider,
 } from '@taiga-ui/core'
-import { TuiSkeleton } from '@taiga-ui/kit'
+import { TuiSkeleton, TuiSwitch } from '@taiga-ui/kit'
 import { TuiHeader } from '@taiga-ui/layout'
 import { startWith } from 'rxjs'
 import { Footer } from 'src/app/components/footer'
@@ -27,8 +29,6 @@ import {
   updateDeviceValidators,
 } from 'src/app/routes/devices/utils'
 import { PublishedPortsUciService } from 'src/app/routes/published-ports/uci/service'
-import { DeviceIp } from './ip'
-import { DeviceName } from './name'
 import { DeviceSummary } from './summary'
 
 @Component({
@@ -93,9 +93,33 @@ import { DeviceSummary } from './summary'
       (reset.prevent)="onCancel()"
       (ngSubmit)="onSave()"
     >
-      <device-name [formGroup]="form" [hostname]="data()?.hostname ?? ''" />
-      <hr />
-      <device-ip formGroupName="ip" [ipv4Locked]="portUsage().usesIpv4" />
+      <section>
+        <div>
+          <tui-textfield>
+            <label tuiLabel>Name</label>
+            <input
+              tuiInput
+              formControlName="name"
+              [placeholder]="data()?.hostname ?? ''"
+            />
+          </tui-textfield>
+          <tui-error formControlName="name" />
+        </div>
+      </section>
+      <section formGroupName="ip">
+        <div>
+          <tui-textfield>
+            <label tuiLabel>IPv4 address</label>
+            <input tuiInput formControlName="ipv4" [readOnly]="!ipv4Static()" />
+          </tui-textfield>
+          <tui-error formControlName="ipv4" />
+        </div>
+        <label tuiLabel>
+          <input tuiSwitch type="checkbox" formControlName="ipv4Static" />
+          Reserve
+          <i tuiHint="Required by a published port rule"></i>
+        </label>
+      </section>
       @if (data()) {
         <footer appFooter></footer>
       }
@@ -119,10 +143,12 @@ import { DeviceSummary } from './summary'
     TuiButton,
     Footer,
     Form,
-    DeviceIp,
-    DeviceName,
     DeviceSummary,
     TuiSkeleton,
+    TuiInput,
+    TuiError,
+    TuiHintDirective,
+    TuiSwitch,
   ],
 })
 export default class DeviceDetail {
@@ -133,19 +159,10 @@ export default class DeviceDetail {
   readonly service = inject(DevicesService)
   readonly mac = this.route.snapshot.queryParams['mac']
   readonly returnUrl = history.state?.['returnUrl'] || '/devices'
-
   readonly form = getDeviceForm(inject(NonNullableFormBuilder))
-
-  // Track if this device has published ports using IPv4
-  readonly portUsage = signal<{ usesIpv4: boolean; usesIpv6: boolean }>({
-    usesIpv4: false,
-    usesIpv6: false,
-  })
-
-  readonly data = computed(() => {
-    const allDevices = this.service.data()
-    return allDevices?.find(d => d.mac === this.mac) ?? null
-  })
+  readonly data = computed(
+    () => this.service.data()?.find(d => d.mac === this.mac) ?? null,
+  )
 
   readonly ipv4Static = toSignal(
     this.form.controls.ip.controls.ipv4Static.valueChanges.pipe(
@@ -185,8 +202,9 @@ export default class DeviceDetail {
   }
 
   private async loadDependencies() {
-    const usage = await this.publishedPortsUci.getDevicePortUsage(this.mac)
-    this.portUsage.set(usage)
+    if ((await this.publishedPortsUci.getDevicePortUsage(this.mac)).usesIpv4) {
+      this.form.controls.ip.controls.ipv4Static.disable()
+    }
   }
 
   async onSave() {
@@ -200,9 +218,8 @@ export default class DeviceDetail {
       ipv6Static: formValue.ip.ipv6Static,
       ipv6: formValue.ip.ipv6,
     })
-    if (success) {
-      this.form.markAsPristine()
-    }
+
+    if (success) this.form.markAsPristine()
   }
 
   onCancel() {
@@ -221,22 +238,19 @@ export default class DeviceDetail {
   }
 
   async onBlock() {
-    const success = await this.service.block(this.mac)
-    if (success) {
+    if (await this.service.block(this.mac)) {
       this.router.navigate(['..'], { relativeTo: this.route })
     }
   }
 
   async onUnblock() {
-    const success = await this.service.unblock(this.mac)
-    if (success) {
+    if (await this.service.unblock(this.mac)) {
       this.router.navigate(['..'], { relativeTo: this.route })
     }
   }
 
   async onForget() {
-    const success = await this.service.forget(this.mac)
-    if (success) {
+    if (await this.service.forget(this.mac)) {
       this.router.navigate(['..'], { relativeTo: this.route })
     }
   }

web/src/app/routes/devices/routes/device/ip.ts
@@ -1,95 +1,0 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  input,
-} from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
-import {
-  TuiError,
-  TuiHint,
-  TuiInput,
-  TuiTextfield,
-  TuiTitle,
-  tuiValidationErrorsProvider,
-} from '@taiga-ui/core'
-import { TuiSwitch } from '@taiga-ui/kit'
-import { TuiCardLarge, TuiForm, TuiHeader } from '@taiga-ui/layout'
-import { FORM } from 'src/app/components/form'
-import { DEVICE_VALIDATION_ERRORS } from 'src/app/routes/devices/utils'
-
-import DeviceDetail from '.'
-
-@Component({
-  selector: 'device-ip',
-  template: `
-    <header tuiHeader="body-l"><h2 tuiTitle>IP Address</h2></header>
-    <section>
-      <div>
-        <tui-textfield>
-          <label tuiLabel>IPv4{{ parent.ipv4Static() ? '*' : '' }}</label>
-          <input
-            tuiInput
-            formControlName="ipv4"
-            [readOnly]="!parent.ipv4Static()"
-          />
-        </tui-textfield>
-        <tui-error formControlName="ipv4" />
-      </div>
-      <label
-        tuiLabel
-        [class.locked]="ipv4Locked() && parent.ipv4Static()"
-        [tuiHint]="
-          ipv4Locked() && parent.ipv4Static()
-            ? 'Required by a published port rule'
-            : null
-        "
-        tuiHintAppearance="error"
-      >
-        <input
-          tuiSwitch
-          type="checkbox"
-          formControlName="ipv4Static"
-          (click)="onToggleClick($event, ipv4Locked() && parent.ipv4Static())"
-        />
-        Reserve
-      </label>
-    </section>
-  `,
-  styles: `
-    label.locked {
-      opacity: 0.6;
-      cursor: not-allowed;
-
-      input {
-        pointer-events: none;
-      }
-    }
-  `,
-  viewProviders: [FORM],
-  hostDirectives: [TuiForm, TuiCardLarge],
-  providers: [tuiValidationErrorsProvider(DEVICE_VALIDATION_ERRORS)],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    ReactiveFormsModule,
-    TuiHeader,
-    TuiTitle,
-    TuiTextfield,
-    TuiInput,
-    TuiError,
-    TuiSwitch,
-    TuiHint,
-  ],
-})
-export class DeviceIp {
-  protected readonly parent = inject(DeviceDetail)
-
-  readonly ipv4Locked = input(false)
-
-  onToggleClick(event: Event, locked: boolean) {
-    if (locked) {
-      event.preventDefault()
-      event.stopPropagation()
-    }
-  }
-}

web/src/app/routes/lan/routes/ipv4/form/ip.ts
@@ -29,7 +29,7 @@ import {
 @Component({
   selector: 'lan-ipv4-ip',
   template: `
-    <fieldset [tuiTextfieldCleaner]="false">
+    <fieldset>
       <legend>Network Block</legend>
       <div tuiGroup>
         <tui-textfield tuiDropdownLimitWidth="min">
@@ -48,7 +48,7 @@ import {
         /16
       </div>
     </fieldset>
-    <fieldset [tuiTextfieldCleaner]="false">
+    <fieldset>
       <legend>Router IP</legend>
       <div tuiGroup>
         <tui-textfield>

web/src/app/routes/outbound/routes/vpn/index.ts
@@ -68,7 +68,7 @@ import { VPNSummary } from './summary'
       <section>
         <div>
           <tui-textfield>
-            <label tuiLabel>Label *</label>
+            <label tuiLabel>Label</label>
             <input tuiInput formControlName="label" />
           </tui-textfield>
           <tui-error formControlName="label" />

web/src/app/routes/settings/routes/general/index.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core'
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
 import { WA_LOCAL_STORAGE } from '@ng-web-apis/common'
-import { TuiValueChanges } from '@taiga-ui/cdk'
+import { TuiAnimated, TuiValueChanges } from '@taiga-ui/cdk'
 import {
   TUI_DARK_MODE,
   TUI_DARK_MODE_KEY,
@@ -29,7 +29,7 @@ import {
   TuiDataListWrapper,
   TuiSelect,
 } from '@taiga-ui/kit'
-import { TuiHeader } from '@taiga-ui/layout'
+import { TuiElasticContainer, TuiHeader } from '@taiga-ui/layout'
 import { Footer } from 'src/app/components/footer'
 import { Form } from 'src/app/components/form'
 import { MarkdownPipe } from 'src/app/pipes/markdown.pipe'
@@ -123,12 +123,14 @@ const THEMES: Record<string, Theme> = {
           </label>
         }
       </fieldset>
-      @if (form.value.remote === 'always') {
-        <div tuiNotification appearance="warning">
-          This setting is not recommended as your router will be exposed to the
-          internet
-        </div>
-      }
+      <tui-elastic-container>
+        @if (form.value.remote === 'always') {
+          <div tuiAnimated tuiNotification appearance="warning">
+            This setting is not recommended as your router will be exposed to
+            the internet
+          </div>
+        }
+      </tui-elastic-container>
       <footer appFooter></footer>
     </form>
   `,
@@ -140,7 +142,7 @@ const THEMES: Record<string, Theme> = {
     .update-banner {
       .release-notes {
         color: var(--tui-text-secondary);
-        font: var(--tui-font-text-s);
+        font: var(--tui-typography-text-s);
 
         ::ng-deep {
           h1,
@@ -148,7 +150,7 @@ const THEMES: Record<string, Theme> = {
           h3,
           h4 {
             margin: 0.75rem 0 0.25rem;
-            font: var(--tui-font-text-s);
+            font: var(--tui-typography-text-s);
             font-weight: bold;
             color: var(--tui-text-primary);
 
@@ -210,6 +212,8 @@ const THEMES: Record<string, Theme> = {
     TuiAccordion,
     MarkdownPipe,
     NgDompurifyPipe,
+    TuiElasticContainer,
+    TuiAnimated,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })

web/src/app/routes/wan/routes/ddns/index.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
-import { tuiMarkControlAsTouchedAndValidate } from '@taiga-ui/cdk'
+import { TuiAnimated, tuiMarkControlAsTouchedAndValidate } from '@taiga-ui/cdk'
 import { TuiTextfield, TuiTitle } from '@taiga-ui/core'
 import {
   TuiChevron,
@@ -15,7 +15,7 @@ import {
   TuiSelect,
   TuiSwitch,
 } from '@taiga-ui/kit'
-import { TuiHeader } from '@taiga-ui/layout'
+import { TuiElasticContainer, TuiHeader } from '@taiga-ui/layout'
 import { startWith } from 'rxjs'
 import { Footer } from 'src/app/components/footer'
 import { Form } from 'src/app/components/form'
@@ -51,26 +51,33 @@ import {
         <input type="checkbox" tuiSwitch formControlName="enabled" />
         Enable Dynamic DNS
       </label>
-      @if (enabled()) {
-        <section>
-          <tui-textfield tuiChevron [tuiTextfieldCleaner]="false">
-            <label tuiLabel>Provider</label>
-            <input tuiSelect formControlName="provider" />
-            <tui-data-list-wrapper
-              *tuiDropdown
-              [itemContent]="providerContent"
-              [items]="providerList"
-            />
-            <ng-template #providerContent let-item>
-              {{ getProviderLabel(item) }}
-            </ng-template>
-          </tui-textfield>
-        </section>
-        @if (providerFields().length) {
-          <hr />
-          <ddns-fields formGroupName="fields" [fields]="providerFields()" />
+      <tui-elastic-container>
+        @if (enabled()) {
+          <section tuiAnimated>
+            <tui-textfield tuiChevron>
+              <label tuiLabel>Provider</label>
+              <input tuiSelect formControlName="provider" />
+              <tui-data-list-wrapper
+                *tuiDropdown
+                [itemContent]="providerContent"
+                [items]="providerList"
+              />
+              <ng-template #providerContent let-item>
+                {{ getProviderLabel(item) }}
+              </ng-template>
+            </tui-textfield>
+          </section>
         }
-      }
+      </tui-elastic-container>
+      <tui-elastic-container>
+        @if (enabled() && providerFields().length) {
+          <ddns-fields
+            tuiAnimated
+            formGroupName="fields"
+            [fields]="providerFields()"
+          />
+        }
+      </tui-elastic-container>
       @if (service.data()) {
         <footer appFooter></footer>
       }
@@ -90,6 +97,8 @@ import {
     Footer,
     DdnsSummary,
     DdnsFields,
+    TuiElasticContainer,
+    TuiAnimated,
   ],
   providers: [provideFormService(DdnsService)],
   changeDetection: ChangeDetectionStrategy.OnPush,

commit 83b07813eccfcf411d96e339a5b685b430097a04

Fix IPv6 port-protection bugs from rebase
Fix WAN IPv6 'disabled' mode handler never matching due to 'ddisabled'
typo (introduced in cca6b7b). Make LAN IPv6 SLAAC lock hint conditional
so it only appears when published ports actually use IPv6.

web/src/app/routes/lan/routes/ipv6/index.ts
@@ -3,6 +3,7 @@ import {
   Component,
   effect,
   inject,
+  signal,
 } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms'
@@ -48,7 +49,13 @@ import { getLanIpv6Form, updateLanIpv6Validators } from './utils'
         <label tuiLabel>
           <input type="checkbox" tuiSwitch formControlName="slaac" />
           Enable
-          <i tuiHint="Cannot disable: published ports using IPv6 exist"></i>
+          <i
+            [tuiHint]="
+              slaacLocked()
+                ? 'Cannot disable: published ports using IPv6 exist'
+                : ''
+            "
+          ></i>
         </label>
       </ng-container>
       <tui-elastic-container formGroupName="subnet">
@@ -100,6 +107,7 @@ export default class LanIpv6 {
   protected readonly service = injectFormService<LanIpv6Data>()
   protected readonly form = getLanIpv6Form(this.builder)
   protected readonly mask = PREFIX
+  readonly slaacLocked = signal(false)
   protected readonly slaacEnabled = toSignal(
     this.form.controls.strategy.controls.slaac.valueChanges.pipe(
       startWith(this.form.controls.strategy.controls.slaac.value),
@@ -136,8 +144,11 @@ export default class LanIpv6 {
 
   private async loadIpv6Dependencies() {
     const ports = await this.api.publishedPortsList()
-    if (ports.some(p => p.ipv6 && p.enabled) && this.slaacEnabled()) {
-      this.form.controls.strategy.controls.slaac.disable()
+    if (ports.some(p => p.ipv6 && p.enabled)) {
+      this.slaacLocked.set(true)
+      if (this.slaacEnabled()) {
+        this.form.controls.strategy.controls.slaac.disable()
+      }
     }
   }
 

web/src/app/routes/wan/routes/ipv6/form/ip.ts
@@ -64,7 +64,11 @@ import {
                   <input
                     tuiInput
                     [formControlName]="name"
-                    [maskito]="['prefix', 'ip6prefixlen', 'ip4prefixlen'].includes(name) ? mask : null"
+                    [maskito]="
+                      ['prefix', 'ip6prefixlen', 'ip4prefixlen'].includes(name)
+                        ? mask
+                        : null
+                    "
                     [placeholder]="optional ? 'Auto' : ''"
                   />
                 </tui-textfield>
@@ -109,7 +113,7 @@ export class WanIpv6Ip {
   protected readonly mask = PREFIX
   protected readonly handler = computed(() =>
     this.parent.hasIpv6Ports()
-      ? (item: string) => item === 'ddisabled'
+      ? (item: string) => item === 'disabled'
       : TUI_FALSE_HANDLER,
   )
 }