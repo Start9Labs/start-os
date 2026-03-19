476b10c413721724de4d41103083adf633f3b2cd
Aiden McClelland <me@drbonez.dev>
2026-03-18 23:47:05 -0600

    chore: update dependencies and build scripts

--- build/download-tor-s9pk.sh
+++ build/download-tor-s9pk.sh
@@ -5,10 +5,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 set -e
 
 ARCH=$1
+VERSION="0.4.9.5:0-beta.0"
 
 if [ -z "$ARCH" ]; then
 	>&2 echo "usage: $0 <ARCH>"
 	exit 1
 fi
 
-curl --fail -L -o "./lib/tor_${ARCH}.s9pk" "https://s9pks.nyc3.cdn.digitaloceanspaces.com/tor_${ARCH}.s9pk"
+curl --fail -L -o "./lib/tor_${ARCH}.s9pk" "https://s9pks.nyc3.cdn.digitaloceanspaces.com/tor/${VERSION}/tor_${ARCH}.s9pk"
