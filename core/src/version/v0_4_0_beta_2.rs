9373f40e8241a650f51b2e9a7aec7043d213a168
Aiden McClelland <me@drbonez.dev>
2026-04-03 09:06:05 -0600

    chore: bump version to 0.4.0-beta.2

---
+++ core/src/version/v0_4_0_beta_2.rs
@@ -0,0 +1,37 @@
+use exver::{PreReleaseSegment, VersionRange};
+
+use super::v0_3_5::V0_3_0_COMPAT;
+use super::{VersionT, v0_4_0_beta_1};
+use crate::prelude::*;
+
+lazy_static::lazy_static! {
+    static ref V0_4_0_beta_2: exver::Version = exver::Version::new(
+        [0, 4, 0],
+        [PreReleaseSegment::String("beta".into()), 2.into()]
+    );
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Version;
+
+impl VersionT for Version {
+    type Previous = v0_4_0_beta_1::Version;
+    type PreUpRes = ();
+
+    async fn pre_up(self) -> Result<Self::PreUpRes, Error> {
+        Ok(())
+    }
+    fn semver(self) -> exver::Version {
+        V0_4_0_beta_2.clone()
+    }
+    fn compat(self) -> &'static VersionRange {
+        &V0_3_0_COMPAT
+    }
+    #[instrument(skip_all)]
+    fn up(self, _db: &mut Value, _: Self::PreUpRes) -> Result<Value, Error> {
+        Ok(Value::Null)
+    }
+    fn down(self, _db: &mut Value) -> Result<(), Error> {
+        Ok(())
+    }
+}
