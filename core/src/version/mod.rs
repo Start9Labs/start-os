51bb2ce5f8a382f950c3adbe347469b152465ba4
Aiden McClelland <me@drbonez.dev>
2026-04-06 22:20:17 -0600

    chore: bump version to 0.4.0-beta.3

--- core/src/version/mod.rs
+++ core/src/version/mod.rs
@@ -66,8 +66,9 @@ mod v0_4_0_alpha_23;
 mod v0_4_0_beta_0;
 mod v0_4_0_beta_1;
 mod v0_4_0_beta_2;
+mod v0_4_0_beta_3;
 
-pub type Current = v0_4_0_beta_2::Version; // VERSION_BUMP
+pub type Current = v0_4_0_beta_3::Version; // VERSION_BUMP
 
 impl Current {
     #[instrument(skip(self, db))]
@@ -201,7 +202,8 @@ enum Version {
     V0_4_0_alpha_23(Wrapper<v0_4_0_alpha_23::Version>),
     V0_4_0_beta_0(Wrapper<v0_4_0_beta_0::Version>),
     V0_4_0_beta_1(Wrapper<v0_4_0_beta_1::Version>),
-    V0_4_0_beta_2(Wrapper<v0_4_0_beta_2::Version>), // VERSION_BUMP
+    V0_4_0_beta_2(Wrapper<v0_4_0_beta_2::Version>),
+    V0_4_0_beta_3(Wrapper<v0_4_0_beta_3::Version>), // VERSION_BUMP
     Other(exver::Version),
 }
 
@@ -270,7 +272,8 @@ impl Version {
             Self::V0_4_0_alpha_23(v) => DynVersion(Box::new(v.0)),
             Self::V0_4_0_beta_0(v) => DynVersion(Box::new(v.0)),
             Self::V0_4_0_beta_1(v) => DynVersion(Box::new(v.0)),
-            Self::V0_4_0_beta_2(v) => DynVersion(Box::new(v.0)), // VERSION_BUMP
+            Self::V0_4_0_beta_2(v) => DynVersion(Box::new(v.0)),
+            Self::V0_4_0_beta_3(v) => DynVersion(Box::new(v.0)), // VERSION_BUMP
             Self::Other(v) => {
                 return Err(Error::new(
                     eyre!("unknown version {v}"),
@@ -331,7 +334,8 @@ impl Version {
             Version::V0_4_0_alpha_23(Wrapper(x)) => x.semver(),
             Version::V0_4_0_beta_0(Wrapper(x)) => x.semver(),
             Version::V0_4_0_beta_1(Wrapper(x)) => x.semver(),
-            Version::V0_4_0_beta_2(Wrapper(x)) => x.semver(), // VERSION_BUMP
+            Version::V0_4_0_beta_2(Wrapper(x)) => x.semver(),
+            Version::V0_4_0_beta_3(Wrapper(x)) => x.semver(), // VERSION_BUMP
             Version::Other(x) => x.clone(),
         }
     }
