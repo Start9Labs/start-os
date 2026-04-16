5994226f9c2d0d936091d478f1092b77c33ee160
waterplea <alexander@inkin.ru>
2026-04-16 11:36:01 +0700

    chore: fix

--- web/projects/start-tunnel/src/app/services/title.service.ts
+++ web/projects/start-tunnel/src/app/services/title.service.ts
@@ -7,6 +7,10 @@ export class AppTitleStrategy extends TitleStrategy {
   private readonly title = inject(Title)
 
   override updateTitle(snapshot: RouterStateSnapshot): void {
-    this.title.setTitle(`StartTunnel – ${this.buildTitle(snapshot)}`)
+    this.title.setTitle(this.parse(this.buildTitle(snapshot)))
+  }
+
+  private parse(title?: string): string {
+    return title ? `StartTunnel – ${title}` : 'StartTunnel'
   }
 }
